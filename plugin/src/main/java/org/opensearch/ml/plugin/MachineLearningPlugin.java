386a837a5f1f9248c6e5783043fe5c0a8f2a29c9
Hanguang Zhang <eva.waallee@gmail.com>
2021-04-19 14:28:22 -0700

    Stat REST API (#6)
    * Add RestStatsMLAction and UT
    
    * Register Stats REST API in MachineLearningPlugin
    Decrease line coverage from 0.8 to 0.7 due to this change
    
    * Add todo to remind adding more logic to triage stats requests based on node type

--- plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
+++ plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
@@ -16,22 +16,87 @@
 
 package org.opensearch.ml.plugin;
 
+import com.google.common.collect.ImmutableMap;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.node.DiscoveryNodes;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.io.stream.NamedWriteableRegistry;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.IndexScopedSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.settings.SettingsFilter;
+import org.opensearch.common.xcontent.NamedXContentRegistry;
+import org.opensearch.env.Environment;
+import org.opensearch.env.NodeEnvironment;
 import org.opensearch.ml.action.stats.MLStatsNodesAction;
 import org.opensearch.ml.action.stats.MLStatsNodesTransportAction;
+import org.opensearch.ml.rest.RestStatsMLAction;
+import org.opensearch.ml.stats.MLStat;
+import org.opensearch.ml.stats.MLStats;
+import org.opensearch.ml.stats.StatNames;
+import org.opensearch.ml.stats.suppliers.CounterSupplier;
 import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.action.ActionRequest;
 import org.opensearch.action.ActionResponse;
 import com.google.common.collect.ImmutableList;
+import org.opensearch.repositories.RepositoriesService;
+import org.opensearch.rest.RestController;
+import org.opensearch.rest.RestHandler;
+import org.opensearch.script.ScriptService;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.watcher.ResourceWatcherService;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class MachineLearningPlugin extends Plugin implements ActionPlugin {
+    public static final String ML_BASE_URI = "/_opendistro/_ml";
+
+    private MLStats mlStats;
+
     @Override
     public List<ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
         return ImmutableList.of(
                 new ActionHandler<>(MLStatsNodesAction.INSTANCE,
                         MLStatsNodesTransportAction.class)
         );
+    }
+
+    @Override
+    public Collection<Object> createComponents(Client client, ClusterService clusterService, ThreadPool threadPool,
+                                               ResourceWatcherService resourceWatcherService,
+                                               ScriptService scriptService,
+                                               NamedXContentRegistry xContentRegistry, Environment environment,
+                                               NodeEnvironment nodeEnvironment,
+                                               NamedWriteableRegistry namedWriteableRegistry,
+                                               IndexNameExpressionResolver indexNameExpressionResolver,
+                                               Supplier<RepositoriesService> repositoriesServiceSupplier) {
+        Map<String, MLStat<?>> stats = ImmutableMap
+                .<String, MLStat<?>>builder()
+                .put(StatNames.ML_EXECUTING_TASK_COUNT.getName(), new MLStat<>(false, new CounterSupplier()))
+                .build();
+        this.mlStats = new MLStats(stats);
+        return ImmutableList.of(mlStats);
+    }
+
+    @Override
+    public List<RestHandler> getRestHandlers(
+            Settings settings,
+            RestController restController,
+            ClusterSettings clusterSettings,
+            IndexScopedSettings indexScopedSettings,
+            SettingsFilter settingsFilter,
+            IndexNameExpressionResolver indexNameExpressionResolver,
+            Supplier<DiscoveryNodes> nodesInCluster
+    ) {
+        RestStatsMLAction restStatsMLAction = new RestStatsMLAction(mlStats);
+        return ImmutableList
+                .of(
+                        restStatsMLAction
+                );
     }
 }
---
+++ plugin/src/main/java/org/opensearch/ml/rest/RestStatsMLAction.java
@@ -0,0 +1,120 @@
+package org.opensearch.ml.rest;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.opensearch.ml.action.stats.MLStatsNodesAction;
+import org.opensearch.ml.action.stats.MLStatsNodesRequest;
+import org.opensearch.ml.stats.MLStats;
+import com.google.common.collect.ImmutableList;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.rest.BaseRestHandler;
+import org.opensearch.rest.RestRequest;
+import org.opensearch.rest.action.RestToXContentListener;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.opensearch.ml.plugin.MachineLearningPlugin.ML_BASE_URI;
+
+public class RestStatsMLAction extends BaseRestHandler {
+    private static final String STATS_ML_ACTION = "stats_ml";
+    private MLStats mlStats;
+
+    /**
+     * Constructor
+     *
+     * @param mlStats MLStats object
+     */
+    public RestStatsMLAction(MLStats mlStats) {
+        this.mlStats = mlStats;
+    }
+
+    @Override
+    public String getName() {
+        return STATS_ML_ACTION;
+    }
+
+
+    @Override
+    public List<Route> routes() {
+        return ImmutableList
+                .of(
+                        new Route(RestRequest.Method.GET, ML_BASE_URI + "/{nodeId}/stats/"),
+                        new Route(RestRequest.Method.GET, ML_BASE_URI + "/{nodeId}/stats/{stat}"),
+                        new Route(RestRequest.Method.GET, ML_BASE_URI + "/stats/"),
+                        new Route(RestRequest.Method.GET, ML_BASE_URI + "/stats/{stat}")
+                );
+    }
+
+
+    @Override
+    protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) {
+        MLStatsNodesRequest mlStatsNodesRequest = getRequest(request);
+        return channel -> client.execute(MLStatsNodesAction.INSTANCE, mlStatsNodesRequest, new RestToXContentListener<>(channel));
+    }
+
+    /**
+     * Creates a MLStatsNodesRequest from a RestRequest
+     *
+     * @param request RestRequest
+     * @return MLStatsNodesRequest
+     */
+    @VisibleForTesting
+    MLStatsNodesRequest getRequest(RestRequest request) {
+        // todo: add logic to triage request based on node type(ML node or data node)
+        MLStatsNodesRequest mlStatsRequest = new MLStatsNodesRequest(
+                splitCommaSeparatedParam(request, "nodeId").orElse(null));
+        mlStatsRequest.timeout(request.param("timeout"));
+
+        List<String> requestedStats =
+                splitCommaSeparatedParam(request, "stat")
+                        .map(Arrays::asList)
+                        .orElseGet(Collections::emptyList);
+
+        Set<String> validStats = mlStats.getStats().keySet();
+        if (isAllStatsRequested(requestedStats)) {
+            mlStatsRequest.addAll(validStats);
+        } else {
+            mlStatsRequest.addAll(getStatsToBeRetrieved(request, validStats, requestedStats));
+        }
+
+        return mlStatsRequest;
+    }
+
+    @VisibleForTesting
+    Set<String> getStatsToBeRetrieved(
+            RestRequest request, Set<String> validStats, List<String> requestedStats) {
+        if (requestedStats.contains(MLStatsNodesRequest.ALL_STATS_KEY)) {
+            throw new IllegalArgumentException(
+                    String.format("Request %s contains both %s and individual stats",
+                            request.path(), MLStatsNodesRequest.ALL_STATS_KEY));
+        }
+
+        Set<String> invalidStats =
+                requestedStats.stream()
+                        .filter(s -> !validStats.contains(s))
+                        .collect(Collectors.toSet());
+
+        if (!invalidStats.isEmpty()) {
+            throw new IllegalArgumentException(
+                    unrecognized(request, invalidStats, new HashSet<>(requestedStats), "stat"));
+        }
+        return new HashSet<>(requestedStats);
+    }
+
+    @VisibleForTesting
+    boolean isAllStatsRequested(List<String> requestedStats) {
+        return requestedStats.isEmpty()
+                || (requestedStats.size() == 1 && requestedStats.contains(MLStatsNodesRequest.ALL_STATS_KEY));
+    }
+
+    @VisibleForTesting
+    Optional<String[]> splitCommaSeparatedParam(RestRequest request, String paramName) {
+        return Optional.ofNullable(request.param(paramName))
+                .map(s -> s.split(","));
+    }
+}
