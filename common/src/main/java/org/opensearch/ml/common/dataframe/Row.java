bfe990f6ee08038832fa66c253f82feb20581ae2
Weicong Sun <61702346+weicongs-amazon@users.noreply.github.com>
2021-05-03 15:32:41 -0700

    update the UT coverage of ml-algorithms (#22)

--- common/src/main/java/org/opensearch/ml/common/dataframe/Row.java
+++ common/src/main/java/org/opensearch/ml/common/dataframe/Row.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 
-import lombok.Getter;
 import org.opensearch.common.io.stream.StreamInput;
 import org.opensearch.common.io.stream.StreamOutput;
 import org.opensearch.common.io.stream.Writeable;
