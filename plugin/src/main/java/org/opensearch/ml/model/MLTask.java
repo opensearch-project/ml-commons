02fb84ae6dfe9deefd35039085c55b838cf0f7b8
Hanguang Zhang <eva.waallee@gmail.com>
2021-05-06 15:37:29 -0700

    Adding MLTask (#24)
    * Add MLTask data model.
    
    * Align task type
    
    * Modify MLTaskTest case
    
    * Add error field to store failure message
    Adjust taskType and state to use Enum type.
    
    * Move @Getter to class level

---
+++ plugin/src/main/java/org/opensearch/ml/model/MLTask.java
@@ -0,0 +1,66 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ *
+ */
+
+package org.opensearch.ml.model;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.common.io.stream.StreamOutput;
+import org.opensearch.common.io.stream.Writeable;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@Getter
+@EqualsAndHashCode
+public class MLTask implements Writeable {
+    private final String taskId;
+    private final MLTaskType taskType;
+    @Setter
+    private MLTaskState state;
+    private final Instant createTime;
+    private final String modelId;
+    @Setter
+    private String error;
+
+    @Builder
+    public MLTask(String taskId, MLTaskType taskType, MLTaskState state, Instant createTime, String modelId, String error) {
+        this.taskId = taskId;
+        this.taskType = taskType;
+        this.state = state;
+        this.createTime = createTime;
+        this.modelId = modelId;
+        this.error = error;
+    }
+
+    public MLTask(StreamInput input) throws IOException {
+        this.taskId = input.readOptionalString();
+        this.taskType = input.readEnum(MLTaskType.class);
+        this.state = input.readEnum(MLTaskState.class);
+        this.createTime = input.readOptionalInstant();
+        this.modelId = input.readOptionalString();
+        this.error = input.readOptionalString();
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeOptionalString(taskId);
+        out.writeEnum(taskType);
+        out.writeEnum(state);
+        out.writeOptionalInstant(createTime);
+        out.writeOptionalString(modelId);
+        out.writeOptionalString(error);
+    }
+}
---
+++ plugin/src/main/java/org/opensearch/ml/model/MLTaskState.java
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ *
+ */
+
+package org.opensearch.ml.model;
+
+/**
+ * ML task states.
+ * <ul>
+ * <li><code>CREATED</code>:
+ *     When user send a machine-learning(ML) request(training/inference), we will create one task to track
+ *     ML task execution and set its state as CREATED
+ *
+ * <li><code>RUNNING</code>:
+ *     Once MLTask is dispatched to work node and start to run corresponding ML algorithm, we will set the task state as RUNNING
+ *
+ * <li><code>COMPLETED</code>:
+ *     When all training/inference completed, we will set task state as COMPLETED
+ *
+ * <li><code>FAILED</code>:
+ *     If any exception happen, we will set task state as FAILED
+ * </ul>
+ */
+public enum MLTaskState {
+    CREATED,
+    RUNNING,
+    COMPLETED,
+    FAILED
+}
