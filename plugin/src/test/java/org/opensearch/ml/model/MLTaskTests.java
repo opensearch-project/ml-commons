69e1eb10604b72319ed6674aedc09fd6e4e19a78
Hanguang Zhang <eva.waallee@gmail.com>
2021-05-06 15:37:29 -0700

    Adding MLTask (#24)
    * Add MLTask data model.
    
    * Align task type
    
    * Modify MLTaskTest case
    
    * Add error field to store failure message
    Adjust taskType and state to use Enum type.
    
    * Move @Getter to class level

---
+++ plugin/src/test/java/org/opensearch/ml/model/MLTaskTests.java
@@ -0,0 +1,39 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ *
+ */
+
+package org.opensearch.ml.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+
+import java.io.IOException;
+import java.time.Instant;
+
+public class MLTaskTests {
+    @Test
+    public void testWriteTo() throws IOException {
+        BytesStreamOutput output = new BytesStreamOutput();
+        Instant now = Instant.now();
+        MLTask task1 = MLTask.builder()
+                .taskId("dummy taskId")
+                .taskType(MLTaskType.PREDICTION)
+                .modelId(null)
+                .createTime(now)
+                .state(MLTaskState.RUNNING)
+                .error(null)
+                .build();
+        task1.writeTo(output);
+        MLTask task2 = new MLTask(output.bytes().streamInput());
+        Assert.assertEquals(task1, task2);
+    }
+}
