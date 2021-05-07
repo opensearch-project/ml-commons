886efe4bdbd89b096bee734b4a09f90cbc3e31ae
Hanguang Zhang <hgzhang@amazon.com>
2021-05-07 13:57:58 -0700

    Modify license header

--- plugin/src/main/java/org/opensearch/ml/utils/MLNodeUtils.java
+++ plugin/src/main/java/org/opensearch/ml/utils/MLNodeUtils.java
@@ -1,16 +1,13 @@
 /*
- *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  *
- *  Licensed under the Apache License, Version 2.0 (the "License").
- *  You may not use this file except in compliance with the License.
- *  A copy of the License is located at
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
  *
- *  or in the "license" file accompanying this file. This file is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *  express or implied. See the License for the specific language governing
- *  permissions and limitations under the License.
  */
 
 package org.opensearch.ml.utils;
