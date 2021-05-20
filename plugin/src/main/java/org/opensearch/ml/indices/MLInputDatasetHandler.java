8d604b968f637b55500de461fc409ab033ed196b
Hanguang Zhang <hgzhang@amazon.com>
2021-05-20 16:44:15 -0700

    Add MLInputDatasetHandler to handle search query input

---
+++ plugin/src/main/java/org/opensearch/ml/indices/MLInputDatasetHandler.java
@@ -0,0 +1,94 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ *
+ */
+
+package org.opensearch.ml.indices;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import org.opensearch.action.ActionListener;
+import org.opensearch.action.search.SearchRequest;
+import org.opensearch.client.Client;
+import org.opensearch.ml.common.dataframe.DataFrame;
+import org.opensearch.ml.common.dataframe.DataFrameBuilder;
+import org.opensearch.ml.common.dataset.DataFrameInputDataset;
+import org.opensearch.ml.common.dataset.MLInputDataset;
+import org.opensearch.ml.common.dataset.SearchQueryInputDataset;
+import org.opensearch.search.SearchHit;
+import org.opensearch.search.SearchHits;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Convert MLInputDataset to Dataframe
+ */
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+@Log4j2
+public class MLInputDatasetHandler {
+    Client client;
+
+    /**
+     * Retrieve DataFrame from DataFrameInputDataset
+     * @param mlInputDataset MLInputDataset
+     * @return DataFrame
+     */
+    public DataFrame parseDataFrameInput(MLInputDataset mlInputDataset) {
+        DataFrameInputDataset inputDataset = (DataFrameInputDataset) mlInputDataset;
+        return inputDataset.getDataFrame();
+    }
+
+    /**
+     * Create DataFrame based on given search query
+     * @param mlInputDataset MLInputDataset
+     * @param listener ActionListener
+     */
+    public void parseSearchQueryInput(MLInputDataset mlInputDataset, ActionListener<DataFrame> listener) {
+        SearchQueryInputDataset inputDataset = (SearchQueryInputDataset) mlInputDataset;
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.source(inputDataset.getSearchSourceBuilder());
+        List<String> indicesList = inputDataset.getIndices();
+        String[] indices = new String[indicesList.size()];
+        indices = indicesList.toArray(indices);
+        searchRequest.indices(indices);
+
+        client.search(searchRequest, ActionListener.wrap(r -> {
+            if (
+                    r == null ||
+                    r.getHits() == null ||
+                    r.getHits().getTotalHits() == null ||
+                    r.getHits().getTotalHits().value == 0
+            ) {
+                // todo: add specific exception
+                listener.onFailure(new RuntimeException("No document found"));
+                return;
+            }
+            SearchHits hits = r.getHits();
+            List<Map<String, Object>> input = new ArrayList<>();
+            SearchHit[] searchHits = hits.getHits();
+            for (SearchHit hit : searchHits) {
+                System.out.println("hit" + hit);
+                input.add(hit.getSourceAsMap());
+            }
+            DataFrame dataFrame = DataFrameBuilder.load(input);
+            listener.onResponse(dataFrame);
+            return;
+        }, e -> {
+            log.error("Failed to search" + e);
+            listener.onFailure(e);
+        }));
+        return;
+    }
+}
