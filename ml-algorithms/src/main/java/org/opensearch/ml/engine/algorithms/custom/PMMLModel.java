73025b25cd5506ca7e46bfcbfc3b5cb851b500ba
Will Wang <wqixian@amazon.com>
2021-07-21 00:15:18 +0000

    Fixed pmml predict.

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/custom/PMMLModel.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/custom/PMMLModel.java
@@ -11,6 +11,10 @@ import java.util.*;
 
 // Cannot import the following library since the name repeats with a previous import
 // import org.pmml4s.model.Model;
+import org.pmml4s.data.Series;
+import org.pmml4s.util.Utils;
+import org.pmml4s.common.StructType;
+import org.pmml4s.common.StructField;
 
 public class PMMLModel implements MLAlgo {
 
@@ -28,12 +32,21 @@ public class PMMLModel implements MLAlgo {
         try {
             // retrieve the model from byte array
             org.pmml4s.model.Model pmmlModel = org.pmml4s.model.Model.fromBytes(model.getContent());
-            // turn dataFrame into pmml4s compatible format
-            Object[] inputArray = prepareInput(dataFrame);
-            // utilize the external pmml4s package to predict
-            Object[] predictions = pmmlModel.predict(inputArray);
+            // input schema contains a list of input fields with its name and data type
+            StructType inputSchema = pmmlModel.inputSchema();
+            // make predictions for each data point
+            List<Series> predictions = new ArrayList<>();
+            for (int i = 0; i < dataFrame.size(); i++) {
+                Row row = dataFrame.getRow(i);
+                // turn row into pmml4s compatible format, object array
+                Object[] inputArray = prepareInput(row, inputSchema);
+                // utilize the external pmml4s package to predict
+                Series result = pmmlModel.predict(Series.fromArray(inputArray));
+                predictions.add(result);
+            }
             // turn output into proper format for data frame builder
-            List<Map<String, Object>> listPrediction = prepareOutput(predictions);
+            String[] outputFields = pmmlModel.outputNames();
+            List<Map<String, Object>> listPrediction = prepareOutput(predictions, outputFields);
             return DataFrameBuilder.load(listPrediction);
         } catch (Exception e) {
             throw new RuntimeException("failed retrieving model from model content or predicting", e);
@@ -56,55 +69,34 @@ public class PMMLModel implements MLAlgo {
     }
 
     /**
-     * Transforms input data frame to pmml4s predict input format
-     * @param dataFrame input data to make predictions
-     * @return array of values or value arrays, depending on how many predicting points are passed in (one or many)
+     * Transforms row to pmml4s predict input format
+     * @param row a row of input data frame
+     * @param inputSchema the input schema of the pmml model
+     * @return object array, representing a row of the data frame
      */
-    private Object[] prepareInput(DataFrame dataFrame) {
-        int numRows = dataFrame.size();
-        int numColumns = dataFrame.getRow(0).size();
-        if (numRows == 1) {
-            // only one row of data, transform data frame into an array of values
-            Object[] dataArray = new Object[numColumns];
-            Row row = dataFrame.getRow(0);
-            for (int j = 0; j < numColumns; j++) {
-                dataArray[j] = row.getValue(j);
-            }
-            return dataArray;
-        } else {
-            // more than 1 row of data, transform data frame into a double array (matrix) of values
-            Object[] dataArray = new Object[numRows];
-            for (int i = 0; i < numRows; i++) {
-                Row row = dataFrame.getRow(i);
-                Object[] objectsRow = new Object[numColumns];
-                for (int j = 0; j < numColumns; j++) {
-                    objectsRow[j] = row.getValue(j);
-                }
-                dataArray[i] = objectsRow;
-            }
-            return dataArray;
+    private Object[] prepareInput(Row row, StructType inputSchema) {
+        // convert the data to the desired type defined by PMML, and keep the same order as defined in the input schema.
+        Object[] values = new Object[inputSchema.size()];
+        for (int i = 0; i < values.length; i++) {
+            // since values in a data point can be different types, utilize pmml4s to create the object array
+            StructField sf = inputSchema.apply(i);
+            values[i] = Utils.toVal(row.getValue(i).getValue(), sf.dataType());
         }
+        return values;
     }
 
     /**
-     * Transforms output array from pmml4s predictions to proper format for data frame builder
-     * @param predictions pmml4s produced predictions
-     * @return list of map, with keys being "Prediction" and values being the prediction values
+     * Transforms output arrays from pmml4s predictions to proper format for data frame builder
+     * @param predictions pmml4s predictions
+     * @param outputFields the field names of each prediction array (custom models can have different fields)
+     * @return list of maps, in which each map represents one prediction
      */
-    private List<Map<String, Object>> prepareOutput(Object[] predictions) {
+    private List<Map<String, Object>> prepareOutput(List<Series> predictions, String[] outputFields) {
         List<Map<String, Object>> listPrediction = new ArrayList<>();
-        boolean multiplePredictions = predictions[0].getClass().isArray();
-        if (!multiplePredictions) {
-            // only one row of data
-            int decision = ((double) predictions[0]) < 0 ? 1 : 0;
-            listPrediction.add(Collections.singletonMap("Prediction", decision));
-        } else {
-            // more than 1 row of data
-            for (Object prediction : predictions) {
-                Object[] p = (Object[]) prediction;
-                int decision = ((double) p[0]) < 0 ? 1 : 0;
-                listPrediction.add(Collections.singletonMap("Prediction", decision));
-            }
+        // transform Series into maps
+        for (Series prediction : predictions) {
+            Map<String, Object> predictionMap = prediction.toJavaMap();
+            listPrediction.add(predictionMap);
         }
         return listPrediction;
     }
