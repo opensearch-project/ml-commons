0b36612e73c43e3e712166daba0c7583dae0e7c4
Lai <57818076+wnbts@users.noreply.github.com>
2021-12-30 14:25:27 -0800

    add anomaly localization implementation (#103)
    * add anomaly localization implementation
    
    Signed-off-by: lai <57818076+wnbts@users.noreply.github.com>

---
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/anomalylocalization/AnomalyLocalizer.java
@@ -0,0 +1,29 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ *
+ */
+
+package org.opensearch.ml.engine.algorithms.anomalylocalization;
+
+import org.opensearch.action.ActionListener;
+
+/**
+ * Localizes aggregate anomalies.
+ */
+public interface AnomalyLocalizer {
+
+    /**
+     * Gets localized entity results for data specified in the input.
+     *
+     * @param input    Information about aggregation and metadata.
+     * @param listener Listener to localized details or exception.
+     */
+    void getLocalizationResults(Input input, ActionListener<Output> listener);
+}
