65a3f39da3105865c716f517efc92d40966b4121
Yaliang Wu <ylwu@amazon.com>
2022-03-09 09:04:27 -0800

    add more UT for ml-algorithms (#182)
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/sample/LocalSampleCalculator.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/sample/LocalSampleCalculator.java
@@ -6,13 +6,9 @@
 package org.opensearch.ml.engine.algorithms.sample;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.opensearch.client.Client;
-import org.opensearch.common.inject.Inject;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.ml.common.dataframe.DataFrame;
 import org.opensearch.ml.common.parameter.FunctionName;
 import org.opensearch.ml.common.parameter.Input;
 import org.opensearch.ml.common.parameter.LocalSampleCalculatorInput;
@@ -57,7 +53,7 @@ public class LocalSampleCalculator implements Executable {
                 double min = inputData.stream().min(Comparator.naturalOrder()).get();
                 return new SampleAlgoOutput(min);
             default:
-                throw new IllegalArgumentException("can't support this operation " + operation);
+                throw new IllegalArgumentException("can't support this operation");
         }
     }
 }
--- ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/clustering/KMeansTest.java
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/clustering/KMeansTest.java
@@ -7,18 +7,21 @@ package org.opensearch.ml.engine.algorithms.clustering;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.opensearch.ml.common.dataframe.DataFrame;
 import org.opensearch.ml.common.parameter.KMeansParams;
 import org.opensearch.ml.common.parameter.FunctionName;
 import org.opensearch.ml.common.parameter.MLPredictionOutput;
 import org.opensearch.ml.common.parameter.Model;
-import org.opensearch.ml.engine.algorithms.clustering.KMeans;
 
 import static org.opensearch.ml.engine.helper.KMeansHelper.constructKMeansDataFrame;
 
 
 public class KMeansTest {
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
     private KMeansParams parameters;
     private KMeans kMeans;
     private DataFrame trainDataFrame;
@@ -49,11 +52,58 @@ public class KMeansTest {
     }
 
     @Test
+    public void predictWithNullModel() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("No model found for KMeans prediction");
+        kMeans.predict(predictionDataFrame, null);
+    }
+
+    @Test
     public void train() {
         Model model = kMeans.train(trainDataFrame);
         Assert.assertEquals(FunctionName.KMEANS.name(), model.getName());
         Assert.assertEquals(1, model.getVersion());
         Assert.assertNotNull(model.getContent());
+    }
+
+    @Test
+    public void trainAndPredict() {
+        KMeansParams parameters = KMeansParams.builder()
+                .distanceType(KMeansParams.DistanceType.EUCLIDEAN)
+                .iterations(10)
+                .centroids(2)
+                .build();
+        KMeans kMeans = new KMeans(parameters);
+        MLPredictionOutput output = (MLPredictionOutput) kMeans.trainAndPredict(trainDataFrame);
+        DataFrame predictions = output.getPredictionResult();
+        Assert.assertEquals(trainSize, predictions.size());
+        predictions.forEach(row -> Assert.assertTrue(row.getValue(0).intValue() == 0 || row.getValue(0).intValue() == 1));
+
+        parameters = parameters.toBuilder().distanceType(KMeansParams.DistanceType.COSINE).build();
+        kMeans = new KMeans(parameters);
+        output = (MLPredictionOutput) kMeans.trainAndPredict(trainDataFrame);
+        predictions = output.getPredictionResult();
+        Assert.assertEquals(trainSize, predictions.size());
+
+        parameters = parameters.toBuilder().distanceType(KMeansParams.DistanceType.L1).build();
+        kMeans = new KMeans(parameters);
+        output = (MLPredictionOutput) kMeans.trainAndPredict(trainDataFrame);
+        predictions = output.getPredictionResult();
+        Assert.assertEquals(trainSize, predictions.size());
+    }
+
+    @Test
+    public void constructorWithNegtiveCentroids() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("K should be positive");
+        new KMeans(KMeansParams.builder().centroids(-1).build());
+    }
+
+    @Test
+    public void constructorWithNegtiveIterations() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Iterations should be positive");
+        new KMeans(KMeansParams.builder().iterations(-1).build());
     }
 
     private void constructKMeansPredictionDataFrame() {
