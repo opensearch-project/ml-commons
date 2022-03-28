88ee33f79ff9466838a794169c3431c0424bd9f9
Yaliang Wu <ylwu@amazon.com>
2022-03-28 18:09:12 +0000

    use protostuff to serialize/deserialize RCF model (#252)
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/action/prediction/PredictionITTests.java
+++ plugin/src/test/java/org/opensearch/ml/action/prediction/PredictionITTests.java
@@ -6,6 +6,10 @@
 package org.opensearch.ml.action.prediction;
 
 import static org.opensearch.ml.utils.TestData.IRIS_DATA_SIZE;
+import static org.opensearch.ml.utils.TestData.TIME_FIELD;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.lucene.util.LuceneTestCase;
 import org.junit.Before;
@@ -14,9 +18,17 @@ import org.junit.rules.ExpectedException;
 import org.opensearch.action.ActionFuture;
 import org.opensearch.action.ActionRequestValidationException;
 import org.opensearch.ml.action.MLCommonsIntegTestCase;
+import org.opensearch.ml.common.dataframe.ColumnMeta;
+import org.opensearch.ml.common.dataframe.ColumnType;
+import org.opensearch.ml.common.dataframe.ColumnValue;
+import org.opensearch.ml.common.dataframe.DataFrame;
+import org.opensearch.ml.common.dataframe.DefaultDataFrame;
+import org.opensearch.ml.common.dataframe.DoubleValue;
+import org.opensearch.ml.common.dataframe.Row;
 import org.opensearch.ml.common.dataset.DataFrameInputDataset;
 import org.opensearch.ml.common.dataset.MLInputDataset;
 import org.opensearch.ml.common.dataset.SearchQueryInputDataset;
+import org.opensearch.ml.common.parameter.FitRCFParams;
 import org.opensearch.ml.common.parameter.FunctionName;
 import org.opensearch.ml.common.parameter.MLInput;
 import org.opensearch.ml.common.parameter.MLModel;
@@ -33,6 +45,8 @@ public class PredictionITTests extends MLCommonsIntegTestCase {
     private String irisIndexName;
     private String kMeansModelId;
     private String batchRcfModelId;
+    private String fitRcfModelId;
+    private String linearRegressionModelId;
     private int batchRcfDataSize = 100;
 
     @Rule
@@ -50,6 +64,8 @@ public class PredictionITTests extends MLCommonsIntegTestCase {
         // assertNotNull(kMeansModel);
 
         batchRcfModelId = trainBatchRCFWithDataFrame(500, false);
+        fitRcfModelId = trainFitRCFWithDataFrame(500, false);
+        linearRegressionModelId = trainLinearRegressionWithDataFrame(100, false);
         MLModel batchRcfModel = getModel(batchRcfModelId);
         assertNotNull(batchRcfModel);
     }
@@ -57,13 +73,13 @@ public class PredictionITTests extends MLCommonsIntegTestCase {
     @LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/oracle/tribuo/issues/223")
     public void testPredictionWithSearchInput_KMeans() {
         MLInputDataset inputDataset = new SearchQueryInputDataset(ImmutableList.of(irisIndexName), irisDataQuery());
-        predictAndVerify(kMeansModelId, inputDataset, FunctionName.KMEANS, IRIS_DATA_SIZE);
+        predictAndVerify(kMeansModelId, inputDataset, FunctionName.KMEANS, null, IRIS_DATA_SIZE);
     }
 
     @LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/oracle/tribuo/issues/223")
     public void testPredictionWithDataInput_KMeans() {
         MLInputDataset inputDataset = new DataFrameInputDataset(irisDataFrame());
-        predictAndVerify(kMeansModelId, inputDataset, FunctionName.KMEANS, IRIS_DATA_SIZE);
+        predictAndVerify(kMeansModelId, inputDataset, FunctionName.KMEANS, null, IRIS_DATA_SIZE);
     }
 
     @LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/oracle/tribuo/issues/223")
@@ -89,6 +105,36 @@ public class PredictionITTests extends MLCommonsIntegTestCase {
 
     public void testPredictionWithDataFrame_BatchRCF() {
         MLInputDataset inputDataset = new DataFrameInputDataset(TestData.constructTestDataFrame(batchRcfDataSize));
-        predictAndVerify(batchRcfModelId, inputDataset, FunctionName.BATCH_RCF, batchRcfDataSize);
+        predictAndVerify(batchRcfModelId, inputDataset, FunctionName.BATCH_RCF, null, batchRcfDataSize);
+    }
+
+    public void testPredictionWithDataFrame_FitRCF() {
+        MLInputDataset inputDataset = new DataFrameInputDataset(TestData.constructTestDataFrame(batchRcfDataSize, true));
+        predictAndVerify(
+            fitRcfModelId,
+            inputDataset,
+            FunctionName.FIT_RCF,
+            FitRCFParams.builder().timeField(TIME_FIELD).build(),
+            batchRcfDataSize
+        );
+    }
+
+    public void testPredictionWithDataFrame_LinearRegression() {
+        int size = 1;
+        int feet = 20;
+        ColumnMeta[] columnMetas = new ColumnMeta[] { new ColumnMeta("feet", ColumnType.DOUBLE) };
+        List<Row> rows = new ArrayList<>();
+        rows.add(new Row(new ColumnValue[] { new DoubleValue(feet) }));
+        DataFrame inputDataFrame = new DefaultDataFrame(columnMetas, rows);
+        MLInputDataset inputDataset = new DataFrameInputDataset(inputDataFrame);
+        DataFrame dataFrame = predictAndVerify(
+            linearRegressionModelId,
+            inputDataset,
+            FunctionName.LINEAR_REGRESSION,
+            getLinearRegressionParams(),
+            size
+        );
+        ColumnValue value = dataFrame.getRow(0).getValue(0);
+        assertNotNull(value);
     }
 }
