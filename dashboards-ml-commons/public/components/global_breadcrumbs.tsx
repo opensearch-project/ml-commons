8b6e2bbcc04e2db4022b145fb40fd8380bafc94c
Lin Wang <wonglam@amazon.com>
2022-09-09 17:57:08 +0800

    hide no label breadcrumb
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- dashboards-ml-commons/public/components/global_breadcrumbs.tsx
+++ dashboards-ml-commons/public/components/global_breadcrumbs.tsx
@@ -6,15 +6,13 @@ import { ROUTES } from '../../common/router';
 import { PLUGIN_NAME } from '../../common';
 
 const getBreadcrumbs = (pathname: string, basename: string) => {
-  const topBreadcrumb: ChromeBreadcrumb = { text: PLUGIN_NAME };
-  const breadcrumbs: ChromeBreadcrumb[] = [topBreadcrumb];
+  const breadcrumbs: ChromeBreadcrumb[] = [{ text: PLUGIN_NAME, href: basename }];
   const matchedRoute = ROUTES.find((route) =>
     matchPath(pathname, { path: route.path, exact: route.exact })
   );
-  if (pathname === '/' || !matchedRoute) {
+  if (!matchedRoute?.label) {
     return breadcrumbs;
   }
-  topBreadcrumb.href = basename;
   return breadcrumbs.concat({
     text: matchedRoute.label,
   });
