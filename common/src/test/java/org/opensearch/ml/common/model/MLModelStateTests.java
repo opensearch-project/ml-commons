91fd0773d4a7eea544167144c260ceea0c650905
Yaliang Wu <ylwu@amazon.com>
2022-10-14 01:36:40 +0000

    tune ML model to support custom model (#471)
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

---
+++ common/src/test/java/org/opensearch/ml/common/model/MLModelStateTests.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.model;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class MLModelStateTests {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void from() {
+        MLModelState modelState = MLModelState.from("UPLOADED");
+        assertEquals(MLModelState.UPLOADED, modelState);
+    }
+
+    @Test
+    public void from_wrongValue() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Wrong model format");
+        MLModelState.from("test_wrong_value");
+    }
+}
