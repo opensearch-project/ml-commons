6a417e61191901c86eebacab040e826a5bc3c0bf
Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
2022-11-03 14:23:37 -0700

    Add test coverage to common package (#514)
    * add test coverage to common package
    
    Signed-off-by: Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
    
    * improve unit test coverage for forward, load, unload and upload class
    
    Signed-off-by: Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
    
    * improve unit test coverage for sync, training, unload and load class
    
    Signed-off-by: Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
    
    * code improvement after review
    
    Signed-off-by: Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
    
    Signed-off-by: Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
    (cherry picked from commit 6f47975b098aa65a78a5aff90caf1e478babab32)

---
+++ common/src/test/java/org/opensearch/ml/common/transport/sync/MLSyncUpNodeResponseTest.java
@@ -0,0 +1,64 @@
+package org.opensearch.ml.common.transport.sync;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opensearch.Version;
+import org.opensearch.cluster.node.DiscoveryNode;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.common.transport.TransportAddress;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+import static org.opensearch.cluster.node.DiscoveryNodeRole.CLUSTER_MANAGER_ROLE;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MLSyncUpNodeResponseTest {
+
+    private DiscoveryNode localNode;
+    private final String modelStatus = "modelStatus";
+    private final String[] loadedModelIds = {"loadedModelIds"};
+    private final String[] runningLoadModelTaskIds = {"runningLoadModelTaskIds"};
+    @Before
+    public void setUp() throws Exception {
+        localNode = new DiscoveryNode(
+                "foo0",
+                "foo0",
+                new TransportAddress(InetAddress.getLoopbackAddress(), 9300),
+                Collections.emptyMap(),
+                Collections.singleton(CLUSTER_MANAGER_ROLE),
+                Version.CURRENT
+        );
+    }
+
+    @Test
+    public void testSerializationDeserialization() throws IOException {
+        MLSyncUpNodeResponse response = new MLSyncUpNodeResponse(localNode, modelStatus, loadedModelIds, runningLoadModelTaskIds);
+        BytesStreamOutput output = new BytesStreamOutput();
+        response.writeTo(output);
+        MLSyncUpNodeResponse newResponse = new MLSyncUpNodeResponse(output.bytes().streamInput());
+        assertNotEquals(newResponse, response);
+        assertEquals(newResponse.getNode().getName(), response.getNode().getName());
+        assertEquals(newResponse.getModelStatus(), response.getModelStatus());
+        assertArrayEquals(newResponse.getLoadedModelIds(), response.getLoadedModelIds());
+        assertArrayEquals(newResponse.getRunningLoadModelTaskIds(), response.getRunningLoadModelTaskIds());
+    }
+
+    @Test
+    public void testReadProfile() throws IOException {
+        MLSyncUpNodeResponse response = new MLSyncUpNodeResponse(localNode, modelStatus, loadedModelIds, runningLoadModelTaskIds);
+        BytesStreamOutput output = new BytesStreamOutput();
+        response.writeTo(output);
+        MLSyncUpNodeResponse newResponse = MLSyncUpNodeResponse.readStats(output.bytes().streamInput());
+        assertNotEquals(newResponse, response);
+        assertEquals(newResponse.getNode().getName(), response.getNode().getName());
+        assertEquals(newResponse.getModelStatus(), response.getModelStatus());
+        assertArrayEquals(newResponse.getLoadedModelIds(), response.getLoadedModelIds());
+        assertArrayEquals(newResponse.getRunningLoadModelTaskIds(), response.getRunningLoadModelTaskIds());
+
+    }
+}
