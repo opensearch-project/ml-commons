ab702a2f4462fb90fef21daafb0a469cd01eff83
Sicheng Song <114637679+b4sjoo@users.noreply.github.com>
2022-11-03 19:19:36 -0700

    Update plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
    Co-authored-by: Yaliang Wu <ylwu@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
+++ plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
@@ -19,7 +19,7 @@ public final class MLCommonsSettings {
     public static final Setting<Integer> ML_COMMONS_MAX_LOAD_MODEL_TASKS_PER_NODE = Setting
         .intSetting("plugins.ml_commons.max_load_model_tasks_per_node", 10, 0, 10, Setting.Property.NodeScope, Setting.Property.Dynamic);
     public static final Setting<Integer> ML_COMMONS_MAX_ML_TASK_PER_NODE = Setting
-        .intSetting("plugins.ml_commons.max_ml_task_per_node", 10, 0, 10, Setting.Property.NodeScope, Setting.Property.Dynamic);
+        .intSetting("plugins.ml_commons.max_ml_task_per_node", 10, 0, 10000, Setting.Property.NodeScope, Setting.Property.Dynamic);
     public static final Setting<Boolean> ML_COMMONS_ONLY_RUN_ON_ML_NODE = Setting
         .boolSetting("plugins.ml_commons.only_run_on_ml_node", false, Setting.Property.NodeScope, Setting.Property.Dynamic);
     public static final Setting<Integer> ML_COMMONS_SYNC_UP_JOB_INTERVAL_IN_SECONDS = Setting
