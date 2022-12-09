b2f18e4d6eccc4fb6b74ca5df0f0ebab9d940b93
wujunshen <frank_wjs@hotmail.com>
2022-12-09 15:47:08 +0800

    [wjunshen] #ML-Commons Model auto reload feat: add 2 properties about model auto reload
    Signed-off-by: wujunshen <frank_wjs@hotmail.com>

--- plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
+++ plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
@@ -9,47 +9,67 @@ import org.opensearch.common.settings.Setting;
 
 public final class MLCommonsSettings {
 
-    private MLCommonsSettings() {}
+	private MLCommonsSettings() {
+	}
 
-    public static final Setting<String> ML_COMMONS_TASK_DISPATCH_POLICY = Setting
-        .simpleString("plugins.ml_commons.task_dispatch_policy", "round_robin", Setting.Property.NodeScope, Setting.Property.Dynamic);
+	public static final Setting<String> ML_COMMONS_TASK_DISPATCH_POLICY = Setting
+			.simpleString("plugins.ml_commons.task_dispatch_policy", "round_robin",
+					Setting.Property.NodeScope, Setting.Property.Dynamic);
 
-    public static final Setting<Integer> ML_COMMONS_MAX_MODELS_PER_NODE = Setting
-        .intSetting("plugins.ml_commons.max_model_on_node", 10, 0, 1000, Setting.Property.NodeScope, Setting.Property.Dynamic);
-    public static final Setting<Integer> ML_COMMONS_MAX_LOAD_MODEL_TASKS_PER_NODE = Setting
-        .intSetting("plugins.ml_commons.max_load_model_tasks_per_node", 10, 0, 10, Setting.Property.NodeScope, Setting.Property.Dynamic);
-    public static final Setting<Integer> ML_COMMONS_MAX_ML_TASK_PER_NODE = Setting
-        .intSetting("plugins.ml_commons.max_ml_task_per_node", 10, 0, 10000, Setting.Property.NodeScope, Setting.Property.Dynamic);
-    public static final Setting<Boolean> ML_COMMONS_ONLY_RUN_ON_ML_NODE = Setting
-        .boolSetting("plugins.ml_commons.only_run_on_ml_node", false, Setting.Property.NodeScope, Setting.Property.Dynamic);
-    public static final Setting<Integer> ML_COMMONS_SYNC_UP_JOB_INTERVAL_IN_SECONDS = Setting
-        .intSetting(
-            "plugins.ml_commons.sync_up_job_interval_in_seconds",
-            10,
-            0,
-            86400,
-            Setting.Property.NodeScope,
-            Setting.Property.Dynamic
-        );
+	public static final Setting<Integer> ML_COMMONS_MAX_MODELS_PER_NODE = Setting
+			.intSetting("plugins.ml_commons.max_model_on_node", 10, 0, 1000, Setting.Property.NodeScope,
+					Setting.Property.Dynamic);
+	public static final Setting<Integer> ML_COMMONS_MAX_LOAD_MODEL_TASKS_PER_NODE = Setting
+			.intSetting("plugins.ml_commons.max_load_model_tasks_per_node", 10, 0, 10,
+					Setting.Property.NodeScope, Setting.Property.Dynamic);
+	public static final Setting<Integer> ML_COMMONS_MAX_ML_TASK_PER_NODE = Setting
+			.intSetting("plugins.ml_commons.max_ml_task_per_node", 10, 0, 10000,
+					Setting.Property.NodeScope, Setting.Property.Dynamic);
+	public static final Setting<Boolean> ML_COMMONS_ONLY_RUN_ON_ML_NODE = Setting
+			.boolSetting("plugins.ml_commons.only_run_on_ml_node", false, Setting.Property.NodeScope,
+					Setting.Property.Dynamic);
+	public static final Setting<Integer> ML_COMMONS_SYNC_UP_JOB_INTERVAL_IN_SECONDS = Setting
+			.intSetting(
+					"plugins.ml_commons.sync_up_job_interval_in_seconds",
+					10,
+					0,
+					86400,
+					Setting.Property.NodeScope,
+					Setting.Property.Dynamic
+			);
 
-    public static final Setting<Long> ML_COMMONS_MONITORING_REQUEST_COUNT = Setting
-        .longSetting(
-            "plugins.ml_commons.monitoring_request_count",
-            100,
-            0,
-            10_000_000,
-            Setting.Property.NodeScope,
-            Setting.Property.Dynamic
-        );
+	public static final Setting<Long> ML_COMMONS_MONITORING_REQUEST_COUNT = Setting
+			.longSetting(
+					"plugins.ml_commons.monitoring_request_count",
+					100,
+					0,
+					10_000_000,
+					Setting.Property.NodeScope,
+					Setting.Property.Dynamic
+			);
 
-    public static final Setting<Integer> ML_COMMONS_MAX_UPLOAD_TASKS_PER_NODE = Setting
-        .intSetting("plugins.ml_commons.max_upload_model_tasks_per_node", 10, 0, 10, Setting.Property.NodeScope, Setting.Property.Dynamic);
+	public static final Setting<Integer> ML_COMMONS_MAX_UPLOAD_TASKS_PER_NODE = Setting
+			.intSetting("plugins.ml_commons.max_upload_model_tasks_per_node", 10, 0, 10,
+					Setting.Property.NodeScope, Setting.Property.Dynamic);
 
-    public static final Setting<String> ML_COMMONS_TRUSTED_URL_REGEX = Setting
-        .simpleString(
-            "plugins.ml_commons.trusted_url_regex",
-            "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
-            Setting.Property.NodeScope,
-            Setting.Property.Dynamic
-        );
+	public static final Setting<String> ML_COMMONS_TRUSTED_URL_REGEX = Setting
+			.simpleString(
+					"plugins.ml_commons.trusted_url_regex",
+					"^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
+					Setting.Property.NodeScope,
+					Setting.Property.Dynamic
+			);
+
+	public static final Setting<Boolean> ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE = Setting
+			.boolSetting(
+					"plugins.ml_commons.model.autoreload.enable",
+					false, Setting.Property.NodeScope
+					, Setting.Property.Dynamic);
+
+	public static final Setting<Integer> ML_COMMONS_MODEL_AUTO_RELOAD_MAX_RETRY = Setting
+			.intSetting(
+					"plugins.ml_commons.model.autoreload.max.retry",
+					3, 0, 3, Setting.Property.NodeScope
+					, Setting.Property.Dynamic);
+
 }
