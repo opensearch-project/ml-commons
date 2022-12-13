f9afb370a2ec7d08c2066ca6352696b060f7513c
wujunshen <frank_wjs@hotmail.com>
2022-12-13 21:27:45 +0800

    [wjunshen] #N/A feat: finish the function: get all loaded model id from one node id

--- plugin/src/main/java/org/opensearch/ml/model/MLModelAndNodeManager.java
+++ plugin/src/main/java/org/opensearch/ml/model/MLModelAndNodeManager.java
@@ -4,27 +4,42 @@
  */
 package org.opensearch.ml.model;
 
+import static org.opensearch.common.xcontent.XContentParserUtils.ensureExpectedToken;
+import static org.opensearch.ml.common.CommonValue.ML_TASK_INDEX;
 import static org.opensearch.ml.settings.MLCommonsSettings.ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE;
 import static org.opensearch.ml.settings.MLCommonsSettings.ML_COMMONS_MODEL_AUTO_RELOAD_MAX_RETRY;
+import static org.opensearch.ml.utils.MLNodeUtils.createXContentParserFromRegistry;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
+import org.opensearch.action.ActionListener;
+import org.opensearch.action.search.SearchRequest;
 import org.opensearch.client.Client;
 import org.opensearch.cluster.node.DiscoveryNode;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.CollectionUtils;
+import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.common.xcontent.NamedXContentRegistry;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.index.IndexNotFoundException;
 import org.opensearch.ml.cluster.DiscoveryNodeHelper;
 import org.opensearch.ml.common.FunctionName;
+import org.opensearch.ml.common.MLTask;
+import org.opensearch.ml.common.MLTaskState;
+import org.opensearch.ml.common.MLTaskType;
 import org.opensearch.ml.common.breaker.MLCircuitBreakerService;
+import org.opensearch.ml.common.exception.MLResourceNotFoundException;
 import org.opensearch.ml.engine.MLEngine;
 import org.opensearch.ml.engine.ModelHelper;
 import org.opensearch.ml.indices.MLIndicesHandler;
 import org.opensearch.ml.stats.MLStats;
 import org.opensearch.ml.task.MLTaskManager;
 import org.opensearch.ml.utils.MLNodeUtils;
+import org.opensearch.search.SearchHit;
+import org.opensearch.search.SearchHits;
 import org.opensearch.threadpool.ThreadPool;
 
 /**
@@ -32,76 +47,80 @@ import org.opensearch.threadpool.ThreadPool;
  */
 @Log4j2
 public class MLModelAndNodeManager {
-    public static final int TIMEOUT_IN_MILLIS = 5000;
 
-    private final Client client;
-    private final ClusterService clusterService;
-    private final ThreadPool threadPool;
-    private final NamedXContentRegistry xContentRegistry;
-    private final ModelHelper modelHelper;
-    private final DiscoveryNodeHelper nodeHelper;
-    private final MLModelCacheHelper modelCacheHelper;
-    private final MLStats mlStats;
-    private final MLCircuitBreakerService mlCircuitBreakerService;
-    private final MLIndicesHandler mlIndicesHandler;
-    private final MLTaskManager mlTaskManager;
-    private final MLModelManager mlModelManager;
-    private final MLEngine mlEngine;
+	public static final int TIMEOUT_IN_MILLIS = 5000;
 
-    private volatile Boolean enableAutoReLoadModel;
+	private final Client client;
+	private final ClusterService clusterService;
+	private final ThreadPool threadPool;
+	private final NamedXContentRegistry xContentRegistry;
+	private final ModelHelper modelHelper;
+	private final DiscoveryNodeHelper nodeHelper;
+	private final MLModelCacheHelper modelCacheHelper;
+	private final MLStats mlStats;
+	private final MLCircuitBreakerService mlCircuitBreakerService;
+	private final MLIndicesHandler mlIndicesHandler;
+	private final MLTaskManager mlTaskManager;
+	private final MLModelManager mlModelManager;
+	private final MLEngine mlEngine;
 
-    private volatile Integer maxRetryTimeAutoReLoadModel;
+	private volatile Boolean enableAutoReLoadModel;
 
-    public MLModelAndNodeManager(
-        ClusterService clusterService,
-        Client client,
-        ThreadPool threadPool,
-        NamedXContentRegistry xContentRegistry,
-        ModelHelper modelHelper,
-        DiscoveryNodeHelper nodeHelper,
-        Settings settings,
-        MLStats mlStats,
-        MLCircuitBreakerService mlCircuitBreakerService,
-        MLIndicesHandler mlIndicesHandler,
-        MLTaskManager mlTaskManager,
-        MLModelManager mlModelManager,
-        MLModelCacheHelper modelCacheHelper,
-        MLEngine mlEngine
-    ) {
-        this.clusterService = clusterService;
-        this.client = client;
-        this.threadPool = threadPool;
-        this.xContentRegistry = xContentRegistry;
-        this.modelHelper = modelHelper;
-        this.nodeHelper = nodeHelper;
-        this.modelCacheHelper = modelCacheHelper;
-        this.mlStats = mlStats;
-        this.mlCircuitBreakerService = mlCircuitBreakerService;
-        this.mlIndicesHandler = mlIndicesHandler;
-        this.mlTaskManager = mlTaskManager;
-        this.mlModelManager = mlModelManager;
-        this.mlEngine = mlEngine;
+	private volatile Integer maxRetryTimeAutoReLoadModel;
 
-        enableAutoReLoadModel = ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE.get(settings);
-        clusterService
-            .getClusterSettings()
-            .addSettingsUpdateConsumer(ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE, it -> enableAutoReLoadModel = it);
+	public MLModelAndNodeManager(
+			ClusterService clusterService,
+			Client client,
+			ThreadPool threadPool,
+			NamedXContentRegistry xContentRegistry,
+			ModelHelper modelHelper,
+			DiscoveryNodeHelper nodeHelper,
+			Settings settings,
+			MLStats mlStats,
+			MLCircuitBreakerService mlCircuitBreakerService,
+			MLIndicesHandler mlIndicesHandler,
+			MLTaskManager mlTaskManager,
+			MLModelManager mlModelManager,
+			MLModelCacheHelper modelCacheHelper,
+			MLEngine mlEngine
+	) {
+		this.clusterService = clusterService;
+		this.client = client;
+		this.threadPool = threadPool;
+		this.xContentRegistry = xContentRegistry;
+		this.modelHelper = modelHelper;
+		this.nodeHelper = nodeHelper;
+		this.modelCacheHelper = modelCacheHelper;
+		this.mlStats = mlStats;
+		this.mlCircuitBreakerService = mlCircuitBreakerService;
+		this.mlIndicesHandler = mlIndicesHandler;
+		this.mlTaskManager = mlTaskManager;
+		this.mlModelManager = mlModelManager;
+		this.mlEngine = mlEngine;
 
-        maxRetryTimeAutoReLoadModel = ML_COMMONS_MODEL_AUTO_RELOAD_MAX_RETRY.get(settings);
-        clusterService
-            .getClusterSettings()
-            .addSettingsUpdateConsumer(ML_COMMONS_MODEL_AUTO_RELOAD_MAX_RETRY, it -> maxRetryTimeAutoReLoadModel = it);
-    }
+		enableAutoReLoadModel = ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE.get(settings);
+		clusterService
+				.getClusterSettings()
+				.addSettingsUpdateConsumer(ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE,
+						it -> enableAutoReLoadModel = it);
 
-    public void autoReLoadModel() {
-        log.debug("enableAutoReLoadModel {} maxRetryTimeAutoReLoadModel {}", enableAutoReLoadModel, maxRetryTimeAutoReLoadModel);
+		maxRetryTimeAutoReLoadModel = ML_COMMONS_MODEL_AUTO_RELOAD_MAX_RETRY.get(settings);
+		clusterService
+				.getClusterSettings()
+				.addSettingsUpdateConsumer(ML_COMMONS_MODEL_AUTO_RELOAD_MAX_RETRY,
+						it -> maxRetryTimeAutoReLoadModel = it);
+	}
 
-        // 如果不需要自动reload，什么都不做直接返回
-        if (!enableAutoReLoadModel) {
-            return;
-        }
+	public void autoReLoadModel() {
+		log.debug("enableAutoReLoadModel {} maxRetryTimeAutoReLoadModel {}", enableAutoReLoadModel,
+				maxRetryTimeAutoReLoadModel);
 
-        doAutoReLoadModel();
+		// 如果不需要自动reload，什么都不做直接返回
+		if (!enableAutoReLoadModel) {
+			return;
+		}
+
+		doAutoReLoadModel();
 //        还没确定是否要做重试retry
 //        Callable<Void> callable = () -> {
 //            doAutoReLoadModel();
@@ -122,79 +141,107 @@ public class MLModelAndNodeManager {
 //        } catch (RetryException | ExecutionException e) { // 重试次数超过阈值或被强制中断
 //            throw new RuntimeException("retry max time, always failure", e);
 //        }
-    }
+	}
 
-    private void doAutoReLoadModel() {
-        log.debug(Thread.currentThread().getName() + " -->model auto reload is not finished yet...");
+	private void doAutoReLoadModel() {
+		log.debug(Thread.currentThread().getName() + " -->model auto reload is not finished yet...");
 
-        // 先获取所有的node id，包括已经挂的节点
-        String[] allNodeIds = nodeHelper.getAllNodeIds();
-        log.debug("all node ids are: {}", allNodeIds);
+		// 先获取所有的node id，包括已经挂的节点
+		String[] allNodeIds = nodeHelper.getAllNodeIds();
+		log.debug("all node ids are: {}", allNodeIds);
 
-        // 再获取所有合法节点，即没有挂掉的节点
-        String[] eligibleNodeIds = nodeHelper.getEligibleNodeIds();
-        log.debug("eligibleNode ids are: {}", eligibleNodeIds);
+		// 再获取所有合法节点，即没有挂掉的节点
+		String[] eligibleNodeIds = nodeHelper.getEligibleNodeIds();
+		log.debug("eligibleNode ids are: {}", eligibleNodeIds);
 
-        // 上面两个数组比较获取属于集群但不合法节点， 然后获取这些节点的model deployment plan，依次把这些model一个个重新load
-        String[] needReLoadModelNodeIds = null;
-        if (allNodeIds.length != eligibleNodeIds.length) {  // 说明有挂掉的节点
-            needReLoadModelNodeIds = Arrays.stream(allNodeIds).filter(m -> !List.of(eligibleNodeIds).contains(m)).toArray(String[]::new);
-        }
+		// 上面两个数组比较获取属于集群但不合法节点， 然后获取这些节点的model deployment plan，依次把这些model一个个重新load
+		String[] needReLoadModelNodeIds = null;
+		if (allNodeIds.length != eligibleNodeIds.length) {  // 说明有挂掉的节点
+			needReLoadModelNodeIds = Arrays.stream(allNodeIds)
+					.filter(m -> !List.of(eligibleNodeIds).contains(m)).toArray(String[]::new);
+		}
 
-        // 如果没有，说明集群中所有节点都没有挂掉，都在正常运行，则什么都不需做
-        if (CollectionUtils.isEmpty(needReLoadModelNodeIds)) {
-            log.debug("all nodes run successfully");
-            return;
-        }
+		// 如果没有，说明集群中所有节点都没有挂掉，都在正常运行，则什么都不需做
+		if (CollectionUtils.isEmpty(needReLoadModelNodeIds)) {
+			log.debug("all nodes run successfully");
+			return;
+		}
 
-        for (String needReLoadModelNodeId : needReLoadModelNodeIds) {
-            DiscoveryNode node = nodeHelper.getNode(needReLoadModelNodeId);
-            if (MLNodeUtils.isMLNode(node)) {
-                String[] modelIds = getAllModelIdsByNode(needReLoadModelNodeId);
-                loadAllModelsByNode(needReLoadModelNodeId, modelIds, FunctionName.TEXT_EMBEDDING);
-            }
-        }
-    }
+		for (String needReLoadModelNodeId : needReLoadModelNodeIds) {
+			DiscoveryNode node = nodeHelper.getNode(needReLoadModelNodeId);
+			if (MLNodeUtils.isMLNode(node)) {
+				String[] modelIds;
+				try {
+					modelIds = getAllLoadedModelIdsByNode(needReLoadModelNodeId);
+				} catch (Exception e) {
+					log.error("Failed to get all loaded model ids from node id {}, the reason is: {}",
+							needReLoadModelNodeId, e);
+					throw new RuntimeException(e);
+				}
+				loadAllModelsByNode(needReLoadModelNodeId, modelIds, FunctionName.TEXT_EMBEDDING);
+			}
+		}
+	}
 
-    private String[] getAllModelIdsByNode(String nodeId) {
-        // GetRequest getRequest = new GetRequest(ML_TASK_INDEX);
-        //// client.searchTask(new SearchRequest()).actionGet()
-        // try (ThreadContext.StoredContext context = client.threadPool().getThreadContext()
-        // .stashContext()) {
-        // client.get(getRequest, ActionListener.wrap(response -> {
-        // if (response != null && response.isExists()) {
-        // try (XContentParser parser = createXContentParserFromRegistry(xContentRegistry,
-        // response.getSourceAsBytesRef())) {
-        // ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
-        // MLTask mlTask = MLTask.parse(parser);
-        //
-        // actionListener.onResponse(MLTaskGetResponse.builder().mlTask(mlTask).build());
-        // } catch (Exception e) {
-        // log.error("Failed to parse ml task" + response.getId(), e);
-        // actionListener.onFailure(e);
-        // }
-        // } else {
-        // actionListener.onFailure(
-        // new MLResourceNotFoundException("Failed to find ML index " + ML_TASK_INDEX));
-        // }
-        // }, e -> {
-        // if (e instanceof IndexNotFoundException) {
-        // log.error("Failed to find ML index " + ML_TASK_INDEX, e);
-        // actionListener.onFailure(new MLResourceNotFoundException(e));
-        // } else {
-        // log.error("Failed to get ML index " + ML_TASK_INDEX, e);
-        // actionListener.onFailure(e);
-        // }
-        // }));
-        // } catch (Exception e) {
-        // log.error("Failed to get ML index " + ML_TASK_INDEX, e);
-        // actionListener.onFailure(e);
-        // }
-        return new String[] {};
-    }
+	private void loadAllModelsByNode(String nodeId, String[] modelIds, FunctionName functionName) {
 
-    private void loadAllModelsByNode(String nodeId, String[] modelIds, FunctionName functionName) {
+		return;
+	}
 
-        return;
-    }
+	private String[] getAllLoadedModelIdsByNode(String nodeId) {
+		List<String> result = new ArrayList<>();
+		SearchRequest searchRequest = new SearchRequest(ML_TASK_INDEX);
+
+		try (ThreadContext.StoredContext context = client.threadPool().getThreadContext()
+				.stashContext()) {
+			client.search(searchRequest, ActionListener.wrap(response -> {
+				if (response == null) {
+					return;
+				}
+				SearchHits searchHits = response.getHits();
+				if (searchHits == null) {
+					return;
+				}
+				SearchHit[] hits = searchHits.getHits();
+				if (CollectionUtils.isEmpty(hits)) {
+					return;
+				}
+
+				for (SearchHit hit : hits) {
+					try (XContentParser parser = createXContentParserFromRegistry(xContentRegistry,
+							hit.getSourceRef())) {
+						ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
+						MLTask mlTask = MLTask.parse(parser);
+
+						String workerNode = mlTask.getWorkerNode();
+						MLTaskType mlTaskType = mlTask.getTaskType();
+						MLTaskState mlTaskState = mlTask.getState();
+
+						if (workerNode.contains(nodeId) && mlTaskType == MLTaskType.LOAD_MODEL
+								&& mlTaskState == MLTaskState.COMPLETED) {
+							result.add(mlTask.getModelId());
+						}
+					} catch (Exception e) {
+						log.error("ML Task id {} failed to parse, the reason is: {}", hit.getId(), e);
+						throw new RuntimeException(e);
+					}
+				}
+			}, e -> {
+				if (e instanceof IndexNotFoundException) {
+					log.error("Index {} not found, the reason is: {}", ML_TASK_INDEX, e);
+					throw new IndexNotFoundException(ML_TASK_INDEX, e);
+				} else {
+					log.error("Resource from Index {} not found, the reason is: {}", ML_TASK_INDEX, e);
+					throw new MLResourceNotFoundException(e);
+				}
+			}));
+		} catch (Exception e) {
+			log.error("Failed to get ML model from node {}, the reason is: {}", nodeId, e);
+			throw new RuntimeException(e);
+		}
+
+		return result.toArray(String[]::new);
+	}
+
+
 }
