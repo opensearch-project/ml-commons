a9f4cb1d0ce010cef03c101a94980d676eec3010
wujunshen <frank_wjs@hotmail.com>
2022-12-29 06:02:58 +0800

    [wjunshen] #N/A feat: model auto reload
    Signed-off-by: wujunshen <frank_wjs@hotmail.com>

--- common/src/main/java/org/opensearch/ml/common/CommonValue.java
+++ common/src/main/java/org/opensearch/ml/common/CommonValue.java
@@ -9,9 +9,6 @@ import static org.opensearch.ml.common.model.MLModelConfig.ALL_CONFIG_FIELD;
 import static org.opensearch.ml.common.model.MLModelConfig.MODEL_TYPE_FIELD;
 import static org.opensearch.ml.common.model.TextEmbeddingModelConfig.EMBEDDING_DIMENSION_FIELD;
 import static org.opensearch.ml.common.model.TextEmbeddingModelConfig.FRAMEWORK_TYPE_FIELD;
-import static org.opensearch.ml.common.model.TextEmbeddingModelConfig.MODEL_MAX_LENGTH_FIELD;
-import static org.opensearch.ml.common.model.TextEmbeddingModelConfig.NORMALIZE_RESULT_FIELD;
-import static org.opensearch.ml.common.model.TextEmbeddingModelConfig.POOLING_METHOD_FIELD;
 
 public class CommonValue {
 
@@ -29,132 +26,134 @@ public class CommonValue {
     public static String WARM_BOX_TYPE = "warm";
 
     public static final String ML_MODEL_INDEX = ".plugins-ml-model";
+    public static final String ML_MODEL_RELOAD_INDEX = ".plugins-ml-model-reload";
+    public static final Integer ML_MODEL_RELOAD_MAX_RETRY_TIMES = 2;
     public static final String ML_TASK_INDEX = ".plugins-ml-task";
     public static final Integer ML_MODEL_INDEX_SCHEMA_VERSION = 2;
     public static final Integer ML_TASK_INDEX_SCHEMA_VERSION = 1;
     public static final String USER_FIELD_MAPPING = "      \""
-            + CommonValue.USER
-            + "\": {\n"
-            + "        \"type\": \"nested\",\n"
-            + "        \"properties\": {\n"
-            + "          \"name\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\", \"ignore_above\":256}}},\n"
-            + "          \"backend_roles\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\"}}},\n"
-            + "          \"roles\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\"}}},\n"
-            + "          \"custom_attribute_names\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\"}}}\n"
-            + "        }\n"
-            + "      }\n";
+        + CommonValue.USER
+        + "\": {\n"
+        + "        \"type\": \"nested\",\n"
+        + "        \"properties\": {\n"
+        + "          \"name\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\", \"ignore_above\":256}}},\n"
+        + "          \"backend_roles\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\"}}},\n"
+        + "          \"roles\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\"}}},\n"
+        + "          \"custom_attribute_names\": {\"type\":\"text\", \"fields\":{\"keyword\":{\"type\":\"keyword\"}}}\n"
+        + "        }\n"
+        + "      }\n";
+    public static final String NODE_ID_FIELD = "node_id";
+    public static final String MODEL_LOAD_RETRY_TIMES_FIELD = "retry_times";
+
     public static final String ML_MODEL_INDEX_MAPPING = "{\n"
-            + "    \"_meta\": {\"schema_version\": "
-            + ML_MODEL_INDEX_SCHEMA_VERSION
-            + "},\n"
-            + "    \"properties\": {\n"
-            + "      \""
-            + MLModel.ALGORITHM_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLModel.MODEL_NAME_FIELD
-            + "\" : {\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\n"
-            + "      \""
-            + MLModel.OLD_MODEL_VERSION_FIELD
-            + "\" : {\"type\": \"long\"},\n"
-            + "      \""
-            + MLModel.MODEL_VERSION_FIELD
-            + "\" : {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLModel.MODEL_CONTENT_FIELD
-            + "\" : {\"type\": \"binary\"},\n"
-            + "      \""
-            + MLModel.CHUNK_NUMBER_FIELD
-            + "\" : {\"type\": \"long\"},\n"
-            + "      \""
-            + MLModel.TOTAL_CHUNKS_FIELD
-            + "\" : {\"type\": \"long\"},\n"
-            + "      \""
-            + MLModel.MODEL_ID_FIELD
-            + "\" : {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLModel.DESCRIPTION_FIELD
-            + "\" : {\"type\": \"text\"},\n"
-            + "      \""
-            + MLModel.MODEL_FORMAT_FIELD
-            + "\" : {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLModel.MODEL_STATE_FIELD
-            + "\" : {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLModel.MODEL_CONTENT_SIZE_IN_BYTES_FIELD
-            + "\" : {\"type\": \"long\"},\n"
-            + "      \""
-            + MLModel.MODEL_CONFIG_FIELD
-            + "\" : {\"properties\":{\""
-            + MODEL_TYPE_FIELD + "\":{\"type\":\"keyword\"},\""
-            + EMBEDDING_DIMENSION_FIELD + "\":{\"type\":\"integer\"},\""
-            + FRAMEWORK_TYPE_FIELD + "\":{\"type\":\"keyword\"},\""
-            + POOLING_METHOD_FIELD + "\":{\"type\":\"keyword\"},\""
-            + NORMALIZE_RESULT_FIELD + "\":{\"type\":\"boolean\"},\""
-            + MODEL_MAX_LENGTH_FIELD + "\":{\"type\":\"integer\"},\""
-            + ALL_CONFIG_FIELD + "\":{\"type\":\"text\"}}},\n"
-            + "      \""
-            + MLModel.MODEL_CONTENT_HASH_VALUE_FIELD
-            + "\" : {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLModel.CREATED_TIME_FIELD
-            + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
-            + "      \""
-            + MLModel.LAST_UPLOADED_TIME_FIELD
-            + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
-            + "      \""
-            + MLModel.LAST_LOADED_TIME_FIELD
-            + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
-            + "      \""
-            + MLModel.LAST_UNLOADED_TIME_FIELD
-            + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
-            + USER_FIELD_MAPPING
-            + "    }\n"
-            + "}";
+        + "    \"_meta\": {\"schema_version\": "
+        + ML_MODEL_INDEX_SCHEMA_VERSION
+        + "},\n"
+        + "    \"properties\": {\n"
+        + "      \""
+        + MLModel.ALGORITHM_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLModel.MODEL_NAME_FIELD
+        + "\" : {\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\n"
+        + "      \""
+        + MLModel.OLD_MODEL_VERSION_FIELD
+        + "\" : {\"type\": \"long\"},\n"
+        + "      \""
+        + MLModel.MODEL_VERSION_FIELD
+        + "\" : {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLModel.MODEL_CONTENT_FIELD
+        + "\" : {\"type\": \"binary\"},\n"
+        + "      \""
+        + MLModel.CHUNK_NUMBER_FIELD
+        + "\" : {\"type\": \"long\"},\n"
+        + "      \""
+        + MLModel.TOTAL_CHUNKS_FIELD
+        + "\" : {\"type\": \"long\"},\n"
+        + "      \""
+        + MLModel.MODEL_ID_FIELD
+        + "\" : {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLModel.DESCRIPTION_FIELD
+        + "\" : {\"type\": \"text\"},\n"
+        + "      \""
+        + MLModel.MODEL_FORMAT_FIELD
+        + "\" : {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLModel.MODEL_STATE_FIELD
+        + "\" : {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLModel.MODEL_CONTENT_SIZE_IN_BYTES_FIELD
+        + "\" : {\"type\": \"long\"},\n"
+        + "      \""
+        + MLModel.MODEL_CONFIG_FIELD
+        + "\" : {\"properties\":{\""
+        + MODEL_TYPE_FIELD + "\":{\"type\":\"keyword\"},\""
+        + EMBEDDING_DIMENSION_FIELD + "\":{\"type\":\"integer\"},\""
+        + FRAMEWORK_TYPE_FIELD + "\":{\"type\":\"keyword\"},\""
+        + ALL_CONFIG_FIELD + "\":{\"type\":\"text\"}}},\n"
+        + "      \""
+        + MLModel.MODEL_CONTENT_HASH_VALUE_FIELD
+        + "\" : {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLModel.CREATED_TIME_FIELD
+        + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
+        + "      \""
+        + MLModel.LAST_UPLOADED_TIME_FIELD
+        + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
+        + "      \""
+        + MLModel.LAST_LOADED_TIME_FIELD
+        + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
+        + "      \""
+        + MLModel.LAST_UNLOADED_TIME_FIELD
+        + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
+        + USER_FIELD_MAPPING
+        + "    }\n"
+        + "}";
 
     public static final String ML_TASK_INDEX_MAPPING = "{\n"
-            + "    \"_meta\": {\"schema_version\": "
-            + ML_TASK_INDEX_SCHEMA_VERSION
-            + "},\n"
-            + "    \"properties\": {\n"
-            + "      \""
-            + MLTask.MODEL_ID_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.TASK_TYPE_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.FUNCTION_NAME_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.STATE_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.INPUT_TYPE_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.PROGRESS_FIELD
-            + "\": {\"type\": \"float\"},\n"
-            + "      \""
-            + MLTask.OUTPUT_INDEX_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.WORKER_NODE_FIELD
-            + "\": {\"type\": \"keyword\"},\n"
-            + "      \""
-            + MLTask.CREATE_TIME_FIELD
-            + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
-            + "      \""
-            + MLTask.LAST_UPDATE_TIME_FIELD
-            + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
-            + "      \""
-            + MLTask.ERROR_FIELD
-            + "\": {\"type\": \"text\"},\n"
-            + "      \""
-            + MLTask.IS_ASYNC_TASK_FIELD
-            + "\" : {\"type\" : \"boolean\"}, \n"
-            + USER_FIELD_MAPPING
-            + "    }\n"
-            + "}";
+        + "    \"_meta\": {\"schema_version\": "
+        + ML_TASK_INDEX_SCHEMA_VERSION
+        + "},\n"
+        + "    \"properties\": {\n"
+        + "      \""
+        + MLTask.MODEL_ID_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.TASK_TYPE_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.FUNCTION_NAME_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.STATE_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.INPUT_TYPE_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.PROGRESS_FIELD
+        + "\": {\"type\": \"float\"},\n"
+        + "      \""
+        + MLTask.OUTPUT_INDEX_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.WORKER_NODE_FIELD
+        + "\": {\"type\": \"keyword\"},\n"
+        + "      \""
+        + MLTask.CREATE_TIME_FIELD
+        + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
+        + "      \""
+        + MLTask.LAST_UPDATE_TIME_FIELD
+        + "\": {\"type\": \"date\", \"format\": \"strict_date_time||epoch_millis\"},\n"
+        + "      \""
+        + MLTask.ERROR_FIELD
+        + "\": {\"type\": \"text\"},\n"
+        + "      \""
+        + MLTask.IS_ASYNC_TASK_FIELD
+        + "\" : {\"type\" : \"boolean\"}, \n"
+        + USER_FIELD_MAPPING
+        + "    }\n"
+        + "}";
 }
---
+++ plugin/src/main/java/org/opensearch/ml/model/MLModelAutoReLoader.java
@@ -0,0 +1,273 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.ml.model;
+
+import static org.opensearch.common.xcontent.XContentParserUtils.ensureExpectedToken;
+import static org.opensearch.ml.common.CommonValue.ML_MODEL_RELOAD_INDEX;
+import static org.opensearch.ml.common.CommonValue.ML_MODEL_RELOAD_MAX_RETRY_TIMES;
+import static org.opensearch.ml.common.CommonValue.ML_TASK_INDEX;
+import static org.opensearch.ml.common.CommonValue.MODEL_LOAD_RETRY_TIMES_FIELD;
+import static org.opensearch.ml.common.CommonValue.NODE_ID_FIELD;
+import static org.opensearch.ml.settings.MLCommonsSettings.ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE;
+import static org.opensearch.ml.utils.MLNodeUtils.createXContentParserFromRegistry;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import lombok.extern.log4j.Log4j2;
+
+import org.opensearch.action.admin.indices.exists.indices.IndicesExistsAction;
+import org.opensearch.action.admin.indices.exists.indices.IndicesExistsRequest;
+import org.opensearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.opensearch.action.index.IndexAction;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
+import org.opensearch.action.search.SearchAction;
+import org.opensearch.action.search.SearchRequest;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.action.support.WriteRequest;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.util.CollectionUtils;
+import org.opensearch.common.xcontent.NamedXContentRegistry;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.index.IndexNotFoundException;
+import org.opensearch.index.query.QueryBuilder;
+import org.opensearch.index.query.QueryBuilders;
+import org.opensearch.ml.cluster.DiscoveryNodeHelper;
+import org.opensearch.ml.common.MLTask;
+import org.opensearch.ml.common.transport.load.MLLoadModelAction;
+import org.opensearch.ml.common.transport.load.MLLoadModelRequest;
+import org.opensearch.ml.utils.MLNodeUtils;
+import org.opensearch.rest.RestStatus;
+import org.opensearch.search.SearchHit;
+import org.opensearch.search.builder.SearchSourceBuilder;
+import org.opensearch.search.sort.FieldSortBuilder;
+import org.opensearch.search.sort.SortBuilder;
+import org.opensearch.search.sort.SortOrder;
+
+import com.google.common.annotations.VisibleForTesting;
+
+/**
+ * Manager class for ML models and nodes. It contains ML model auto reload operations etc.
+ */
+@Log4j2
+public class MLModelAutoReLoader {
+    private final Client client;
+    private final ClusterService clusterService;
+    private final NamedXContentRegistry xContentRegistry;
+    private final DiscoveryNodeHelper nodeHelper;
+    private volatile Boolean enableAutoReLoadModel;
+
+    /**
+     * constructor method， init all the params necessary for model auto reloading
+     * @param clusterService
+     * @param client
+     * @param xContentRegistry
+     * @param nodeHelper
+     * @param settings
+     */
+    public MLModelAutoReLoader(
+        ClusterService clusterService,
+        Client client,
+        NamedXContentRegistry xContentRegistry,
+        DiscoveryNodeHelper nodeHelper,
+        Settings settings
+    ) {
+        this.clusterService = clusterService;
+        this.client = client;
+        this.xContentRegistry = xContentRegistry;
+        this.nodeHelper = nodeHelper;
+
+        enableAutoReLoadModel = ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE.get(settings);
+        clusterService
+            .getClusterSettings()
+            .addSettingsUpdateConsumer(ML_COMMONS_MODEL_AUTO_RELOAD_ENABLE, it -> enableAutoReLoadModel = it);
+    }
+
+    /**
+     * the main method: model auto reloading
+     */
+    public void autoReLoadModel() {
+        log.debug("enableAutoReLoadModel {} ", enableAutoReLoadModel);
+
+        // if we don't need to reload automatically, just return without doing anything
+        if (!enableAutoReLoadModel) {
+            return;
+        }
+
+        // At opensearch startup, get local node id, if not ml node,we ignored, just return without doing anything
+        String localNodeId = clusterService.localNode().getId();
+        if (!MLNodeUtils.isMLNode(nodeHelper.getNode(localNodeId))) {
+            return;
+        }
+
+        // According to the node id to get retry times, if more than the maxi retry times, don't need to retry
+        // that the number of unsuccessful reload has reached the maximum number of times, do not need to reload
+        int reTryTimes = getReTryTimes(localNodeId);
+        if (reTryTimes > ML_MODEL_RELOAD_MAX_RETRY_TIMES) {
+            log.debug("have exceeded max retry times, always failure");
+        }
+
+        // auto reload all models of this local node, if it fails, reTryTimes+1, if it succeeds, reTryTimes is cleared to 0
+        CompletableFuture.runAsync(() -> autoReLoadModelByNodeId(localNodeId));
+    }
+
+    /**
+     * auto reload all the models under the node id
+     * the node must be a ml node
+     * @param nodeId node id
+     */
+    @VisibleForTesting
+    void autoReLoadModelByNodeId(String nodeId) {
+        if (!isExistedIndex(ML_TASK_INDEX)) {
+            return;
+        }
+
+        SearchResponse response = queryTask(nodeId);
+
+        SearchHit[] hits = response.getHits().getHits();
+        if (CollectionUtils.isEmpty(hits)) {
+            return;
+        }
+
+        if (hits.length != 1) {
+            log.error("Can't get the latest successful deployment in node id {}", nodeId);
+        }
+
+        int reTryTimes = 0;
+        try (XContentParser parser = createXContentParserFromRegistry(xContentRegistry, hits[0].getSourceRef())) {
+            ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
+            MLTask mlTask = MLTask.parse(parser);
+
+            autoReLoadModelByNodeAndModelId(nodeId, mlTask.getModelId());
+        } catch (RuntimeException | IOException e) {
+            reTryTimes++;
+            // Store the latest value of the reTryTimes and node id under the index ".plugins-ml-model-reload"
+            saveLatestReTryTimes(nodeId, reTryTimes);
+            log.error("Can't auto reload model in node id {} ,has try {} times\nThe reason is:{}", nodeId, reTryTimes, e.getMessage());
+        }
+
+        // Store the latest value of the reTryTimes and node id under the index ".plugins-ml-model-reload"
+        saveLatestReTryTimes(nodeId, reTryTimes);
+    }
+
+    /**
+     * query task index, and get the result of "task_type"="LOAD_MODEL" and "state"="COMPLETED"
+     * and "worker_node" match nodeId
+     * @param nodeId one of query condition
+     * @return SearchResponse
+     */
+    @VisibleForTesting
+    SearchResponse queryTask(String nodeId) {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().from(0).size(1);
+
+        QueryBuilder queryBuilder = QueryBuilders
+            .boolQuery()
+            .must(QueryBuilders.matchPhraseQuery("task_type", "LOAD_MODEL"))
+            .must(QueryBuilders.matchPhraseQuery("state", "COMPLETED"))
+            .must(QueryBuilders.matchPhraseQuery("worker_node", nodeId));
+        searchSourceBuilder.query(queryBuilder);
+
+        SortBuilder sortBuilderOrder = new FieldSortBuilder("last_update_time").order(SortOrder.DESC);
+        searchSourceBuilder.sort(sortBuilderOrder);
+
+        SearchRequest searchRequest = new SearchRequest(ML_TASK_INDEX);
+        searchRequest.source(searchSourceBuilder).indices(ML_TASK_INDEX);
+        try {
+            return client.execute(SearchAction.INSTANCE, searchRequest).actionGet(5000);
+        } catch (IndexNotFoundException e) {
+            throw new IndexNotFoundException("index " + ML_TASK_INDEX + " not found");
+        }
+    }
+
+    /**
+     *  auto reload 1 model under the node id
+     * @param nodeId node id
+     * @param modelId model id
+     */
+    @VisibleForTesting
+    void autoReLoadModelByNodeAndModelId(String nodeId, String modelId) {
+        MLLoadModelRequest mlLoadModelRequest = new MLLoadModelRequest(modelId, new String[] { nodeId }, false, false);
+        client.execute(MLLoadModelAction.INSTANCE, mlLoadModelRequest).actionGet(5000);
+    }
+
+    /**
+     * get retry times from the index ".plugins-ml-model-reload" by 1 ml node
+     * @param nodeId the filter condition to query
+     * @return retry times
+     */
+    @VisibleForTesting
+    int getReTryTimes(String nodeId) {
+        if (!isExistedIndex(ML_MODEL_RELOAD_INDEX)) {
+            return 0;
+        }
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.fetchSource(new String[] { MODEL_LOAD_RETRY_TIMES_FIELD }, null);
+        QueryBuilder queryBuilder = QueryBuilders.idsQuery().addIds(nodeId);
+        searchSourceBuilder.query(queryBuilder);
+        SearchRequest searchRequest = new SearchRequest().source(searchSourceBuilder).indices(ML_MODEL_RELOAD_INDEX);
+
+        try {
+            SearchResponse response = client.execute(SearchAction.INSTANCE, searchRequest).actionGet(5000);
+
+            SearchHit[] hits = response.getHits().getHits();
+            if (CollectionUtils.isEmpty(hits)) {
+                return 0;
+            }
+
+            if (hits.length != 1) {
+                throw new RuntimeException("can't get retry times by " + nodeId);
+            }
+
+            Map<String, Object> sourceAsMap = hits[0].getSourceAsMap();
+            return (Integer) sourceAsMap.get(MODEL_LOAD_RETRY_TIMES_FIELD);
+        } catch (IndexNotFoundException e) {
+            throw new IndexNotFoundException("index " + ML_MODEL_RELOAD_INDEX + " not found");
+        }
+    }
+
+    /**
+     * judge whether the index ".plugins-ml-model-reload" existed
+     * @param indexName index name
+     * @return true: existed. false: not existed
+     */
+    @VisibleForTesting
+    boolean isExistedIndex(String indexName) {
+        IndicesExistsRequest existsRequest = new IndicesExistsRequest(indexName);
+        IndicesExistsResponse exists = client.execute(IndicesExistsAction.INSTANCE, existsRequest).actionGet(5000);
+
+        return exists.isExists();
+    }
+
+    /**
+     * save retry times
+     * @param nodeId node id
+     * @param reTryTimes actual retry times
+     */
+    @VisibleForTesting
+    void saveLatestReTryTimes(String nodeId, int reTryTimes) {
+        Map<String, Object> content = new HashMap<>();
+        content.put(NODE_ID_FIELD, nodeId);
+        content.put(MODEL_LOAD_RETRY_TIMES_FIELD, reTryTimes);
+
+        IndexRequest indexRequest = new IndexRequest(ML_MODEL_RELOAD_INDEX);
+        indexRequest.id(nodeId);
+        indexRequest.source(content);
+        indexRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE);
+
+        IndexResponse indexResponse = client.execute(IndexAction.INSTANCE, indexRequest).actionGet(5000);
+
+        if (indexResponse.status() == RestStatus.CREATED || indexResponse.status() == RestStatus.OK) {
+            log.debug("node id:{} insert retry times successfully", nodeId);
+        } else {
+            throw new RuntimeException("can't insert retry times by " + nodeId);
+        }
+    }
+}
