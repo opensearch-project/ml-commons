e6ce4ec62c4088b173b0e40c82b358abb2b27fff
Yaliang Wu <ylwu@amazon.com>
2023-01-10 15:53:03 -0800

    change only run on ml node setting default value to true (#686)
    * change only run on ml node setting default value to true
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    
    * format code
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    Signed-off-by: wujunshen <frank_wjs@hotmail.com>

--- plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
+++ plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
@@ -23,7 +23,7 @@ public final class MLCommonsSettings {
     public static final Setting<Integer> ML_COMMONS_MAX_ML_TASK_PER_NODE = Setting
         .intSetting("plugins.ml_commons.max_ml_task_per_node", 10, 0, 10000, Setting.Property.NodeScope, Setting.Property.Dynamic);
     public static final Setting<Boolean> ML_COMMONS_ONLY_RUN_ON_ML_NODE = Setting
-        .boolSetting("plugins.ml_commons.only_run_on_ml_node", false, Setting.Property.NodeScope, Setting.Property.Dynamic);
+        .boolSetting("plugins.ml_commons.only_run_on_ml_node", true, Setting.Property.NodeScope, Setting.Property.Dynamic);
     public static final Setting<Integer> ML_COMMONS_SYNC_UP_JOB_INTERVAL_IN_SECONDS = Setting
         .intSetting(
             "plugins.ml_commons.sync_up_job_interval_in_seconds",
