f76aa5b8b121f49ed8838424a4943f8236b64501
Yaliang Wu <ylwu@amazon.com>
2023-03-03 10:05:42 -0800

    fix class not found exception when deserialize model (#781)
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/utils/ModelSerDeSer.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/utils/ModelSerDeSer.java
@@ -6,14 +6,17 @@
 package org.opensearch.ml.engine.utils;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.serialization.ValidatingObjectInputStream;
 import org.opensearch.ml.engine.exceptions.ModelSerDeSerException;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+@Log4j2
 @UtilityClass
 public class ModelSerDeSer {
     // Welcome list includes OpenSearch ml plugin classes, JDK common classes and Tribuo libraries.
@@ -42,11 +45,13 @@ public class ModelSerDeSer {
 
     public static Object deserialize(byte[] modelBin) {
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(modelBin);
-             ValidatingObjectInputStream validatingObjectInputStream = new ValidatingObjectInputStream(inputStream)){
+             ValidatingObjectInputStream validatingObjectInputStream = new ValidatingObjectInputStream(inputStream);
+             ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(modelBin))){
             // Validate the model class type to avoid deserialization attack.
             validatingObjectInputStream.accept(ACCEPT_CLASS_PATTERNS);
-            return validatingObjectInputStream.readObject();
-        } catch (IOException | ClassNotFoundException e) {
+            return objectInputStream.readObject();
+        } catch (Throwable e) {
+            log.error("Failed to deserialize model", e);
             throw new ModelSerDeSerException("Failed to deserialize model.", e.getCause());
         }
     }
