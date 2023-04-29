4e80f6395cf4a084dda06b3d75d4098dd4a66337
Bhavana Goud Ramaram <rbhavna@amazon.com>
2023-04-29 00:12:57 +0000

    Add a setting to enable/disable local upload while registering model
    Signed-off-by: Bhavana Goud Ramaram <rbhavna@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/rest/RestMLRegisterModelMetaAction.java
+++ plugin/src/main/java/org/opensearch/ml/rest/RestMLRegisterModelMetaAction.java
@@ -7,12 +7,15 @@ package org.opensearch.ml.rest;
 
 import static org.opensearch.common.xcontent.XContentParserUtils.ensureExpectedToken;
 import static org.opensearch.ml.plugin.MachineLearningPlugin.ML_BASE_URI;
+import static org.opensearch.ml.settings.MLCommonsSettings.ML_COMMONS_ALLOW_LOCAL_FILE_UPLOAD;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 import org.opensearch.client.node.NodeClient;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.Settings;
 import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.ml.common.transport.upload_chunk.MLRegisterModelMetaAction;
 import org.opensearch.ml.common.transport.upload_chunk.MLRegisterModelMetaInput;
@@ -27,10 +30,19 @@ import com.google.common.collect.ImmutableList;
 public class RestMLRegisterModelMetaAction extends BaseRestHandler {
     private static final String ML_REGISTER_MODEL_META_ACTION = "ml_register_model_meta_action";
 
+    private volatile boolean isLocalFileUploadAllowed;
+
     /**
      * Constructor
+     * @param clusterService cluster service
+     * @param settings settings
      */
-    public RestMLRegisterModelMetaAction() {}
+    public RestMLRegisterModelMetaAction(ClusterService clusterService, Settings settings) {
+        isLocalFileUploadAllowed = ML_COMMONS_ALLOW_LOCAL_FILE_UPLOAD.get(settings);
+        clusterService
+            .getClusterSettings()
+            .addSettingsUpdateConsumer(ML_COMMONS_ALLOW_LOCAL_FILE_UPLOAD, it -> isLocalFileUploadAllowed = it);
+    }
 
     @Override
     public String getName() {
@@ -66,7 +78,11 @@ public class RestMLRegisterModelMetaAction extends BaseRestHandler {
     @VisibleForTesting
     MLRegisterModelMetaRequest getRequest(RestRequest request) throws IOException {
         boolean hasContent = request.hasContent();
-        if (!hasContent) {
+        if (!isLocalFileUploadAllowed) {
+            throw new IllegalArgumentException(
+                "To upload custom model from local file, user needs to enable allow_registering_model_via_local_file settings. Otherwise please use opensearch pre-trained models"
+            );
+        } else if (!hasContent) {
             throw new IOException("Model meta request has empty body");
         }
         XContentParser parser = request.contentParser();
