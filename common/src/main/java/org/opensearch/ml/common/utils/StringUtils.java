3c112241adddac0061ae0992e2876fc20542c3d2
Yaliang Wu <ylwu@amazon.com>
2023-05-31 02:10:33 -0700

    enhance parsing response logic to support list
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- common/src/main/java/org/opensearch/ml/common/utils/StringUtils.java
+++ common/src/main/java/org/opensearch/ml/common/utils/StringUtils.java
@@ -6,12 +6,17 @@
 package org.opensearch.ml.common.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StringUtils {
 
@@ -38,5 +43,20 @@ public class StringUtils {
 
         String utf8EncodedString = StandardCharsets.UTF_8.decode(buffer).toString();
         return utf8EncodedString;
+    }
+
+    public static Map<String, Object> fromJson(String jsonStr, String defaultKey) {
+        Map<String, Object> result;
+        JsonElement jsonElement = JsonParser.parseString(jsonStr);
+        if (jsonElement.isJsonObject()) {
+            result = gson.fromJson(jsonElement, Map.class);
+        } else if (jsonElement.isJsonArray()) {
+            List<Object> list = gson.fromJson(jsonElement, List.class);
+            result = new HashMap<>();
+            result.put(defaultKey, list);
+        } else {
+            throw new IllegalArgumentException("Unsupported response type");
+        }
+        return result;
     }
 }
