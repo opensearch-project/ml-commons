c7394ea63a7b26255b82c17af809a3810db927d9
Sicheng Song <sicheng.song@outlook.com>
2023-06-20 21:59:39 +0000

    fix breaking change in main during backport
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

--- plugin/src/main/java/org/opensearch/ml/autoredeploy/MLModelAutoReDeployer.java
+++ plugin/src/main/java/org/opensearch/ml/autoredeploy/MLModelAutoReDeployer.java
@@ -9,11 +9,7 @@ package org.opensearch.ml.autoredeploy;
 
 import static org.opensearch.ml.common.CommonValue.ML_MODEL_INDEX;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
