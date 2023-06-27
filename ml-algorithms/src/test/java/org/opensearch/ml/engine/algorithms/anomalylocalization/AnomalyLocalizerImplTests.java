1af9f5da79ea34bc019de89402a94b69496346ae
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-06-26 19:18:46 -0700

    Fix build.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/anomalylocalization/AnomalyLocalizerImplTests.java
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/anomalylocalization/AnomalyLocalizerImplTests.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
@@ -439,9 +440,9 @@ public class AnomalyLocalizerImplTests {
                 new HashMap<>(), roleSet,
                 Version.CURRENT);
         Metadata metadata = new Metadata.Builder()
-                .indices(ImmutableOpenMap
+                .indices(ImmutableMap
                         .<String, IndexMetadata>builder()
-                        .fPut(indexName, IndexMetadata.builder("test")
+                        .put(indexName, IndexMetadata.builder("test")
                                 .settings(Settings.builder()
                                         .put("index.number_of_shards", 1)
                                         .put("index.number_of_replicas", 1)
@@ -450,7 +451,7 @@ public class AnomalyLocalizerImplTests {
                         .build()).build();
         return new ClusterState(new ClusterName(clusterName), 123l, "111111",
                 metadata, null, DiscoveryNodes.builder().add(node).build(),
-                null, null, 0, false);
+                null, Map.of(), 0, false);
     }
 }
 
