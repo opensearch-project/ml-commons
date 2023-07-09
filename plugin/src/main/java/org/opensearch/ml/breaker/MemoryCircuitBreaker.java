eaa626d3b4cc9990a85118751553fac7b976c383
Xun Zhang <xunzh@amazon.com>
2023-07-09 13:17:53 -0700

    connector transport actions and disable native memory CB
    Signed-off-by: Xun Zhang <xunzh@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/breaker/MemoryCircuitBreaker.java
+++ plugin/src/main/java/org/opensearch/ml/breaker/MemoryCircuitBreaker.java
@@ -5,6 +5,10 @@
 
 package org.opensearch.ml.breaker;
 
+import static org.opensearch.ml.settings.MLCommonsSettings.ML_COMMONS_JVM_HEAP_MEM_THRESHOLD;
+
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.Settings;
 import org.opensearch.monitor.jvm.JvmService;
 
 /**
@@ -15,6 +19,7 @@ public class MemoryCircuitBreaker extends ThresholdCircuitBreaker<Short> {
     private static final String ML_MEMORY_CB = "Memory Circuit Breaker";
     public static final short DEFAULT_JVM_HEAP_USAGE_THRESHOLD = 85;
     private final JvmService jvmService;
+    private volatile Integer jvmHeapMemThreshold = 85;
 
     public MemoryCircuitBreaker(JvmService jvmService) {
         super(DEFAULT_JVM_HEAP_USAGE_THRESHOLD);
@@ -24,6 +29,13 @@ public class MemoryCircuitBreaker extends ThresholdCircuitBreaker<Short> {
     public MemoryCircuitBreaker(short threshold, JvmService jvmService) {
         super(threshold);
         this.jvmService = jvmService;
+    }
+
+    public MemoryCircuitBreaker(Settings settings, ClusterService clusterService, JvmService jvmService) {
+        super(DEFAULT_JVM_HEAP_USAGE_THRESHOLD);
+        this.jvmService = jvmService;
+        this.jvmHeapMemThreshold = ML_COMMONS_JVM_HEAP_MEM_THRESHOLD.get(settings);
+        clusterService.getClusterSettings().addSettingsUpdateConsumer(ML_COMMONS_JVM_HEAP_MEM_THRESHOLD, it -> jvmHeapMemThreshold = it);
     }
 
     @Override
