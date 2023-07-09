ebb01ec259a1ca7ca0458bc95f4a9dae48eec69e
Xun Zhang <xunzh@amazon.com>
2023-07-08 22:39:43 -0700

    connector transport actions, requests and  responses
    Signed-off-by: Xun Zhang <xunzh@amazon.com>

---
+++ common/src/main/java/org/opensearch/ml/common/transport/connector/MLCreateConnectorRequest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.transport.connector;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.opensearch.action.ActionRequest;
+import org.opensearch.action.ActionRequestValidationException;
+import org.opensearch.common.io.stream.InputStreamStreamInput;
+import org.opensearch.common.io.stream.OutputStreamStreamOutput;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.common.io.stream.StreamOutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import static org.opensearch.action.ValidateActions.addValidationError;
+
+@Getter
+public class MLCreateConnectorRequest extends ActionRequest {
+    private MLCreateConnectorInput mlCreateConnectorInput;
+
+    @Builder
+    public MLCreateConnectorRequest(MLCreateConnectorInput mlCreateConnectorInput) {
+        this.mlCreateConnectorInput = mlCreateConnectorInput;
+    }
+
+    public MLCreateConnectorRequest(StreamInput in) throws IOException {
+        super(in);
+        this.mlCreateConnectorInput = new MLCreateConnectorInput(in);
+    }
+
+    @Override
+    public ActionRequestValidationException validate() {
+        ActionRequestValidationException exception = null;
+        if (mlCreateConnectorInput == null) {
+            exception = addValidationError("ML Connector input can't be null", exception);
+        }
+
+        return exception;
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        this.mlCreateConnectorInput.writeTo(out);
+    }
+
+    public static MLCreateConnectorRequest fromActionRequest(ActionRequest actionRequest) {
+        if (actionRequest instanceof MLCreateConnectorRequest) {
+            return (MLCreateConnectorRequest) actionRequest;
+        }
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             OutputStreamStreamOutput osso = new OutputStreamStreamOutput(baos)) {
+            actionRequest.writeTo(osso);
+            try (StreamInput input = new InputStreamStreamInput(new ByteArrayInputStream(baos.toByteArray()))) {
+                return new MLCreateConnectorRequest(input);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to parse ActionRequest into MLCreateConnectorRequest", e);
+        }
+    }
+}
