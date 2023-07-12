c2e5e7468a1da35d6f0801b430bad6a31bace350
Yaliang Wu <ylwu@amazon.com>
2023-07-11 17:12:13 -0700

    tune syncup jot interval
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
+++ plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
@@ -42,7 +42,7 @@ public final class MLCommonsSettings {
     public static final Setting<Integer> ML_COMMONS_SYNC_UP_JOB_INTERVAL_IN_SECONDS = Setting
         .intSetting(
             "plugins.ml_commons.sync_up_job_interval_in_seconds",
-            3,
+            10,
             0,
             86400,
             Setting.Property.NodeScope,
