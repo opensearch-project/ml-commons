acaccbffc4aefc91b21c03baa63aeec4e47e9d9d
xinyual <xinyual@amazon.com>
2023-08-28 13:01:23 +0800

    add functionName pipeline

--- plugin/src/main/java/org/opensearch/ml/model/MLModelManager.java
+++ plugin/src/main/java/org/opensearch/ml/model/MLModelManager.java
@@ -519,7 +519,7 @@ public class MLModelManager {
                 modelName,
                 version,
                 registerModelInput.getUrl(),
-                registerModelInput.getHashValue(),
+                registerModelInput.getHashValue(), functionName,
                 ActionListener.wrap(result -> {
                     Long modelSizeInBytes = (Long) result.get(MODEL_SIZE_IN_BYTES);
                     if (modelSizeInBytes >= MODEL_FILE_SIZE_LIMIT) {
