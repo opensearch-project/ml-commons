5aa5698ae1a7a3ca4cb899248f5b6084f9d16b3c
xinyual <xinyual@amazon.com>
2023-08-28 12:50:25 +0800

    remove pt file detect
    Signed-off-by: xinyual <xinyual@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/ModelHelper.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/ModelHelper.java
@@ -237,7 +237,7 @@ public class ModelHelper {
                 }
             }
         }
-        if (!hasPtFile && !hasOnnxFile) {
+        if (!hasPtFile && !hasOnnxFile && modelName != FunctionName.TOKENIZE.toString()) {
             throw new IllegalArgumentException("Can't find model file");
         }
         if (!hasTokenizerFile) {
