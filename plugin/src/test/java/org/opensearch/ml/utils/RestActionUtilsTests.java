d84f3a5decee4aa0e62bfdfd0c1d75a62abf48da
zane-neo <zaniu@amazon.com>
2023-09-01 14:47:56 +0800

    Fix low code coverage issue
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/utils/RestActionUtilsTests.java
+++ plugin/src/test/java/org/opensearch/ml/utils/RestActionUtilsTests.java
@@ -5,6 +5,9 @@
 
 package org.opensearch.ml.utils;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.opensearch.ml.settings.MLCommonsSettings.ML_COMMONS_CONNECTOR_ACCESS_CONTROL_ENABLED;
 import static org.opensearch.ml.utils.RestActionUtils.OPENSEARCH_DASHBOARDS_USER_AGENT;
 import static org.opensearch.ml.utils.RestActionUtils.PARAMETER_ALGORITHM;
 import static org.opensearch.ml.utils.RestActionUtils.PARAMETER_ASYNC;
@@ -12,15 +15,29 @@ import static org.opensearch.ml.utils.RestActionUtils.PARAMETER_MODEL_ID;
 import static org.opensearch.ml.utils.RestActionUtils.UI_METADATA_EXCLUDE;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import org.apache.commons.math3.ml.clustering.Cluster;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.ClusterState;
+import org.opensearch.cluster.node.DiscoveryNode;
+import org.opensearch.cluster.node.DiscoveryNodes;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.commons.ConfigConstants;
+import org.opensearch.commons.authuser.User;
+import org.opensearch.core.rest.RestStatus;
 import org.opensearch.ml.common.FunctionName;
 import org.opensearch.ml.plugin.MachineLearningPlugin;
+import org.opensearch.rest.RestChannel;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.search.builder.SearchSourceBuilder;
 import org.opensearch.search.fetch.subphase.FetchSourceContext;
@@ -28,6 +45,7 @@ import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.test.rest.FakeRestRequest;
 
 import com.google.common.collect.ImmutableMap;
+import org.opensearch.threadpool.ThreadPool;
 
 public class RestActionUtilsTests extends OpenSearchTestCase {
 
@@ -124,5 +142,94 @@ public class RestActionUtilsTests extends OpenSearchTestCase {
         testSearchSourceBuilder.fetchSource(new String[] { "a" }, new String[] { "b" });
         FetchSourceContext sourceContext = RestActionUtils.getSourceContext(request, testSearchSourceBuilder);
         assertEquals(sourceContext.excludes().length, 2);
+    }
+
+    public void test_getSourceContext_fetchSource_null_dashboardUserAgent_null() {
+        FakeRestRequest request = new FakeRestRequest.Builder(xContentRegistry())
+            .withMethod(RestRequest.Method.POST)
+            .withPath(urlPath)
+            .withParams(param)
+            .build();
+        SearchSourceBuilder testSearchSourceBuilder = new SearchSourceBuilder();
+        FetchSourceContext sourceContext = RestActionUtils.getSourceContext(request, testSearchSourceBuilder);
+        assertNotNull(sourceContext);
+    }
+
+    public void test_getSourceContext_fetchSource_null_dashboardUserAgent_notNull() {
+        FakeRestRequest request = new FakeRestRequest.Builder(xContentRegistry())
+            .withMethod(RestRequest.Method.POST)
+            .withPath(urlPath)
+            .withParams(param)
+            .withHeaders(ImmutableMap.of("User-Agent", Arrays.asList("OpenSearch Dashboards")))
+            .build();
+        SearchSourceBuilder testSearchSourceBuilder = new SearchSourceBuilder();
+        FetchSourceContext sourceContext = RestActionUtils.getSourceContext(request, testSearchSourceBuilder);
+        assertNotNull(sourceContext);
+    }
+
+    public void test_getFetchSourceContext_return_modelContent() {
+        FetchSourceContext result = RestActionUtils.getFetchSourceContext(true);
+        assertNotNull(result);
+    }
+
+    public void test_getFetchSourceContext_not_return_modelContent() {
+        FetchSourceContext result = RestActionUtils.getFetchSourceContext(false);
+        assertNotNull(result);
+    }
+
+    public void test_getAllNodes() {
+        ClusterService clusterService = mock(ClusterService.class);
+        ClusterState clusterState = mock(ClusterState.class);
+        DiscoveryNodes discoveryNodes = mock(DiscoveryNodes.class);
+        final Map<String, DiscoveryNode> dataNodes = Map.of("dataNodeId", mock(DiscoveryNode.class));
+        when(discoveryNodes.getDataNodes()).thenReturn(dataNodes);
+        when(discoveryNodes.getSize()).thenReturn(1);
+        when(discoveryNodes.iterator()).thenReturn(dataNodes.values().iterator());
+        when(clusterState.nodes()).thenReturn(discoveryNodes);
+        when(clusterService.state()).thenReturn(clusterState);
+        String[] result = RestActionUtils.getAllNodes(clusterService);
+        assertNotNull(result);
+        assertEquals(1, result.length);
+    }
+
+    public void test_onFailure() {
+        fakeRestRequest = createRestRequest(ImmutableMap.<String, String>builder().put(PARAMETER_ALGORITHM, "").build());
+        RestActionUtils.onFailure(mock(RestChannel.class), RestStatus.CREATED, "error", new IllegalArgumentException("test"));
+    }
+
+    public void test_splitCommaSeparatedParam() {
+        FakeRestRequest request = new FakeRestRequest.Builder(xContentRegistry())
+            .withMethod(RestRequest.Method.POST)
+            .withPath(urlPath)
+            .withParams(param)
+            .withHeaders(ImmutableMap.of("User-Agent", Arrays.asList("OpenSearch Dashboards")))
+            .build();
+        Optional<String[]> result = RestActionUtils.splitCommaSeparatedParam(request, PARAMETER_ALGORITHM);
+        assertNotNull(result);
+        assertNotNull(result.get());
+    }
+
+    public void test_getStringParam() {
+        FakeRestRequest request = new FakeRestRequest.Builder(xContentRegistry())
+            .withMethod(RestRequest.Method.POST)
+            .withPath(urlPath)
+            .withParams(param)
+            .withHeaders(ImmutableMap.of("User-Agent", Arrays.asList("OpenSearch Dashboards")))
+            .build();
+        Optional<String> result = RestActionUtils.getStringParam(request, PARAMETER_ALGORITHM);
+        assertNotNull(result);
+        assertNotNull(result.get());
+    }
+
+    public void test_getUserContext() {
+        Client client = mock(Client.class);
+        Settings settings = Settings.builder().put(ML_COMMONS_CONNECTOR_ACCESS_CONTROL_ENABLED.getKey(), true).build();
+        ThreadContext threadContext = new ThreadContext(settings);
+        threadContext.putTransient(ConfigConstants.OPENSEARCH_SECURITY_USER_INFO_THREAD_CONTEXT, "myuser||myrole");
+        ThreadPool threadPool = mock(ThreadPool.class);
+        when(client.threadPool()).thenReturn(threadPool);
+        when(threadPool.getThreadContext()).thenReturn(threadContext);
+        User user = RestActionUtils.getUserContext(client);
+        assertNotNull(user);
     }
 }
