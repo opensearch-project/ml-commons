ada0be338abff287b753877704de7fa3fb82cc13
Yaliang Wu <ylwu@amazon.com>
2023-09-28 10:49:20 -0700

    fix null exception in text docs data set (#1403)
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    (cherry picked from commit cda89ffd407be450c367750b20f7f28146dff6cb)

---
+++ common/src/test/java/org/opensearch/ml/common/dataset/TextDocsInputDataSetTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.dataset;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.core.common.io.stream.StreamInput;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class TextDocsInputDataSetTest {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void writeTo_Success() throws IOException {
+        TextDocsInputDataSet inputDataSet = TextDocsInputDataSet.builder().docs(Arrays.asList("doc1", "doc2")).build();
+        BytesStreamOutput bytesStreamOutput = new BytesStreamOutput();
+        inputDataSet.writeTo(bytesStreamOutput);
+
+        StreamInput streamInput = bytesStreamOutput.bytes().streamInput();
+        MLInputDataType inputDataType = streamInput.readEnum(MLInputDataType.class);
+        assertEquals(MLInputDataType.TEXT_DOCS, inputDataType);
+        TextDocsInputDataSet inputDataSet1 = new TextDocsInputDataSet((streamInput));
+        assertEquals(2, inputDataSet1.getDocs().size());
+        assertEquals("doc1", inputDataSet1.getDocs().get(0));
+        assertEquals("doc2", inputDataSet1.getDocs().get(1));
+    }
+
+    @Test
+    public void writeTo_Success_NullValue() throws IOException {
+        TextDocsInputDataSet inputDataSet = TextDocsInputDataSet.builder().docs(Arrays.asList("doc1", null)).build();
+        BytesStreamOutput bytesStreamOutput = new BytesStreamOutput();
+        inputDataSet.writeTo(bytesStreamOutput);
+
+        StreamInput streamInput = bytesStreamOutput.bytes().streamInput();
+        MLInputDataType inputDataType = streamInput.readEnum(MLInputDataType.class);
+        assertEquals(MLInputDataType.TEXT_DOCS, inputDataType);
+        TextDocsInputDataSet inputDataSet1 = new TextDocsInputDataSet((streamInput));
+        assertEquals(2, inputDataSet1.getDocs().size());
+        assertEquals("doc1", inputDataSet1.getDocs().get(0));
+        assertEquals(null, inputDataSet1.getDocs().get(1));
+    }
+}
