9858730f728865d6a75ac4fd77ba7211af382de1
Sicheng Song <sicheng.song@outlook.com>
2023-10-06 16:39:14 -0700

    Update Model API (#1350)
    * Update Model API POC
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Using GetRequest to get model
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Finalize model update API
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Fix compile
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Fix compileTest
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Add Unit Test Cases for Update Model API
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Tune back test coverage thereshold
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Add more unit tests on Update model API
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Add unit test for TransportUpdateModelAction class
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Fix a test error
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Change exception thrown to failure response
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Move the function judgement to the outter block
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Check if model is undeployed before update model
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Add more unit test for update model API
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Fix unit test due to blocking java 11 CI workflow
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Enabling auto bumping model version during registering to a new model group and address reviewers' other concern
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Autobump new model groups' latest version when register to a new model
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Change the REST API method from POST to PUT
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Change the update REST API endpoint
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    ---------
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

---
+++ plugin/src/test/java/org/opensearch/ml/rest/RestMLUpdateModelActionTests.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.rest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opensearch.action.update.UpdateResponse;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.xcontent.XContentType;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.common.Strings;
+import org.opensearch.core.common.bytes.BytesArray;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.ml.common.transport.model.MLUpdateModelAction;
+import org.opensearch.ml.common.transport.model.MLUpdateModelInput;
+import org.opensearch.ml.common.transport.model.MLUpdateModelRequest;
+import org.opensearch.rest.RestChannel;
+import org.opensearch.rest.RestHandler;
+import org.opensearch.rest.RestRequest;
+import org.opensearch.test.OpenSearchTestCase;
+import org.opensearch.test.rest.FakeRestRequest;
+import org.opensearch.threadpool.TestThreadPool;
+import org.opensearch.threadpool.ThreadPool;
+
+import com.google.gson.Gson;
+
+public class RestMLUpdateModelActionTests extends OpenSearchTestCase {
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    private RestMLUpdateModelAction restMLUpdateModelAction;
+    private NodeClient client;
+    private ThreadPool threadPool;
+
+    @Mock
+    RestChannel channel;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        threadPool = new TestThreadPool(this.getClass().getSimpleName() + "ThreadPool");
+        client = spy(new NodeClient(Settings.EMPTY, threadPool));
+        restMLUpdateModelAction = new RestMLUpdateModelAction();
+        doAnswer(invocation -> {
+            ActionListener<UpdateResponse> actionListener = invocation.getArgument(2);
+            return null;
+        }).when(client).execute(eq(MLUpdateModelAction.INSTANCE), any(), any());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        threadPool.shutdown();
+        client.close();
+    }
+
+    @Test
+    public void testConstructor() {
+        RestMLUpdateModelAction UpdateModelAction = new RestMLUpdateModelAction();
+        assertNotNull(UpdateModelAction);
+    }
+
+    @Test
+    public void testGetName() {
+        String actionName = restMLUpdateModelAction.getName();
+        assertFalse(Strings.isNullOrEmpty(actionName));
+        assertEquals("ml_update_model_action", actionName);
+    }
+
+    @Test
+    public void testRoutes() {
+        List<RestHandler.Route> routes = restMLUpdateModelAction.routes();
+        assertNotNull(routes);
+        assertFalse(routes.isEmpty());
+        RestHandler.Route route = routes.get(0);
+        assertEquals(RestRequest.Method.PUT, route.getMethod());
+        assertEquals("/_plugins/_ml/models/{model_id}", route.getPath());
+    }
+
+    @Test
+    public void testUpdateModelRequest() throws Exception {
+        RestRequest request = getRestRequest();
+        restMLUpdateModelAction.handleRequest(request, channel, client);
+        ArgumentCaptor<MLUpdateModelRequest> argumentCaptor = ArgumentCaptor.forClass(MLUpdateModelRequest.class);
+        verify(client, times(1)).execute(eq(MLUpdateModelAction.INSTANCE), argumentCaptor.capture(), any());
+        MLUpdateModelInput updateModelInput = argumentCaptor.getValue().getUpdateModelInput();
+        assertEquals("testModelName", updateModelInput.getName());
+        assertEquals("This is test description", updateModelInput.getDescription());
+    }
+
+    @Test
+    public void testUpdateModelRequestWithEmptyContent() throws Exception {
+        exceptionRule.expect(IOException.class);
+        exceptionRule.expectMessage("Model update request has empty body");
+        RestRequest request = getRestRequestWithEmptyContent();
+        restMLUpdateModelAction.handleRequest(request, channel, client);
+    }
+
+    @Test
+    public void testUpdateModelRequestWithNullModelId() throws Exception {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Request should contain model_id");
+        RestRequest request = getRestRequestWithNullModelId();
+        restMLUpdateModelAction.handleRequest(request, channel, client);
+    }
+
+    private RestRequest getRestRequest() {
+        RestRequest.Method method = RestRequest.Method.PUT;
+        final Map<String, Object> modelContent = Map.of("name", "testModelName", "description", "This is test description");
+        String requestContent = new Gson().toJson(modelContent);
+        Map<String, String> params = new HashMap<>();
+        params.put("model_id", "test_modelId");
+        RestRequest request = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY)
+            .withMethod(method)
+            .withPath("/_plugins/_ml/models/{model_id}")
+            .withParams(params)
+            .withContent(new BytesArray(requestContent), XContentType.JSON)
+            .build();
+        return request;
+    }
+
+    private RestRequest getRestRequestWithEmptyContent() {
+        RestRequest.Method method = RestRequest.Method.PUT;
+        Map<String, String> params = new HashMap<>();
+        params.put("model_id", "test_modelId");
+        RestRequest request = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY)
+            .withMethod(method)
+            .withPath("/_plugins/_ml/models/{model_id}")
+            .withParams(params)
+            .withContent(new BytesArray(""), XContentType.JSON)
+            .build();
+        return request;
+    }
+
+    private RestRequest getRestRequestWithNullModelId() {
+        RestRequest.Method method = RestRequest.Method.PUT;
+        final Map<String, Object> modelContent = Map.of("name", "testModelName", "description", "This is test description");
+        String requestContent = new Gson().toJson(modelContent);
+        Map<String, String> params = new HashMap<>();
+        RestRequest request = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY)
+            .withMethod(method)
+            .withPath("/_plugins/_ml/models/{model_id}")
+            .withParams(params)
+            .withContent(new BytesArray(requestContent), XContentType.JSON)
+            .build();
+        return request;
+    }
+}
