c08dcf8c108f77f62e22688cb67b14acdf73950e
Sicheng Song <sicheng.song@outlook.com>
2023-10-09 09:21:56 -0700

    Add a setting to control the update connector API (#1465)
    * Add a setting to control the update connector API
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Enabling the update connnector setting in unit test
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    * Enabling the update connnector setting in corresponding unit test
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>
    
    ---------
    
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

--- plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
+++ plugin/src/main/java/org/opensearch/ml/settings/MLCommonsSettings.java
@@ -114,6 +114,9 @@ public final class MLCommonsSettings {
     public static final Setting<Boolean> ML_COMMONS_REMOTE_INFERENCE_ENABLED = Setting
         .boolSetting("plugins.ml_commons.remote_inference.enabled", true, Setting.Property.NodeScope, Setting.Property.Dynamic);
 
+    public static final Setting<Boolean> ML_COMMONS_UPDATE_CONNECTOR_ENABLED = Setting
+        .boolSetting("plugins.ml_commons.update_connector.enabled", false, Setting.Property.NodeScope, Setting.Property.Dynamic);
+
     public static final Setting<Boolean> ML_COMMONS_MODEL_ACCESS_CONTROL_ENABLED = Setting
         .boolSetting("plugins.ml_commons.model_access_control_enabled", false, Setting.Property.NodeScope, Setting.Property.Dynamic);
 
