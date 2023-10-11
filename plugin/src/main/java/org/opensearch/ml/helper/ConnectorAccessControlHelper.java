2f3f39e8f8b010e85797533fc4fa3a7df9e1f6b3
Yaliang Wu <ylwu@amazon.com>
2023-10-11 15:15:16 -0700

    fix update connector API (#1484)
    * fix update connector API
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    
    * fix ut
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>
    
    ---------
    
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/helper/ConnectorAccessControlHelper.java
+++ plugin/src/main/java/org/opensearch/ml/helper/ConnectorAccessControlHelper.java
@@ -64,35 +64,48 @@ public class ConnectorAccessControlHelper {
             listener.onResponse(true);
             return;
         }
-        GetRequest getRequest = new GetRequest().index(CommonValue.ML_CONNECTOR_INDEX).id(connectorId);
         try (ThreadContext.StoredContext context = client.threadPool().getThreadContext().stashContext()) {
             ActionListener<Boolean> wrappedListener = ActionListener.runBefore(listener, context::restore);
-            client.get(getRequest, ActionListener.wrap(r -> {
-                if (r != null && r.isExists()) {
-                    try (
-                        XContentParser parser = MLNodeUtils
-                            .createXContentParserFromRegistry(NamedXContentRegistry.EMPTY, r.getSourceAsBytesRef())
-                    ) {
-                        ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
-                        Connector connector = Connector.createConnector(parser);
-                        boolean hasPermission = hasPermission(user, connector);
-                        wrappedListener.onResponse(hasPermission);
-                    } catch (Exception e) {
-                        log.error("Failed to parse connector:" + connectorId);
-                        wrappedListener.onFailure(e);
-                    }
-                } else {
-                    wrappedListener.onFailure(new MLResourceNotFoundException("Fail to find connector:" + connectorId));
-                }
-            }, e -> {
-                log.error("Fail to get connector", e);
-                wrappedListener.onFailure(new IllegalStateException("Fail to get connector:" + connectorId));
-            }));
+            getConnector(client, connectorId, ActionListener.wrap(connector -> {
+                boolean hasPermission = hasPermission(user, connector);
+                wrappedListener.onResponse(hasPermission);
+            }, e -> { wrappedListener.onFailure(e); }));
         } catch (Exception e) {
             log.error("Failed to validate Access for connector:" + connectorId, e);
             listener.onFailure(e);
         }
+    }
 
+    public boolean validateConnectorAccess(Client client, Connector connector) {
+        User user = RestActionUtils.getUserContext(client);
+        if (isAdmin(user) || accessControlNotEnabled(user)) {
+            return true;
+        }
+        return hasPermission(user, connector);
+    }
+
+    public void getConnector(Client client, String connectorId, ActionListener<Connector> listener) {
+        GetRequest getRequest = new GetRequest().index(CommonValue.ML_CONNECTOR_INDEX).id(connectorId);
+        client.get(getRequest, ActionListener.wrap(r -> {
+            if (r != null && r.isExists()) {
+                try (
+                    XContentParser parser = MLNodeUtils
+                        .createXContentParserFromRegistry(NamedXContentRegistry.EMPTY, r.getSourceAsBytesRef())
+                ) {
+                    ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
+                    Connector connector = Connector.createConnector(parser);
+                    listener.onResponse(connector);
+                } catch (Exception e) {
+                    log.error("Failed to parse connector:" + connectorId);
+                    listener.onFailure(e);
+                }
+            } else {
+                listener.onFailure(new MLResourceNotFoundException("Fail to find connector:" + connectorId));
+            }
+        }, e -> {
+            log.error("Fail to get connector", e);
+            listener.onFailure(new IllegalStateException("Fail to get connector:" + connectorId));
+        }));
     }
 
     public boolean skipConnectorAccessControl(User user) {
