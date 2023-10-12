8c274297d7cff30b000827c0f9187edd60bb9438
Owais Kazi <owaiskazi19@gmail.com>
2023-10-12 10:40:34 -0700

    Added create connector API for MLClient (#1437)
    * Adds create connector API for MLClient
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed PR comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed PR Comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    ---------
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    (cherry picked from commit d265fc7f48b4b17f969b9a589ccde4f5ffd9891e)

--- client/src/main/java/org/opensearch/ml/client/MachineLearningNodeClient.java
+++ client/src/main/java/org/opensearch/ml/client/MachineLearningNodeClient.java
@@ -32,6 +32,10 @@ import org.opensearch.ml.common.model.MLModelFormat;
 import org.opensearch.ml.common.model.MetricsCorrelationModelConfig;
 import org.opensearch.ml.common.output.MLOutput;
 import org.opensearch.ml.common.transport.MLTaskResponse;
+import org.opensearch.ml.common.transport.connector.MLCreateConnectorAction;
+import org.opensearch.ml.common.transport.connector.MLCreateConnectorInput;
+import org.opensearch.ml.common.transport.connector.MLCreateConnectorRequest;
+import org.opensearch.ml.common.transport.connector.MLCreateConnectorResponse;
 import org.opensearch.ml.common.transport.deploy.MLDeployModelAction;
 import org.opensearch.ml.common.transport.deploy.MLDeployModelInput;
 import org.opensearch.ml.common.transport.deploy.MLDeployModelRequest;
@@ -226,6 +230,12 @@ public class MachineLearningNodeClient implements MachineLearningClient {
         client.execute(MLDeployModelAction.INSTANCE, deployModelRequest, ActionListener.wrap(listener::onResponse, e -> {
             listener.onFailure(e);
         }));
+    }
+
+    @Override
+    public void createConnector(MLCreateConnectorInput mlCreateConnectorInput, ActionListener<MLCreateConnectorResponse> listener) {
+        MLCreateConnectorRequest createConnectorRequest = new MLCreateConnectorRequest(mlCreateConnectorInput);
+        client.execute(MLCreateConnectorAction.INSTANCE, createConnectorRequest, listener);
     }
 
     private ActionListener<MLTaskResponse> getMlPredictionTaskResponseActionListener(ActionListener<MLOutput> listener) {
