713dcba9f843b6fb1bb23dcb88973373371ea8df
Yaliang Wu <ylwu@amazon.com>
2023-10-27 00:20:58 -0700

    fix register client API (#1560)
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- common/src/main/java/org/opensearch/ml/common/transport/register/MLRegisterModelResponse.java
+++ common/src/main/java/org/opensearch/ml/common/transport/register/MLRegisterModelResponse.java
@@ -7,13 +7,19 @@ package org.opensearch.ml.common.transport.register;
 
 import lombok.Getter;
 import org.opensearch.core.action.ActionResponse;
+import org.opensearch.core.common.io.stream.InputStreamStreamInput;
+import org.opensearch.core.common.io.stream.OutputStreamStreamOutput;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
 import org.opensearch.core.xcontent.ToXContent;
 import org.opensearch.core.xcontent.ToXContentObject;
 import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.ml.common.transport.MLTaskResponse;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
 @Getter
 public class MLRegisterModelResponse extends ActionResponse implements ToXContentObject {
@@ -60,5 +66,21 @@ public class MLRegisterModelResponse extends ActionResponse implements ToXConten
         }
         builder.endObject();
         return builder;
+    }
+
+    public static MLRegisterModelResponse fromActionResponse(ActionResponse actionResponse) {
+        if (actionResponse instanceof MLRegisterModelResponse) {
+            return (MLRegisterModelResponse) actionResponse;
+        }
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             OutputStreamStreamOutput osso = new OutputStreamStreamOutput(baos)) {
+            actionResponse.writeTo(osso);
+            try (StreamInput input = new InputStreamStreamInput(new ByteArrayInputStream(baos.toByteArray()))) {
+                return new MLRegisterModelResponse(input);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException("failed to parse ActionResponse into MLRegisterModelResponse", e);
+        }
     }
 }
