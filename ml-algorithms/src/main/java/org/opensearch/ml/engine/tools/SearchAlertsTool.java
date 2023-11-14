213a0951edce33435342617d7adfc722561aa2a2
Tyler Ohlsen <ohltyler@amazon.com>
2023-11-14 17:12:38 +0000

    Add search alerts tool
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

---
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/SearchAlertsTool.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.engine.tools;
+
+import static org.opensearch.ml.common.utils.StringUtils.gson;
+
+import java.util.List;
+import java.util.Map;
+
+import org.opensearch.client.Client;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.commons.alerting.AlertingPluginInterface;
+import org.opensearch.commons.alerting.action.GetAlertsRequest;
+import org.opensearch.commons.alerting.action.GetAlertsResponse;
+import org.opensearch.commons.alerting.model.Table;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.ml.common.output.model.ModelTensors;
+import org.opensearch.ml.common.spi.tools.Parser;
+import org.opensearch.ml.common.spi.tools.Tool;
+import org.opensearch.ml.common.spi.tools.ToolAnnotation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@ToolAnnotation(SearchAlertsTool.NAME)
+public class SearchAlertsTool implements Tool {
+    public static final String NAME = "SearchAlertsTool";
+    private static final String DEFAULT_DESCRIPTION = "Use this tool to search alerts.";
+
+    @Setter
+    @Getter
+    private String name = SearchAlertsTool.NAME;
+    @Getter
+    @Setter
+    private String description = DEFAULT_DESCRIPTION;
+    @Getter
+    private String type;
+    @Getter
+    private String version;
+
+    private Client client;
+    @Setter
+    private Parser<?, ?> inputParser;
+    @Setter
+    private Parser<?, ?> outputParser;
+    @SuppressWarnings("unused")
+    private ClusterService clusterService;
+
+    public SearchAlertsTool(Client client, ClusterService clusterService) {
+        this.client = client;
+        this.clusterService = clusterService;
+
+        // probably keep this overridden output parser. need to ensure the output matches what's expected
+        outputParser = new Parser<>() {
+            @Override
+            public Object parse(Object o) {
+                @SuppressWarnings("unchecked")
+                List<ModelTensors> mlModelOutputs = (List<ModelTensors>) o;
+                return mlModelOutputs.get(0).getMlModelTensors().get(0).getDataAsMap().get("response");
+            }
+        };
+    }
+
+    @Override
+    public <T> void run(Map<String, String> parameters, ActionListener<T> listener) {
+
+        // build out the request
+        final String tableSortOrder = parameters.containsKey("sortOrder") ? parameters.get("sortOrder") : "asc";
+        final String tableSortString = parameters.containsKey("sortString") ? parameters.get("sortString") : "monitor_name.keyword";
+        final int tableSize = parameters.containsKey("size") ? Integer.parseInt(parameters.get("size")) : 20;
+        final String searchString = parameters.containsKey("searchString") ? parameters.get("searchString") : null;
+
+        // not exposing "missing" or "startIndex" from the table, using defaults of null/0, respectively
+        final Table table = new Table(tableSortOrder, tableSortString, null, tableSize, 0, searchString);
+
+        final String severityLevel = parameters.containsKey("severityLevel") ? parameters.get("severityLevel") : "ALL";
+        final String alertState = parameters.containsKey("alertState") ? parameters.get("alertState") : "ALL";
+        final String monitorId = parameters.containsKey("monitorId") ? parameters.get("monitorId") : null;
+        final String alertIndex = parameters.containsKey("alertIndex") ? parameters.get("alertIndex") : null;
+        @SuppressWarnings("unchecked")
+        final List<String> monitorIds = parameters.containsKey("monitorIds")
+            ? gson.fromJson(parameters.get("monitorIds"), List.class)
+            : null;
+        @SuppressWarnings("unchecked")
+        final List<String> workflowIds = parameters.containsKey("workflowIds")
+            ? gson.fromJson(parameters.get("workflowIds"), List.class)
+            : null;
+        @SuppressWarnings("unchecked")
+        final List<String> alertIds = parameters.containsKey("alertIds") ? gson.fromJson(parameters.get("alertIds"), List.class) : null;
+
+        GetAlertsRequest getAlertsRequest = new GetAlertsRequest(
+            table,
+            severityLevel,
+            alertState,
+            monitorId,
+            alertIndex,
+            monitorIds,
+            workflowIds,
+            alertIds
+        );
+
+        // create response listener
+        ActionListener<GetAlertsResponse> getAlertsListener = ActionListener.<GetAlertsResponse>wrap(response -> {
+            listener.onResponse((T) response);
+        }, e -> { listener.onFailure(e); });
+
+        // execute the search
+        AlertingPluginInterface.INSTANCE.getAlerts((NodeClient) client, getAlertsRequest, getAlertsListener);
+    }
+
+    @Override
+    public boolean validate(Map<String, String> parameters) {
+        if (parameters == null || parameters.size() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Factory for the {@link SearchAlertsTool}
+     */
+    public static class Factory implements Tool.Factory<SearchAlertsTool> {
+        private Client client;
+        private ClusterService clusterService;
+
+        private static Factory INSTANCE;
+
+        /** 
+         * Create or return the singleton factory instance
+         */
+        public static Factory getInstance() {
+            if (INSTANCE != null) {
+                return INSTANCE;
+            }
+            synchronized (SearchAlertsTool.class) {
+                if (INSTANCE != null) {
+                    return INSTANCE;
+                }
+                INSTANCE = new Factory();
+                return INSTANCE;
+            }
+        }
+
+        /**
+         * Initialize this factory
+         * @param client The OpenSearch client
+         * @param clusterService The OpenSearch cluster service
+         */
+        public void init(Client client, ClusterService clusterService) {
+            this.client = client;
+            this.clusterService = clusterService;
+        }
+
+        @Override
+        public SearchAlertsTool create(Map<String, Object> map) {
+            return new SearchAlertsTool(client, clusterService);
+        }
+
+        @Override
+        public String getDefaultDescription() {
+            return DEFAULT_DESCRIPTION;
+        }
+    }
+
+}
---
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/tools/SearchAlertsToolTests.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.engine.tools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opensearch.action.ActionType;
+import org.opensearch.client.AdminClient;
+import org.opensearch.client.ClusterAdminClient;
+import org.opensearch.client.IndicesAdminClient;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.commons.alerting.action.GetAlertsResponse;
+import org.opensearch.commons.alerting.model.Alert;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.ml.common.spi.tools.Tool;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class SearchAlertsToolTests {
+    @Mock
+    private NodeClient nodeClient;
+    @Mock
+    private AdminClient adminClient;
+    @Mock
+    private IndicesAdminClient indicesAdminClient;
+    @Mock
+    private ClusterAdminClient clusterAdminClient;
+    @Mock
+    private ClusterService clusterService;
+
+    private Map<String, String> indicesParams;
+    private Map<String, String> otherParams;
+    private Map<String, String> emptyParams;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        SearchAlertsTool.Factory.getInstance().init(nodeClient, clusterService);
+
+        indicesParams = Map.of("index", "[\"foo\"]");
+        otherParams = Map.of("other", "[\"bar\"]");
+        emptyParams = Collections.emptyMap();
+    }
+
+    @Test
+    public void testRunWithNoAlerts() throws Exception {
+        Tool tool = SearchAlertsTool.Factory.getInstance().create(Collections.emptyMap());
+        GetAlertsResponse getAlertsResponse = new GetAlertsResponse(Collections.emptyList(), 0);
+
+        @SuppressWarnings("unchecked")
+        ActionListener<GetAlertsResponse> listener = Mockito.mock(ActionListener.class);
+
+        doAnswer((invocation) -> {
+            ActionListener<GetAlertsResponse> responseListener = invocation.getArgument(2);
+            responseListener.onResponse(getAlertsResponse);
+            return null;
+        }).when(nodeClient).execute(any(ActionType.class), any(), any());
+
+        tool.run(otherParams, listener);
+        ArgumentCaptor<GetAlertsResponse> responseCaptor = ArgumentCaptor.forClass(GetAlertsResponse.class);
+        verify(listener, times(1)).onResponse(responseCaptor.capture());
+        assertEquals((Integer) 0, responseCaptor.getValue().getTotalAlerts());
+    }
+
+    @Test
+    public void testRunWithAlerts() throws Exception {
+        Tool tool = SearchAlertsTool.Factory.getInstance().create(Collections.emptyMap());
+        Alert alert1 = new Alert(
+            "alert-id-1",
+            1234,
+            1,
+            "monitor-id",
+            "workflow-id",
+            "workflow-name",
+            "monitor-name",
+            1234,
+            null,
+            "trigger-id",
+            "trigger-name",
+            Collections.emptyList(),
+            Collections.emptyList(),
+            Alert.State.ACKNOWLEDGED,
+            Instant.now(),
+            null,
+            null,
+            null,
+            null,
+            Collections.emptyList(),
+            "test-severity",
+            Collections.emptyList(),
+            null,
+            null,
+            Collections.emptyList()
+        );
+        Alert alert2 = new Alert(
+            "alert-id-2",
+            1234,
+            1,
+            "monitor-id",
+            "workflow-id",
+            "workflow-name",
+            "monitor-name",
+            1234,
+            null,
+            "trigger-id",
+            "trigger-name",
+            Collections.emptyList(),
+            Collections.emptyList(),
+            Alert.State.ACKNOWLEDGED,
+            Instant.now(),
+            null,
+            null,
+            null,
+            null,
+            Collections.emptyList(),
+            "test-severity",
+            Collections.emptyList(),
+            null,
+            null,
+            Collections.emptyList()
+        );
+        List<Alert> mockAlerts = List.of(alert1, alert2);
+
+        GetAlertsResponse getAlertsResponse = new GetAlertsResponse(mockAlerts, mockAlerts.size());
+
+        @SuppressWarnings("unchecked")
+        ActionListener<GetAlertsResponse> listener = Mockito.mock(ActionListener.class);
+
+        doAnswer((invocation) -> {
+            ActionListener<GetAlertsResponse> responseListener = invocation.getArgument(2);
+            responseListener.onResponse(getAlertsResponse);
+            return null;
+        }).when(nodeClient).execute(any(ActionType.class), any(), any());
+
+        tool.run(otherParams, listener);
+        ArgumentCaptor<GetAlertsResponse> responseCaptor = ArgumentCaptor.forClass(GetAlertsResponse.class);
+        verify(listener, times(1)).onResponse(responseCaptor.capture());
+        assertEquals((Integer) mockAlerts.size(), responseCaptor.getValue().getTotalAlerts());
+    }
+
+    @Test
+    public void testValidate() {
+        Tool tool = SearchAlertsTool.Factory.getInstance().create(Collections.emptyMap());
+        assertEquals(SearchAlertsTool.NAME, tool.getName());
+        assertTrue(tool.validate(indicesParams));
+        assertTrue(tool.validate(otherParams));
+        assertFalse(tool.validate(emptyParams));
+    }
+}
