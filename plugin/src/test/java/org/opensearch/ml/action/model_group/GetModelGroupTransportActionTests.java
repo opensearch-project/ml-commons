db719cf69f66e090aa0f372ce9038507b02c0188
Bhavana Ramaram <rbhavna@amazon.com>
2023-11-28 02:35:56 +0530

    Get model group API (#1670)
    * Get model group API
    
    Signed-off-by: Bhavana Ramaram <rbhavna@amazon.com>

---
+++ plugin/src/test/java/org/opensearch/ml/action/model_group/GetModelGroupTransportActionTests.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.action.model_group;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opensearch.action.get.GetResponse;
+import org.opensearch.action.support.ActionFilters;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.common.bytes.BytesReference;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.core.xcontent.ToXContent;
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.index.IndexNotFoundException;
+import org.opensearch.index.get.GetResult;
+import org.opensearch.ml.common.MLModelGroup;
+import org.opensearch.ml.common.transport.model_group.MLModelGroupGetRequest;
+import org.opensearch.ml.common.transport.model_group.MLModelGroupGetResponse;
+import org.opensearch.ml.helper.ModelAccessControlHelper;
+import org.opensearch.test.OpenSearchTestCase;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.transport.TransportService;
+
+public class GetModelGroupTransportActionTests extends OpenSearchTestCase {
+    @Mock
+    ThreadPool threadPool;
+
+    @Mock
+    Client client;
+
+    @Mock
+    NamedXContentRegistry xContentRegistry;
+
+    @Mock
+    TransportService transportService;
+
+    @Mock
+    ActionFilters actionFilters;
+
+    @Mock
+    ActionListener<MLModelGroupGetResponse> actionListener;
+
+    @Mock
+    ClusterService clusterService;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    GetModelGroupTransportAction getModelGroupTransportAction;
+    MLModelGroupGetRequest mlModelGroupGetRequest;
+    ThreadContext threadContext;
+
+    @Mock
+    private ModelAccessControlHelper modelAccessControlHelper;
+
+    @Before
+    public void setup() throws IOException {
+        MockitoAnnotations.openMocks(this);
+        mlModelGroupGetRequest = MLModelGroupGetRequest.builder().modelGroupId("test_id").build();
+        Settings settings = Settings.builder().build();
+
+        getModelGroupTransportAction = spy(
+            new GetModelGroupTransportAction(
+                transportService,
+                actionFilters,
+                client,
+                xContentRegistry,
+                clusterService,
+                modelAccessControlHelper
+            )
+        );
+
+        doAnswer(invocation -> {
+            ActionListener<Boolean> listener = invocation.getArgument(3);
+            listener.onResponse(true);
+            return null;
+        }).when(modelAccessControlHelper).validateModelGroupAccess(any(), any(), any(), any());
+
+        threadContext = new ThreadContext(settings);
+        when(client.threadPool()).thenReturn(threadPool);
+        when(threadPool.getThreadContext()).thenReturn(threadContext);
+    }
+
+    public void test_Success() throws IOException {
+
+        GetResponse getResponse = prepareMLModelGroup();
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> listener = invocation.getArgument(1);
+            listener.onResponse(getResponse);
+            return null;
+        }).when(client).get(any(), any());
+
+        getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
+        ArgumentCaptor<MLModelGroupGetResponse> argumentCaptor = ArgumentCaptor.forClass(MLModelGroupGetResponse.class);
+        verify(actionListener).onResponse(argumentCaptor.capture());
+    }
+
+    public void testGetModel_UserHasNoAccess() throws IOException {
+        doAnswer(invocation -> {
+            ActionListener<Boolean> listener = invocation.getArgument(3);
+            listener.onResponse(false);
+            return null;
+        }).when(modelAccessControlHelper).validateModelGroupAccess(any(), any(), any(), any());
+
+        GetResponse getResponse = prepareMLModelGroup();
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> listener = invocation.getArgument(1);
+            listener.onResponse(getResponse);
+            return null;
+        }).when(client).get(any(), any());
+
+        getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
+        ArgumentCaptor<Exception> argumentCaptor = ArgumentCaptor.forClass(Exception.class);
+        verify(actionListener).onFailure(argumentCaptor.capture());
+        assertEquals("User doesn't have privilege to perform this operation on this model group", argumentCaptor.getValue().getMessage());
+    }
+
+    public void testGetModel_ValidateAccessFailed() throws IOException {
+        doAnswer(invocation -> {
+            ActionListener<Boolean> listener = invocation.getArgument(3);
+            listener.onFailure(new Exception("Failed to validate access"));
+            return null;
+        }).when(modelAccessControlHelper).validateModelGroupAccess(any(), any(), any(), any());
+
+        GetResponse getResponse = prepareMLModelGroup();
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> listener = invocation.getArgument(1);
+            listener.onResponse(getResponse);
+            return null;
+        }).when(client).get(any(), any());
+
+        getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
+        ArgumentCaptor<Exception> argumentCaptor = ArgumentCaptor.forClass(Exception.class);
+        verify(actionListener).onFailure(argumentCaptor.capture());
+        assertEquals("Failed to validate access", argumentCaptor.getValue().getMessage());
+    }
+
+    public void testGetModel_NullResponse() {
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> listener = invocation.getArgument(1);
+            listener.onResponse(null);
+            return null;
+        }).when(client).get(any(), any());
+        getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
+        ArgumentCaptor<Exception> argumentCaptor = ArgumentCaptor.forClass(Exception.class);
+        verify(actionListener).onFailure(argumentCaptor.capture());
+        assertEquals("Failed to find model group with the provided model group id: test_id", argumentCaptor.getValue().getMessage());
+    }
+
+    public void testGetModel_IndexNotFoundException() {
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> listener = invocation.getArgument(1);
+            listener.onFailure(new IndexNotFoundException("Fail to find model group index"));
+            return null;
+        }).when(client).get(any(), any());
+        getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
+        ArgumentCaptor<Exception> argumentCaptor = ArgumentCaptor.forClass(Exception.class);
+        verify(actionListener).onFailure(argumentCaptor.capture());
+        assertEquals("Fail to find model group index", argumentCaptor.getValue().getMessage());
+    }
+
+    public void testGetModel_RuntimeException() {
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> listener = invocation.getArgument(1);
+            listener.onFailure(new RuntimeException("errorMessage"));
+            return null;
+        }).when(client).get(any(), any());
+        getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
+        ArgumentCaptor<Exception> argumentCaptor = ArgumentCaptor.forClass(Exception.class);
+        verify(actionListener).onFailure(argumentCaptor.capture());
+        assertEquals("errorMessage", argumentCaptor.getValue().getMessage());
+    }
+
+    public GetResponse prepareMLModelGroup() throws IOException {
+        MLModelGroup mlModelGroup = MLModelGroup
+            .builder()
+            .modelGroupId("test_id")
+            .name("modelGroup")
+            .description("this is an example description")
+            .latestVersion(1)
+            .access("private")
+            .build();
+        XContentBuilder content = mlModelGroup.toXContent(XContentFactory.jsonBuilder(), ToXContent.EMPTY_PARAMS);
+        BytesReference bytesReference = BytesReference.bytes(content);
+        GetResult getResult = new GetResult("indexName", "111", 111l, 111l, 111l, true, bytesReference, null, null);
+        GetResponse getResponse = new GetResponse(getResult);
+        return getResponse;
+    }
+}
