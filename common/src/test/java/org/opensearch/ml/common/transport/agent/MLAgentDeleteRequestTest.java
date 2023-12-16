253c09857e842335e61e43b5433b370dbd512a8e
Mingshi Liu <113382730+mingshl@users.noreply.github.com>
2023-12-16 11:27:00 -0800

    Get and delete agent APIs (#1752)
    * get and delete agent APIs (#1703)
    
    Signed-off-by: Bhavana Ramaram <rbhavna@amazon.com>
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    
    * Add unit tests for Get and Delete APIs
    
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    
    * Add header and increase code coverage
    
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    
    * change IndexNotFoundException error message
    
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    
    ---------
    
    Signed-off-by: Bhavana Ramaram <rbhavna@amazon.com>
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    Signed-off-by: Mingshi Liu <113382730+mingshl@users.noreply.github.com>
    (cherry picked from commit 4d8d32e73845ead7574d9bac3ce77651c2158404)

---
+++ common/src/test/java/org/opensearch/ml/common/transport/agent/MLAgentDeleteRequestTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.ml.common.transport.agent;
+
+import org.junit.Test;
+import org.opensearch.action.ActionRequestValidationException;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.opensearch.action.ValidateActions.addValidationError;
+
+public class MLAgentDeleteRequestTest {
+    String agentId;
+
+    @Test
+    public void constructor_AgentId() {
+        agentId = "test-abc";
+        MLAgentDeleteRequest mLAgentDeleteRequest = new MLAgentDeleteRequest(agentId);
+        assertEquals(mLAgentDeleteRequest.agentId,agentId);
+    }
+
+    @Test
+    public void writeTo() throws IOException {
+        agentId = "test-hij";
+
+        MLAgentDeleteRequest mLAgentDeleteRequest = new MLAgentDeleteRequest(agentId);
+        BytesStreamOutput output = new BytesStreamOutput();
+        mLAgentDeleteRequest.writeTo(output);
+
+        MLAgentDeleteRequest mLAgentDeleteRequest1 = new MLAgentDeleteRequest(output.bytes().streamInput());
+
+        assertEquals(mLAgentDeleteRequest.agentId, mLAgentDeleteRequest1.agentId);
+        assertEquals(agentId, mLAgentDeleteRequest1.agentId);
+    }
+
+    @Test
+    public void validate_Success() {
+        agentId = "not-null";
+        MLAgentDeleteRequest mLAgentDeleteRequest = new MLAgentDeleteRequest(agentId);
+
+        assertEquals(null, mLAgentDeleteRequest.validate());
+    }
+
+    @Test
+    public void validate_Failure() {
+        agentId = null;
+        MLAgentDeleteRequest mLAgentDeleteRequest = new MLAgentDeleteRequest(agentId);
+        assertEquals(null,mLAgentDeleteRequest.agentId);
+
+        ActionRequestValidationException exception = addValidationError("ML agent id can't be null", null);
+        mLAgentDeleteRequest.validate().equals(exception) ;
+    }
+
+    @Test
+    public void fromActionRequest() throws IOException {
+        agentId = "test-lmn";
+        MLAgentDeleteRequest mLAgentDeleteRequest = new MLAgentDeleteRequest(agentId);
+        assertEquals(mLAgentDeleteRequest.fromActionRequest(mLAgentDeleteRequest), mLAgentDeleteRequest);
+
+    }
+}
---
+++ common/src/test/java/org/opensearch/ml/common/transport/agent/MLAgentGetResponseTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.transport.agent;
+import org.junit.Test;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.core.common.io.stream.*;
+import org.opensearch.core.xcontent.ToXContent;
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.ml.common.agent.LLMSpec;
+import org.opensearch.ml.common.agent.MLAgent;
+import org.opensearch.ml.common.agent.MLMemorySpec;
+import org.opensearch.ml.common.agent.MLToolSpec;
+
+import java.io.*;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import static org.junit.Assert.assertEquals;
+import static org.opensearch.core.xcontent.ToXContent.EMPTY_PARAMS;
+
+public class MLAgentGetResponseTest {
+
+    MLAgent mlAgent;
+
+    @Test
+    public void Create_MLAgentResponse_With_StreamInput() throws IOException {
+        // Create a BytesStreamOutput to simulate the StreamOutput
+        BytesStreamOutput bytesStreamOutput = new BytesStreamOutput();
+
+        //create a test agent using input
+        bytesStreamOutput.writeString("Test Agent");
+        bytesStreamOutput.writeString("flow");
+        bytesStreamOutput.writeBoolean(false);
+        bytesStreamOutput.writeBoolean(false);
+        bytesStreamOutput.writeBoolean(false);
+        bytesStreamOutput.writeBoolean(false);
+        bytesStreamOutput.writeBoolean(false);
+        bytesStreamOutput.writeInstant(Instant.parse("2023-12-31T12:00:00Z"));
+        bytesStreamOutput.writeInstant(Instant.parse("2023-12-31T12:00:00Z"));
+        bytesStreamOutput.writeString("test");
+
+        StreamInput testInputStream = bytesStreamOutput.bytes().streamInput();
+
+        MLAgentGetResponse mlAgentGetResponse = new MLAgentGetResponse(testInputStream);
+        MLAgent testMlAgent = mlAgentGetResponse.mlAgent;
+        assertEquals("flow",testMlAgent.getType());
+        assertEquals("Test Agent",testMlAgent.getName());
+        assertEquals("test",testMlAgent.getAppType());
+    }
+
+    @Test
+    public void mLAgentGetResponse_Builder() throws IOException {
+
+        MLAgentGetResponse mlAgentGetResponse = MLAgentGetResponse.builder()
+                .mlAgent(mlAgent)
+                .build();
+
+        assertEquals(mlAgentGetResponse.mlAgent, mlAgent);
+    }
+    @Test
+    public void writeTo() throws IOException {
+        //create ml agent using MLAgent and mlAgentGetResponse
+        mlAgent = new MLAgent("test", "test", "test", new LLMSpec("test_model", Map.of("test_key", "test_value")), List.of(new MLToolSpec("test", "test", "test", Collections.EMPTY_MAP, false)), Map.of("test", "test"), new MLMemorySpec("test", "123", 0), Instant.EPOCH, Instant.EPOCH, "test");
+        MLAgentGetResponse mlAgentGetResponse = MLAgentGetResponse.builder()
+                .mlAgent(mlAgent)
+                .build();
+        //use write out for both agents
+        BytesStreamOutput output = new BytesStreamOutput();
+        mlAgent.writeTo(output);
+        mlAgentGetResponse.writeTo(output);
+        MLAgent agent1 = mlAgentGetResponse.mlAgent;
+
+        assertEquals(mlAgent.getAppType(), agent1.getAppType());
+        assertEquals(mlAgent.getDescription(), agent1.getDescription());
+        assertEquals(mlAgent.getCreatedTime(), agent1.getCreatedTime());
+        assertEquals(mlAgent.getName(), agent1.getName());
+        assertEquals(mlAgent.getParameters(), agent1.getParameters());
+        assertEquals(mlAgent.getType(), agent1.getType());
+    }
+
+    @Test
+    public void toXContent() throws IOException {
+        mlAgent = new MLAgent("mock", "flow", "test", null, null, null, null, null, null, "test");
+        MLAgentGetResponse mlAgentGetResponse = MLAgentGetResponse.builder()
+                .mlAgent(mlAgent)
+                .build();
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        ToXContent.Params params = EMPTY_PARAMS;
+        XContentBuilder getResponseXContentBuilder = mlAgentGetResponse.toXContent(builder, params);
+        assertEquals(getResponseXContentBuilder, mlAgent.toXContent(builder, params));
+    }
+
+    @Test
+    public void FromActionResponse() throws IOException {
+        MLAgentGetResponse mlAgentGetResponse = MLAgentGetResponse.builder()
+                .mlAgent(mlAgent)
+                .build();
+        assertEquals(mlAgentGetResponse.fromActionResponse(mlAgentGetResponse), mlAgentGetResponse);
+
+        }
+    }
