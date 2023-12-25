d8b6d3c10c26b1fd9b08fededa133a6d81f9704d
Yuye Zhu <yuyezhu@amazon.com>
2023-12-25 11:50:15 +0800

    Update common/src/main/java/org/opensearch/ml/common/transport/agent/MLSearchAgentAction.java
    Co-authored-by: Yaliang Wu <ylwu@amazon.com>
    Signed-off-by: Yuye Zhu <yuyezhu@amazon.com>

--- common/src/main/java/org/opensearch/ml/common/transport/agent/MLSearchAgentAction.java
+++ common/src/main/java/org/opensearch/ml/common/transport/agent/MLSearchAgentAction.java
@@ -10,7 +10,7 @@ import org.opensearch.action.search.SearchResponse;
 
 public class MLSearchAgentAction extends ActionType<SearchResponse> {
     // External Action which used for public facing RestAPIs.
-    public static final String NAME = "cluster:admin/opensearch/ml/search_agent";
+    public static final String NAME = "cluster:admin/opensearch/ml/agents/search";
     public static final MLSearchAgentAction INSTANCE = new MLSearchAgentAction();
 
     private MLSearchAgentAction() {
