9155655c22d9d4c93a83054963c233c1b2ccda15
Yuye Zhu <yuyezhu@amazon.com>
2023-12-26 10:09:54 +0800

    feat: search agent API (#1722)
    * implement search agent API
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    remove extra exludes for search agent
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    resolve conflicts
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    run spotless apply
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    move search agent function into transport action
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    run spotless apply
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    add unit tests for rest ml search agent action
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    add unit tests for transport ml search agent action
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    run spotless apply
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    optimize lambda expression
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * change error message for transport search agent action
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * Update common/src/main/java/org/opensearch/ml/common/transport/agent/MLSearchAgentAction.java
    
    Co-authored-by: Yaliang Wu <ylwu@amazon.com>
    Signed-off-by: Yuye Zhu <yuyezhu@amazon.com>
    
    * fix comment
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * update ut for rest search agent action
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * rename to agent response
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * update tests to cover onresponse and onfailure
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    ---------
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    Signed-off-by: Yuye Zhu <yuyezhu@amazon.com>
    Co-authored-by: Yaliang Wu <ylwu@amazon.com>

---
+++ plugin/src/main/java/org/opensearch/ml/rest/RestMLSearchAgentAction.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.rest;
+
+import static org.opensearch.ml.common.CommonValue.ML_AGENT_INDEX;
+import static org.opensearch.ml.plugin.MachineLearningPlugin.ML_BASE_URI;
+
+import org.opensearch.ml.common.agent.MLAgent;
+import org.opensearch.ml.common.transport.agent.MLSearchAgentAction;
+import org.opensearch.ml.repackage.com.google.common.collect.ImmutableList;
+
+/**
+ * This class consists of the REST handler to search ML Agents.
+ */
+public class RestMLSearchAgentAction extends AbstractMLSearchAction<MLAgent> {
+    private static final String ML_SEARCH_AGENT_ACTION = "ml_search_agent_action";
+    private static final String SEARCH_AGENT_PATH = ML_BASE_URI + "/agents/_search";
+
+    public RestMLSearchAgentAction() {
+        super(ImmutableList.of(SEARCH_AGENT_PATH), ML_AGENT_INDEX, MLAgent.class, MLSearchAgentAction.INSTANCE);
+    }
+
+    @Override
+    public String getName() {
+        return ML_SEARCH_AGENT_ACTION;
+    }
+}
