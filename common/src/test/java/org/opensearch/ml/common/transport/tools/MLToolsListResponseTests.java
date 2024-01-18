682166d44aa0bd42a00a66a30f21ee31462ba63d
Jackie Han <jkhanjob@gmail.com>
2024-01-18 14:28:01 -0800

    cleanup
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>

--- common/src/test/java/org/opensearch/ml/common/transport/tools/MLToolsListResponseTests.java
+++ common/src/test/java/org/opensearch/ml/common/transport/tools/MLToolsListResponseTests.java
@@ -67,7 +67,6 @@ public class MLToolsListResponseTests {
         mlToolsListResponse.toXContent(builder, ToXContent.EMPTY_PARAMS);
         assertNotNull(builder);
         String jsonStr = builder.toString();
-        System.out.println(jsonStr);
         assertEquals("[{\"name\":\"SearchWikipediaTool\",\"description\":\"Useful when you need to use this tool to search general knowledge on wikipedia.\",\"type\":\"SearchWikipediaTool\",\"version\":\"undefined\"},{\"name\":\"MathTool\",\"description\":\"Use this tool to calculate any math problem.\",\"type\":\"MathTool\",\"version\":\"test\"}]", jsonStr);
     }
 
