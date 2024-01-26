849fecfa7e012c55a0dd3091069db324e5831db8
Daniel Widdis <widdis@gmail.com>
2024-01-26 09:37:01 -0800

    Add IndexMapping Tool (#1891)
    * Add IndexMapping Tool
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Improve description and make index a required parameter
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add some more test coverage
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
+++ plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
@@ -164,6 +164,7 @@ import org.opensearch.ml.engine.memory.ConversationIndexMemory;
 import org.opensearch.ml.engine.memory.MLMemoryManager;
 import org.opensearch.ml.engine.tools.AgentTool;
 import org.opensearch.ml.engine.tools.CatIndexTool;
+import org.opensearch.ml.engine.tools.IndexMappingTool;
 import org.opensearch.ml.engine.tools.MLModelTool;
 import org.opensearch.ml.helper.ConnectorAccessControlHelper;
 import org.opensearch.ml.helper.ModelAccessControlHelper;
@@ -551,10 +552,12 @@ public class MachineLearningPlugin extends Plugin implements ActionPlugin, Searc
         MLModelTool.Factory.getInstance().init(client);
         AgentTool.Factory.getInstance().init(client);
         CatIndexTool.Factory.getInstance().init(client, clusterService);
+        IndexMappingTool.Factory.getInstance().init(client);
 
         toolFactories.put(MLModelTool.TYPE, MLModelTool.Factory.getInstance());
         toolFactories.put(AgentTool.TYPE, AgentTool.Factory.getInstance());
         toolFactories.put(CatIndexTool.TYPE, CatIndexTool.Factory.getInstance());
+        toolFactories.put(IndexMappingTool.TYPE, IndexMappingTool.Factory.getInstance());
 
         if (externalToolFactories != null) {
             toolFactories.putAll(externalToolFactories);
