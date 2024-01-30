5ecf3f26615c71bced1be2a13bd7bcd2cc5c0a66
zane-neo <zaniu@amazon.com>
2024-01-30 09:17:29 +0800

    Add UTs
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/remote/MLSdkAsyncHttpResponseHandlerTest.java
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/remote/MLSdkAsyncHttpResponseHandlerTest.java
@@ -13,15 +13,19 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.ml.common.connector.Connector;
+import org.opensearch.ml.common.connector.ConnectorAction;
+import org.opensearch.ml.common.connector.HttpConnector;
 import org.opensearch.ml.common.output.model.ModelTensors;
 import org.opensearch.script.ScriptService;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscription;
 import software.amazon.awssdk.http.HttpStatusCode;
 import software.amazon.awssdk.http.SdkHttpFullResponse;
+import software.amazon.awssdk.http.SdkHttpResponse;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
@@ -32,14 +36,13 @@ import static org.mockito.Mockito.when;
 
 public class MLSdkAsyncHttpResponseHandlerTest {
 
-    private WrappedCountDownLatch countDownLatch = new WrappedCountDownLatch(0, new CountDownLatch(1));
+    private final WrappedCountDownLatch countDownLatch = new WrappedCountDownLatch(0, new CountDownLatch(1));
 @Mock
     private ActionListener<List<ModelTensors>> actionListener;
 @Mock
     private Map<String, String> parameters;
 @Mock
     private Map<Integer, ModelTensors> tensorOutputs;
-@Mock
     private Connector connector;
 
 @Mock
@@ -54,8 +57,22 @@ private MLSdkAsyncHttpResponseHandler mlSdkAsyncHttpResponseHandler;
     @Before
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        mlSdkAsyncHttpResponseHandler = new MLSdkAsyncHttpResponseHandler(countDownLatch, actionListener, parameters, tensorOutputs, connector, scriptService);
         when(sdkHttpResponse.statusCode()).thenReturn(HttpStatusCode.OK);
+        ConnectorAction predictAction = ConnectorAction
+            .builder()
+            .actionType(ConnectorAction.ActionType.PREDICT)
+            .method("POST")
+            .url("http://test.com/mock")
+            .requestBody("{\"input\": \"${parameters.input}\"}")
+            .build();
+        connector = HttpConnector
+            .builder()
+            .name("test connector")
+            .version("1")
+            .protocol("http")
+            .actions(Arrays.asList(predictAction))
+            .build();
+        mlSdkAsyncHttpResponseHandler = new MLSdkAsyncHttpResponseHandler(countDownLatch, actionListener, parameters, tensorOutputs, connector, scriptService);
         responseSubscriber = mlSdkAsyncHttpResponseHandler.new MLResponseSubscriber();
     }
 
@@ -104,7 +121,61 @@ private MLSdkAsyncHttpResponseHandler mlSdkAsyncHttpResponseHandler;
     }
 
     @Test
-    public void test_onComplete() throws IOException {
+    public void test_onComplete_success() {
+        String response = "{\n" + "    \"embedding\": [\n" + "        0.46484375,\n" + "        -0.017822266,\n" + "        0.17382812,\n"
+            + "        0.10595703,\n" + "        0.875,\n" + "        0.19140625,\n" + "        -0.36914062,\n" + "        -0.0011978149\n"
+            + "    ]\n" + "}";
+        mlSdkAsyncHttpResponseHandler.onHeaders(sdkHttpResponse);
+        Publisher<ByteBuffer> stream = s -> {
+            try {
+                s.onSubscribe(mock(Subscription.class));
+                s.onNext(ByteBuffer.wrap(response.getBytes()));
+                s.onComplete();
+            } catch (Throwable e) {
+                s.onError(e);
+            }
+        };
+        mlSdkAsyncHttpResponseHandler.onStream(stream);
+        responseSubscriber.onComplete();
+    }
+
+    @Test
+    public void test_onComplete_empty_response_body() {
+        mlSdkAsyncHttpResponseHandler.onHeaders(sdkHttpResponse);
+        Publisher<ByteBuffer> stream = s -> {
+            try {
+                s.onSubscribe(mock(Subscription.class));
+                s.onNext(ByteBuffer.wrap("".getBytes()));
+                s.onComplete();
+            } catch (Throwable e) {
+                s.onError(e);
+            }
+        };
+        mlSdkAsyncHttpResponseHandler.onStream(stream);
+        responseSubscriber.onComplete();
+    }
+
+    @Test
+    public void test_onComplete_error_http_status() {
+        String error = "{\n" + "    \"message\": \"runtime error\"\n" + "}";
+        SdkHttpResponse response = mock(SdkHttpFullResponse.class);
+        when(response.statusCode()).thenReturn(HttpStatusCode.INTERNAL_SERVER_ERROR);
+        mlSdkAsyncHttpResponseHandler.onHeaders(response);
+        Publisher<ByteBuffer> stream = s -> {
+            try {
+                s.onSubscribe(mock(Subscription.class));
+                s.onNext(ByteBuffer.wrap(error.getBytes()));
+                s.onComplete();
+            } catch (Throwable e) {
+                s.onError(e);
+            }
+        };
+        mlSdkAsyncHttpResponseHandler.onStream(stream);
+        responseSubscriber.onComplete();
+    }
+
+    @Test
+    public void test_onComplete_error() {
         mlSdkAsyncHttpResponseHandler.onHeaders(sdkHttpResponse);
         Publisher<ByteBuffer> stream = s -> {
             try {
@@ -116,10 +187,6 @@ private MLSdkAsyncHttpResponseHandler mlSdkAsyncHttpResponseHandler;
             }
         };
         mlSdkAsyncHttpResponseHandler.onStream(stream);
-        mockStatic(ConnectorUtils.class);
-        ModelTensors modelTensors = mock(ModelTensors.class);
-        when(ConnectorUtils.processOutput(mlSdkAsyncHttpResponseHandler.getResponseBody().toString(), connector, scriptService, parameters)).thenReturn(modelTensors);
-        when(modelTensors.getStatusCode()).thenReturn(200);
         responseSubscriber.onComplete();
     }
 }
