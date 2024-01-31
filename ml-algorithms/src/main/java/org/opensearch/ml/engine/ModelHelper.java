8a6328586873e7040a20b75e1242c0d10e8069fe
xinyual <74362153+xinyual@users.noreply.github.com>
2024-02-01 05:02:47 +0800

    fix error message (#1967)
    * fix error message
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * modify error message
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    ---------
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    (cherry picked from commit 0fd07f0b843b0bfc14dd3b2d4145eca0ba41568e)

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/ModelHelper.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/ModelHelper.java
@@ -227,7 +227,12 @@ public class ModelHelper {
                 DownloadUtils.download(url, modelPath, new ProgressBar());
                 verifyModelZipFile(modelFormat, modelPath, modelName, functionName);
                 String hash = calculateFileHash(modelZipFile);
-                if (hash.equals(modelContentHash)) {
+                if (modelContentHash == null) {
+                    log.error("Hash code need to be provided when register via url.");
+                    throw (new IllegalArgumentException(
+                        "Model content Hash code need to be provided when register via url. Please calculate sha 256 Hash code."
+                    ));
+                } else if (hash.equals(modelContentHash)) {
                     List<String> chunkFiles = splitFileIntoChunks(modelZipFile, modelPartsPath, CHUNK_SIZE);
                     Map<String, Object> result = new HashMap<>();
                     result.put(CHUNK_FILES, chunkFiles);
