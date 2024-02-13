02bbc3a23e6aac861feac9f9514dbdb94879712b
Sicheng Song <sicheng.song@outlook.com>
2024-02-13 01:05:03 +0000

    Fix bwc issue in remote prediction
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

--- common/src/main/java/org/opensearch/ml/common/dataset/MLInputDataType.java
+++ common/src/main/java/org/opensearch/ml/common/dataset/MLInputDataType.java
@@ -5,10 +5,13 @@
 
 package org.opensearch.ml.common.dataset;
 
+// Please strictly added new MLInputDataType to the last line
+// and avoid altering the order of the existing MLInputDataType,
+// or it will break the backward compatibility!
 public enum MLInputDataType {
     SEARCH_QUERY,
     DATA_FRAME,
     TEXT_DOCS,
-    TEXT_SIMILARITY,
-    REMOTE
+    REMOTE,
+    TEXT_SIMILARITY
 }
