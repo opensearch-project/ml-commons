7f4ac4e7fc7ed5fc398130c2ed1d47f1a9c514d6
Jing Zhang <jngz@amazon.com>
2024-02-27 14:44:04 -0800

    add more UT
    Signed-off-by: Jing Zhang <jngz@amazon.com>

--- ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/agent/MLAgentExecutorTest.java
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/agent/MLAgentExecutorTest.java
@@ -189,6 +189,13 @@ public class MLAgentExecutorTest {
         mlAgentExecutor.execute(agentMLInput, agentActionListener);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void test_NonInputParas_ThrowsException() {
+        RemoteInferenceInputDataSet inputDataSet = RemoteInferenceInputDataSet.builder().parameters(null).build();
+        AgentMLInput agentMLInput = new AgentMLInput("test", FunctionName.AGENT, inputDataSet);
+        mlAgentExecutor.execute(agentMLInput, agentActionListener);
+    }
+
     @Test
     public void test_HappyCase_ReturnsResult() {
         ModelTensor modelTensor = ModelTensor.builder().name("response").dataAsMap(ImmutableMap.of("test_key", "test_value")).build();
