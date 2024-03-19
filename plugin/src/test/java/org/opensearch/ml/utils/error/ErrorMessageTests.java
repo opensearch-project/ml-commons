64ffa75ba0fc7fdce37a2b945990ceb0b6d1fd7a
Jing Zhang <jngz@amazon.com>
2024-03-18 22:24:16 -0700

    prevent exposing internal ip (#2154)
    * prevent exposing internal ip
    
    Signed-off-by: Jing Zhang <jngz@amazon.com>
    
    * add final
    
    Signed-off-by: Jing Zhang <jngz@amazon.com>
    
    * add ipv6 support
    
    Signed-off-by: Jing Zhang <jngz@amazon.com>
    
    ---------
    
    Signed-off-by: Jing Zhang <jngz@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/utils/error/ErrorMessageTests.java
+++ plugin/src/test/java/org/opensearch/ml/utils/error/ErrorMessageTests.java
@@ -10,7 +10,12 @@ import static org.junit.Assert.assertEquals;
 import static org.opensearch.core.rest.RestStatus.BAD_REQUEST;
 import static org.opensearch.core.rest.RestStatus.SERVICE_UNAVAILABLE;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.junit.Test;
+import org.opensearch.core.common.transport.TransportAddress;
+import org.opensearch.transport.ActionTransportException;
 
 public class ErrorMessageTests {
 
@@ -26,6 +31,70 @@ public class ErrorMessageTests {
         ErrorMessage errorMessage = new ErrorMessage(new IllegalStateException("illegal state"), SERVICE_UNAVAILABLE.getStatus());
 
         assertEquals(errorMessage.fetchDetails(), "illegal state");
+    }
+
+    @Test
+    public void fetchDetailsWithPrivateIPv4() throws UnknownHostException {
+        InetAddress ipAddress = InetAddress.getByName("192.168.1.1");
+        Throwable throwable = new ActionTransportException(
+            "node 1",
+            new TransportAddress(ipAddress, 9300),
+            null,
+            "Node not connected",
+            null
+        );
+        ErrorMessage errorMessage = new ErrorMessage(throwable, SERVICE_UNAVAILABLE.getStatus());
+
+        assertEquals(throwable.getLocalizedMessage(), "[node 1][192.168.1.1:9300] Node not connected");
+        assertEquals(errorMessage.fetchDetails(), "[node 1][x.x.x.x:x] Node not connected");
+    }
+
+    @Test
+    public void fetchDetailsWithPrivateIPv6_0() throws UnknownHostException {
+        InetAddress ipAddress = InetAddress.getByName("0:0:0:0:0:0:0:1");
+        Throwable throwable = new ActionTransportException(
+            "node 1",
+            new TransportAddress(ipAddress, 9300),
+            null,
+            "Node not connected",
+            null
+        );
+        ErrorMessage errorMessage = new ErrorMessage(throwable, SERVICE_UNAVAILABLE.getStatus());
+
+        assertEquals(throwable.getLocalizedMessage(), "[node 1][[::1]:9300] Node not connected");
+        assertEquals(errorMessage.fetchDetails(), "[node 1][x.x.x.x.x.x:x] Node not connected");
+    }
+
+    @Test
+    public void fetchDetailsWithPrivateIPv6_1() throws UnknownHostException {
+        InetAddress ipAddress = InetAddress.getByName("::1");
+        Throwable throwable = new ActionTransportException(
+            "node 1",
+            new TransportAddress(ipAddress, 9300),
+            null,
+            "Node not connected",
+            null
+        );
+        ErrorMessage errorMessage = new ErrorMessage(throwable, SERVICE_UNAVAILABLE.getStatus());
+
+        assertEquals(throwable.getLocalizedMessage(), "[node 1][[::1]:9300] Node not connected");
+        assertEquals(errorMessage.fetchDetails(), "[node 1][x.x.x.x.x.x:x] Node not connected");
+    }
+
+    @Test
+    public void fetchDetailsWithPrivateIPv61() throws UnknownHostException {
+        InetAddress ipAddress = InetAddress.getByName("1234:0000:F560:0000:0000:07C0:89AB:222D");
+        Throwable throwable = new ActionTransportException(
+            "node 1",
+            new TransportAddress(ipAddress, 9300),
+            null,
+            "Node not connected",
+            null
+        );
+        ErrorMessage errorMessage = new ErrorMessage(throwable, SERVICE_UNAVAILABLE.getStatus());
+
+        assertEquals(throwable.getLocalizedMessage(), "[node 1][[1234:0:f560::7c0:89ab:222d]:9300] Node not connected");
+        assertEquals(errorMessage.fetchDetails(), "[node 1][x.x.x.x.x.x:x] Node not connected");
     }
 
     @Test
