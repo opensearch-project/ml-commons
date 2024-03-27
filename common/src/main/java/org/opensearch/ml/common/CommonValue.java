412fdd84cf4b8c05414035a5486cd85c8f78fde8
Sicheng Song <sicheng.song@outlook.com>
2024-03-27 21:48:14 +0000

    Bump back schema version due to already bumped in guardrail pr
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

--- common/src/main/java/org/opensearch/ml/common/CommonValue.java
+++ common/src/main/java/org/opensearch/ml/common/CommonValue.java
@@ -56,7 +56,7 @@ public class CommonValue {
         public static final String ML_MODEL_INDEX = ".plugins-ml-model";
         public static final String ML_TASK_INDEX = ".plugins-ml-task";
         public static final Integer ML_MODEL_GROUP_INDEX_SCHEMA_VERSION = 2;
-        public static final Integer ML_MODEL_INDEX_SCHEMA_VERSION = 11;
+        public static final Integer ML_MODEL_INDEX_SCHEMA_VERSION = 10;
         public static final String ML_CONNECTOR_INDEX = ".plugins-ml-connector";
         public static final Integer ML_TASK_INDEX_SCHEMA_VERSION = 2;
         public static final Integer ML_CONNECTOR_SCHEMA_VERSION = 3;
