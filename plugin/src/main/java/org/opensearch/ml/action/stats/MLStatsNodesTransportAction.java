cbb097b16789f3490abebafe28845ce5656ba37d
Dhrubo Saha <dhrubo@amazon.com>
2024-04-22 16:36:05 -0700

    Filtering hidden model info from model profiling for users other than superadmin (#2332)
    * adding changes for profile
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * spotlessApply
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * added is_hidden field in the stats api
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * fixing unit test
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * fixing tests
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * added search with actionListener
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * spotlessApply
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * addressed comments
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * spotlessApply
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * fixing integ test
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * addressing comments
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * spotlessApply
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * addressing comments
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * fixing and adding more tests
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    * spotlessApply
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    
    ---------
    
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>
    (cherry picked from commit 6ac0377535e7e74dade310204eb69f61671c2d1c)

--- plugin/src/main/java/org/opensearch/ml/action/stats/MLStatsNodesTransportAction.java
+++ plugin/src/main/java/org/opensearch/ml/action/stats/MLStatsNodesTransportAction.java
@@ -6,31 +6,22 @@
 package org.opensearch.ml.action.stats;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import org.opensearch.action.FailedNodeException;
-import org.opensearch.action.search.SearchRequest;
-import org.opensearch.action.search.SearchResponse;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.action.support.nodes.TransportNodesAction;
 import org.opensearch.client.Client;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.inject.Inject;
-import org.opensearch.common.util.concurrent.ThreadContext;
-import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.env.Environment;
-import org.opensearch.index.query.BoolQueryBuilder;
-import org.opensearch.index.query.QueryBuilders;
-import org.opensearch.ml.common.CommonValue;
 import org.opensearch.ml.common.FunctionName;
-import org.opensearch.ml.common.MLModel;
 import org.opensearch.ml.model.MLModelManager;
 import org.opensearch.ml.stats.ActionName;
 import org.opensearch.ml.stats.MLActionStats;
@@ -42,7 +33,6 @@ import org.opensearch.ml.stats.MLStats;
 import org.opensearch.ml.stats.MLStatsInput;
 import org.opensearch.ml.utils.RestActionUtils;
 import org.opensearch.monitor.jvm.JvmService;
-import org.opensearch.search.SearchHit;
 import org.opensearch.threadpool.ThreadPool;
 import org.opensearch.transport.TransportService;
 
@@ -122,118 +112,70 @@ public class MLStatsNodesTransportAction extends
         return createMLStatsNodeResponse(request.getMlStatsNodesRequest());
     }
 
-    MLStatsNodeResponse createMLStatsNodeResponse(MLStatsNodesRequest mlStatsNodesRequest) {
-        Map<MLNodeLevelStat, Object> statValues = new HashMap<>();
-        MLStatsInput mlStatsInput = mlStatsNodesRequest.getMlStatsInput();
-        // return node level stats
-        if (mlStatsInput.getTargetStatLevels().contains(MLStatLevel.NODE)) {
-            if (mlStatsInput.retrieveStat(MLNodeLevelStat.ML_JVM_HEAP_USAGE)) {
+    private MLStatsNodeResponse createMLStatsNodeResponse(MLStatsNodesRequest request) {
+        Map<MLNodeLevelStat, Object> nodeLevelStats = getNodeLevelStats(request.getMlStatsInput());
+        Map<FunctionName, MLAlgoStats> algoStats = getAlgorithmStats(request.getMlStatsInput());
+        Map<String, MLModelStats> modelStats = getModelStats(request);
+        return new MLStatsNodeResponse(clusterService.localNode(), nodeLevelStats, algoStats, modelStats);
+    }
+
+    private Map<MLNodeLevelStat, Object> getNodeLevelStats(MLStatsInput input) {
+        Map<MLNodeLevelStat, Object> stats = new HashMap<>();
+        if (input.getTargetStatLevels().contains(MLStatLevel.NODE)) {
+            if (input.retrieveStat(MLNodeLevelStat.ML_JVM_HEAP_USAGE)) {
                 long heapUsedPercent = jvmService.stats().getMem().getHeapUsedPercent();
-                statValues.put(MLNodeLevelStat.ML_JVM_HEAP_USAGE, heapUsedPercent);
+                stats.put(MLNodeLevelStat.ML_JVM_HEAP_USAGE, heapUsedPercent);
             }
-
-            for (Enum statName : mlStats.getNodeStats().keySet()) {
-                if (mlStatsInput.retrieveStat(statName)) {
-                    statValues.put((MLNodeLevelStat) statName, mlStats.getStats().get(statName).getValue());
+            mlStats.getNodeStats().forEach((statName, stat) -> {
+                if (input.retrieveStat(statName)) {
+                    stats.put((MLNodeLevelStat) statName, stat.getValue());
                 }
-            }
-        }
-
-        Map<FunctionName, MLAlgoStats> algorithmStats = new HashMap<>();
-        // return algorithm level stats
-        if (mlStatsInput.includeAlgoStats()) {
-            for (FunctionName algoName : mlStats.getAllAlgorithms()) {
-                if (mlStatsInput.retrieveStatsForAlgo(algoName)) {
-                    Map<ActionName, MLActionStats> actionStatsMap = new HashMap<>();
-                    for (Map.Entry<ActionName, MLActionStats> entry : mlStats.getAlgorithmStats(algoName).entrySet()) {
-                        if (mlStatsInput.retrieveStatsForAction(entry.getKey())) {
-                            actionStatsMap.put(entry.getKey(), entry.getValue());
-                        }
-                    }
-                    algorithmStats.put(algoName, new MLAlgoStats(actionStatsMap));
-                }
-            }
-        }
-
-        Map<String, MLModelStats> modelStats = new HashMap<>();
-        if (mlStatsInput.includeModelStats()) {
-            CountDownLatch latch = new CountDownLatch(1);
-            boolean isSuperAdmin = isSuperAdminUserWrapper(clusterService, client);
-            searchHiddenModels(ActionListener.wrap(hiddenModels -> {
-                for (String modelId : mlStats.getAllModels()) {
-                    if (isSuperAdmin || !hiddenModels.contains(modelId)) {
-                        if (mlStatsInput.retrieveStatsForModel(modelId)) {
-                            Map<ActionName, MLActionStats> actionStatsMap = new HashMap<>();
-                            for (Map.Entry<ActionName, MLActionStats> entry : mlStats.getModelStats(modelId).entrySet()) {
-                                if (mlStatsInput.retrieveStatsForAction(entry.getKey())) {
-                                    actionStatsMap.put(entry.getKey(), entry.getValue());
-                                }
-                            }
-                            modelStats.put(modelId, new MLModelStats(actionStatsMap));
-                        }
-                    }
-                }
-            }, e -> { log.error("Search Hidden model wasn't successful"); }), latch);
-            // Wait for the asynchronous call to complete
-            try {
-                latch.await(10, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                // Handle interruption if necessary
-                Thread.currentThread().interrupt();
-            }
-        }
-        return new MLStatsNodeResponse(clusterService.localNode(), statValues, algorithmStats, modelStats);
-    }
-
-    @VisibleForTesting
-    void searchHiddenModels(ActionListener<Set<String>> listener, CountDownLatch latch) {
-        SearchRequest searchRequest = buildHiddenModelSearchRequest();
-        // Use a try-with-resources block to ensure resources are properly released
-        try (ThreadContext.StoredContext threadContext = client.threadPool().getThreadContext().stashContext()) {
-            // Wrap the listener to restore thread context before calling it
-            ActionListener<Set<String>> internalListener = ActionListener.runAfter(listener, () -> {
-                latch.countDown();
-                threadContext.restore();
             });
-            // Wrap the search response handler to handle success and failure cases
-            // Notify the listener of any search failures
-            ActionListener<SearchResponse> al = ActionListener.wrap(response -> {
-                // Initialize the result set
-                Set<String> result = new HashSet<>(response.getHits().getHits().length); // Set initial capacity to the number of hits
-
-                // Iterate over the search hits and add their IDs to the result set
-                for (SearchHit hit : response.getHits()) {
-                    result.add(hit.getId());
-                }
-                // Notify the listener of the search results
-                internalListener.onResponse(result);
-            }, internalListener::onFailure);
-
-            // Execute the search request asynchronously
-            client.search(searchRequest, al);
-        } catch (Exception e) {
-            // Notify the listener of any unexpected errors
-            listener.onFailure(e);
         }
+        return stats;
     }
 
-    private SearchRequest buildHiddenModelSearchRequest() {
-        SearchRequest searchRequest = new SearchRequest(CommonValue.ML_MODEL_INDEX);
-        // Build the query
-        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
-        boolQueryBuilder
-            .filter(
-                QueryBuilders
-                    .boolQuery()
-                    .must(QueryBuilders.termQuery(MLModel.IS_HIDDEN_FIELD, true))
-                    // Add the additional filter to exclude documents where "chunk_number" exists
-                    .mustNot(QueryBuilders.existsQuery("chunk_number"))
-            );
-        searchRequest.source().query(boolQueryBuilder);
-        // Specify the fields to include in the search results (only the "_id" field)
-        // No fields to exclude
-        searchRequest.source().fetchSource(new String[] { "_id" }, new String[] {});
-        return searchRequest;
+    private Map<FunctionName, MLAlgoStats> getAlgorithmStats(MLStatsInput input) {
+        Map<FunctionName, MLAlgoStats> stats = new HashMap<>();
+        if (input.includeAlgoStats()) {
+            for (FunctionName algo : mlStats.getAllAlgorithms()) {
+                if (input.retrieveStatsForAlgo(algo)) {
+                    Map<ActionName, MLActionStats> actionStats = collectActionStats(mlStats.getAlgorithmStats(algo), input);
+                    stats.put(algo, new MLAlgoStats(actionStats));
+                }
+            }
+        }
+        return stats;
+    }
+
+    private Map<ActionName, MLActionStats> collectActionStats(Map<ActionName, MLActionStats> stats, MLStatsInput input) {
+        Map<ActionName, MLActionStats> filteredStats = new HashMap<>();
+        stats.forEach((action, stat) -> {
+            if (input.retrieveStatsForAction(action)) {
+                filteredStats.put(action, stat);
+            }
+        });
+        return filteredStats;
+    }
+
+    private Map<String, MLModelStats> getModelStats(MLStatsNodesRequest request) {
+        Map<String, MLModelStats> stats = new HashMap<>();
+        boolean isSuperAdmin = isSuperAdminUserWrapper(clusterService, client);
+        Set<String> hiddenModels = Optional.ofNullable(request.getHiddenModelIds()).orElse(Collections.emptySet());
+
+        for (String modelId : mlStats.getAllModels()) {
+            if (isSuperAdmin || !hiddenModels.contains(modelId)) {
+                if (request.getMlStatsInput().retrieveStatsForModel(modelId)) {
+                    Map<ActionName, MLActionStats> actionStats = collectActionStats(
+                        mlStats.getModelStats(modelId),
+                        request.getMlStatsInput()
+                    );
+                    boolean isHidden = hiddenModels.contains(modelId);
+                    stats.put(modelId, new MLModelStats(actionStats, isHidden));
+                }
+            }
+        }
+        return stats;
     }
 
     @VisibleForTesting
