ea7fefa6fbac0d0e1edd111d4fd450d599ff04ba
Yuye Zhu <yuyezhu@amazon.com>
2024-04-26 11:33:37 +0800

    feat: Add search index tool (#2356)
    * add search index tool
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * add search index tool
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * spotless apply
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * add integration test for search index tool
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * fix unit test error
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * run spotless apply
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * change json file path for search index tool it
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * fix integration test for search index tool
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * run spotless apply
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * skip multiple node integration test for search index tool
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * resolve code duplication in search index tool it
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * change method access
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * remove unused class
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * remove wild card import
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * add comment in gradle file
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    ---------
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

---
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/tools/SearchIndexToolTests.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.engine.tools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.client.Client;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.xcontent.json.JsonXContent;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.common.Strings;
+import org.opensearch.core.xcontent.DeprecationHandler;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.ml.common.transport.connector.MLConnectorSearchAction;
+import org.opensearch.ml.common.transport.model.MLModelSearchAction;
+import org.opensearch.ml.common.transport.model_group.MLModelGroupSearchAction;
+import org.opensearch.search.SearchModule;
+
+import lombok.SneakyThrows;
+
+public class SearchIndexToolTests {
+    static public final NamedXContentRegistry TEST_XCONTENT_REGISTRY_FOR_QUERY = new NamedXContentRegistry(
+        new SearchModule(Settings.EMPTY, List.of()).getNamedXContents()
+    );
+
+    private Client client;
+
+    private SearchIndexTool mockedSearchIndexTool;
+
+    private String mockedSearchResponseString;
+
+    @Before
+    @SneakyThrows
+    public void setup() {
+        client = mock(Client.class);
+        mockedSearchIndexTool = mock(
+            SearchIndexTool.class,
+            Mockito.withSettings().useConstructor(client, TEST_XCONTENT_REGISTRY_FOR_QUERY).defaultAnswer(Mockito.CALLS_REAL_METHODS)
+        );
+
+        try (InputStream searchResponseIns = SearchIndexTool.class.getResourceAsStream("retrieval_tool_search_response.json")) {
+            if (searchResponseIns != null) {
+                mockedSearchResponseString = new String(searchResponseIns.readAllBytes());
+            }
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void testGetType() {
+        String type = mockedSearchIndexTool.getType();
+        assertFalse(Strings.isNullOrEmpty(type));
+        assertEquals("SearchIndexTool", type);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testValidate() {
+        Map<String, String> parameters = Map.of("input", "{}");
+        assertTrue(mockedSearchIndexTool.validate(parameters));
+    }
+
+    @Test
+    @SneakyThrows
+    public void testValidateWithEmptyInput() {
+        Map<String, String> parameters = Map.of();
+        assertFalse(mockedSearchIndexTool.validate(parameters));
+    }
+
+    @Test
+    public void testRunWithNormalIndex() {
+        String inputString = "{\"index\": \"test-index\", \"query\": {\"query\": {\"match_all\": {}}}}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        mockedSearchIndexTool.run(parameters, null);
+        Mockito.verify(client, times(1)).search(any(), any());
+        Mockito.verify(client, Mockito.never()).execute(any(), any(), any());
+    }
+
+    @Test
+    public void testRunWithConnectorIndex() {
+        String inputString = "{\"index\": \".plugins-ml-connector\", \"query\": {\"query\": {\"match_all\": {}}}}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        mockedSearchIndexTool.run(parameters, null);
+        Mockito.verify(client, never()).search(any(), any());
+        Mockito.verify(client, times(1)).execute(eq(MLConnectorSearchAction.INSTANCE), any(), any());
+    }
+
+    @Test
+    public void testRunWithModelIndex() {
+        String inputString = "{\"index\": \".plugins-ml-model\", \"query\": {\"query\": {\"match_all\": {}}}}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        mockedSearchIndexTool.run(parameters, null);
+        Mockito.verify(client, never()).search(any(), any());
+        Mockito.verify(client, times(1)).execute(eq(MLModelSearchAction.INSTANCE), any(), any());
+    }
+
+    @Test
+    public void testRunWithModelGroupIndex() {
+        String inputString = "{\"index\": \".plugins-ml-model-group\", \"query\": {\"query\": {\"match_all\": {}}}}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        mockedSearchIndexTool.run(parameters, null);
+        Mockito.verify(client, never()).search(any(), any());
+        Mockito.verify(client, times(1)).execute(eq(MLModelGroupSearchAction.INSTANCE), any(), any());
+    }
+
+    @Test
+    @SneakyThrows
+    public void testRunWithSearchResults() {
+        SearchResponse mockedSearchResponse = SearchResponse
+            .fromXContent(
+                JsonXContent.jsonXContent
+                    .createParser(NamedXContentRegistry.EMPTY, DeprecationHandler.IGNORE_DEPRECATIONS, mockedSearchResponseString)
+            );
+        doAnswer(invocation -> {
+            ActionListener<SearchResponse> listener = invocation.getArgument(1);
+            listener.onResponse(mockedSearchResponse);
+            return null;
+        }).when(client).search(any(), any());
+
+        String inputString = "{\"index\": \"test-index\", \"query\": {\"query\": {\"match_all\": {}}}}";
+        final CompletableFuture<String> future = new CompletableFuture<>();
+        ActionListener<String> listener = ActionListener.wrap(r -> { future.complete(r); }, e -> { future.completeExceptionally(e); });
+        Map<String, String> parameters = Map.of("input", inputString);
+        mockedSearchIndexTool.run(parameters, listener);
+
+        future.join();
+
+        Mockito.verify(client, times(1)).search(any(), any());
+        Mockito.verify(client, Mockito.never()).execute(any(), any(), any());
+    }
+
+    @Test
+    @SneakyThrows
+    public void testRunWithEmptyQuery() {
+        String inputString = "{\"index\": \"test_index\"}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        ActionListener<String> listener = mock(ActionListener.class);
+        mockedSearchIndexTool.run(parameters, listener);
+        Mockito.verify(client, Mockito.never()).execute(any(), any(), any());
+        Mockito.verify(client, Mockito.never()).search(any(), any());
+    }
+
+    @Test
+    public void testRunWithInvalidQuery() {
+        String inputString = "{\"index\": \"test-index\", \"query\": \"invalid query\"}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        ActionListener<String> listener = mock(ActionListener.class);
+        mockedSearchIndexTool.run(parameters, listener);
+        Mockito.verify(client, Mockito.never()).execute(any(), any(), any());
+        Mockito.verify(client, Mockito.never()).search(any(), any());
+    }
+
+    @Test
+    public void testRunWithEmptyQueryBody() {
+        String inputString = "{\"index\": \"test-index\", \"query\": {}}";
+        Map<String, String> parameters = Map.of("input", inputString);
+        mockedSearchIndexTool.run(parameters, null);
+        Mockito.verify(client, times(1)).search(any(), any());
+        Mockito.verify(client, Mockito.never()).execute(any(), any(), any());
+    }
+
+    @Test
+    public void testFactory() {
+        SearchIndexTool searchIndexTool = SearchIndexTool.Factory.getInstance().create(Collections.emptyMap());
+        assertEquals(SearchIndexTool.TYPE, searchIndexTool.getType());
+    }
+}
