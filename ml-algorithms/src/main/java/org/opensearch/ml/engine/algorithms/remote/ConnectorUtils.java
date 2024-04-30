4edcd17fdda173170ed1f3babb19547032dfbe96
zane-neo <zaniu@amazon.com>
2024-04-30 15:40:13 +0800

    Change httpclient to async (#1958) (#2375)
    * Change httpclient from sync to async
    
    
    
    * Change from CRTAsyncHttpClient to NettyAsyncHttpClient
    
    
    
    * Add publisher to request
    
    
    
    * Change sync httpclient to async
    
    
    
    * Handle error case and return error response in actionLListener
    
    
    
    * Fix no response when exception
    
    
    
    * Add content type header
    
    
    
    * Fix issues found in functional test
    
    
    
    * Fix no response issue in functional test
    
    
    
    * fix default step size error
    
    
    
    * Add track inference duration for async httpclient
    
    
    
    * Change client appsec highlight issues implementation for async httpclient
    
    
    
    * Add UTs
    
    
    
    * Add UTs
    
    
    
    * Remove unused file
    
    
    
    * Add UTs
    
    
    
    * format code
    
    
    
    * Change error code to honor remote service error code
    
    
    
    * Add more UTs
    
    
    
    * Change SSRF code to make it correct for return error stattus
    
    
    
    * Fix failure UTs and add more UTs
    
    
    
    * Fix failure ITs
    
    
    
    * format code
    
    
    
    * Fix partial success response not correct issue
    
    
    
    * format code
    
    
    
    * Fix failure ITs
    
    
    
    * Add more UTs to increase code coverage
    
    
    
    * Change url regex
    
    
    
    * Address comments
    
    
    
    * format code
    
    
    
    * Fix failure UTs
    
    
    
    * Add UT for httpclientFactory throw exception when creating httpclient
    
    
    
    * format code
    
    
    
    * Address comments and add modelTensor status code
    
    
    
    * Address comments
    
    
    
    * format code
    
    
    
    * Add status code to process error response
    
    
    
    * format code
    
    
    
    * Rebase main after connector level http parameter support
    
    
    
    * Fix UT
    
    
    
    * Change error message when remote model return empty and chaange the behavior when one of the requests fails
    
    
    
    * Add comments\
    
    
    
    * Remove redundant builder and change the error code check
    
    
    
    * format code
    
    
    
    * Add more UTs for throw exception cases
    
    
    
    * fix failure UTs
    
    
    
    * format code
    
    
    
    * Fix test cases since the error message change
    
    
    
    * Rebase code
    
    
    
    * fix failure IT
    
    
    
    * Add more UTs
    
    
    
    * Fix duplicate response to client issue
    
    
    
    * fix duplicate response in channel
    
    
    
    * change code for all successfully responses case
    
    
    
    * Address comments
    
    
    
    * format code
    
    
    
    * Increase nio httpclient version to fix vulnerbility
    
    
    
    * Change validate localhost logic to same with existing code
    
    
    
    * change method signature to private
    
    
    
    * format code
    
    
    
    ---------
    
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/ConnectorUtils.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/ConnectorUtils.java
@@ -15,6 +15,8 @@ import static org.opensearch.ml.common.utils.StringUtils.processTextDocs;
 import static org.opensearch.ml.engine.utils.ScriptUtils.executePostProcessFunction;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -34,6 +36,7 @@ import org.opensearch.ml.common.dataset.TextDocsInputDataSet;
 import org.opensearch.ml.common.dataset.TextSimilarityInputDataSet;
 import org.opensearch.ml.common.dataset.remote.RemoteInferenceInputDataSet;
 import org.opensearch.ml.common.input.MLInput;
+import org.opensearch.ml.common.model.MLGuard;
 import org.opensearch.ml.common.output.model.ModelTensor;
 import org.opensearch.ml.common.output.model.ModelTensors;
 import org.opensearch.script.ScriptService;
@@ -46,7 +49,9 @@ import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.signer.Aws4Signer;
 import software.amazon.awssdk.auth.signer.params.Aws4SignerParams;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
 import software.amazon.awssdk.regions.Region;
 
 @Log4j2
@@ -179,10 +184,14 @@ public class ConnectorUtils {
         String modelResponse,
         Connector connector,
         ScriptService scriptService,
-        Map<String, String> parameters
+        Map<String, String> parameters,
+        MLGuard mlGuard
     ) throws IOException {
         if (modelResponse == null) {
             throw new IllegalArgumentException("model response is null");
+        }
+        if (mlGuard != null && !mlGuard.validate(modelResponse, MLGuard.Type.OUTPUT)) {
+            throw new IllegalArgumentException("guardrails triggered for LLM output");
         }
         List<ModelTensor> modelTensors = new ArrayList<>();
         Optional<ConnectorAction> predictAction = connector.findPredictAction();
@@ -251,5 +260,43 @@ public class ConnectorUtils {
             .build();
 
         return signer.sign(request, params);
+    }
+
+    public static SdkHttpFullRequest buildSdkRequest(
+        Connector connector,
+        Map<String, String> parameters,
+        String payload,
+        SdkHttpMethod method
+    ) {
+        String charset = parameters.getOrDefault("charset", "UTF-8");
+        RequestBody requestBody;
+        if (payload != null) {
+            requestBody = RequestBody.fromString(payload, Charset.forName(charset));
+        } else {
+            requestBody = RequestBody.empty();
+        }
+        if (SdkHttpMethod.POST == method && 0 == requestBody.optionalContentLength().get()) {
+            log.error("Content length is 0. Aborting request to remote model");
+            throw new IllegalArgumentException("Content length is 0. Aborting request to remote model");
+        }
+        String endpoint = connector.getPredictEndpoint(parameters);
+        SdkHttpFullRequest.Builder builder = SdkHttpFullRequest
+            .builder()
+            .method(method)
+            .uri(URI.create(endpoint))
+            .contentStreamProvider(requestBody.contentStreamProvider());
+        Map<String, String> headers = connector.getDecryptedHeaders();
+        if (headers != null) {
+            for (String key : headers.keySet()) {
+                builder.putHeader(key, headers.get(key));
+            }
+        }
+        if (builder.matchingHeaders("Content-Type").isEmpty()) {
+            builder.putHeader("Content-Type", "application/json");
+        }
+        if (builder.matchingHeaders("Content-Length").isEmpty()) {
+            builder.putHeader("Content-Length", requestBody.optionalContentLength().get().toString());
+        }
+        return builder.build();
     }
 }
