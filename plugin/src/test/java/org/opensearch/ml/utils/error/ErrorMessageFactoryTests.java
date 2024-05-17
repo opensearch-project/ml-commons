29a2c1ea247d27960caa659236b07490300062d0
Jing Zhang <jngz@amazon.com>
2024-05-17 15:56:26 -0700

    fix error message with unwrapping the root cause
    Signed-off-by: Jing Zhang <jngz@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/utils/error/ErrorMessageFactoryTests.java
+++ plugin/src/test/java/org/opensearch/ml/utils/error/ErrorMessageFactoryTests.java
@@ -5,43 +5,41 @@
 
 package org.opensearch.ml.utils.error;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.opensearch.OpenSearchException;
 import org.opensearch.core.rest.RestStatus;
+import org.opensearch.transport.RemoteTransportException;
 
 public class ErrorMessageFactoryTests {
 
-    private Throwable nonOpenSearchThrowable = new Throwable();
-    private Throwable openSearchThrowable = new OpenSearchException(nonOpenSearchThrowable);
-
     @Test
-    public void openSearchExceptionShouldCreateEsErrorMessage() {
-        Exception exception = new OpenSearchException(nonOpenSearchThrowable);
-        ErrorMessage msg = ErrorMessageFactory.createErrorMessage(exception, RestStatus.BAD_REQUEST.getStatus());
-        assertTrue(msg.exception instanceof OpenSearchException);
+    public void openSearchExceptionWithoutNestedException() {
+        Throwable openSearchThrowable = new OpenSearchException("OpenSearch Exception");
+        ErrorMessage errorMessage = ErrorMessageFactory.createErrorMessage(openSearchThrowable, RestStatus.BAD_REQUEST.getStatus());
+        assertTrue(errorMessage.exception instanceof OpenSearchException);
+        assertEquals(RestStatus.INTERNAL_SERVER_ERROR.getStatus(), errorMessage.getStatus());
     }
 
     @Test
-    public void nonOpenSearchExceptionShouldCreateGenericErrorMessage() {
-        Exception exception = new Exception(nonOpenSearchThrowable);
-        ErrorMessage msg = ErrorMessageFactory.createErrorMessage(exception, RestStatus.BAD_REQUEST.getStatus());
-        assertFalse(msg.exception instanceof OpenSearchException);
+    public void openSearchExceptionWithNestedException() {
+        Throwable nestedThrowable = new IllegalArgumentException("Illegal Argument Exception");
+        Throwable openSearchThrowable = new RemoteTransportException("Remote Transport Exception", nestedThrowable);
+        ErrorMessage errorMessage = ErrorMessageFactory
+            .createErrorMessage(openSearchThrowable, RestStatus.INTERNAL_SERVER_ERROR.getStatus());
+        assertTrue(errorMessage.exception instanceof IllegalArgumentException);
+        assertEquals(RestStatus.BAD_REQUEST.getStatus(), errorMessage.getStatus());
     }
 
     @Test
-    public void nonOpenSearchExceptionWithWrappedEsExceptionCauseShouldCreateEsErrorMessage() {
-        Exception exception = (Exception) openSearchThrowable;
-        ErrorMessage msg = ErrorMessageFactory.createErrorMessage(exception, RestStatus.BAD_REQUEST.getStatus());
-        assertTrue(msg.exception instanceof OpenSearchException);
-    }
-
-    @Test
-    public void nonOpenSearchExceptionWithMultiLayerWrappedEsExceptionCauseShouldCreateEsErrorMessage() {
-        Exception exception = new Exception(new Throwable(new Throwable(openSearchThrowable)));
-        ErrorMessage msg = ErrorMessageFactory.createErrorMessage(exception, RestStatus.BAD_REQUEST.getStatus());
-        assertTrue(msg.exception instanceof OpenSearchException);
+    public void nonOpenSearchExceptionWithNestedException() {
+        Throwable nestedThrowable = new IllegalArgumentException("Illegal Argument Exception");
+        Throwable nonOpenSearchThrowable = new Exception("Remote Transport Exception", nestedThrowable);
+        ErrorMessage errorMessage = ErrorMessageFactory
+            .createErrorMessage(nonOpenSearchThrowable, RestStatus.INTERNAL_SERVER_ERROR.getStatus());
+        assertTrue(errorMessage.exception instanceof IllegalArgumentException);
+        assertEquals(RestStatus.INTERNAL_SERVER_ERROR.getStatus(), errorMessage.getStatus());
     }
 }
