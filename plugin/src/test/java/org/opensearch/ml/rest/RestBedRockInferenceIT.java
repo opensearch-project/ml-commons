9a75bc00b5bc3405453c6dee4b315cd3131d3799
zane-neo <zaniu@amazon.com>
2024-06-05 15:13:01 +0800

    format code
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/rest/RestBedRockInferenceIT.java
+++ plugin/src/test/java/org/opensearch/ml/rest/RestBedRockInferenceIT.java
@@ -5,19 +5,20 @@
 
 package org.opensearch.ml.rest;
 
-import lombok.SneakyThrows;
-import org.junit.Before;
-import org.opensearch.ml.common.FunctionName;
-import org.opensearch.ml.common.dataset.TextDocsInputDataSet;
-import org.opensearch.ml.common.input.MLInput;
-import org.opensearch.ml.common.utils.StringUtils;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
+import org.junit.Before;
+import org.opensearch.ml.common.FunctionName;
+import org.opensearch.ml.common.dataset.TextDocsInputDataSet;
+import org.opensearch.ml.common.input.MLInput;
+import org.opensearch.ml.common.utils.StringUtils;
+
+import lombok.SneakyThrows;
 
 public class RestBedRockInferenceIT extends MLCommonsRestTestCase {
     private static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
@@ -32,19 +33,38 @@ public class RestBedRockInferenceIT extends MLCommonsRestTestCase {
         Thread.sleep(20000);
     }
 
-
     public void test_bedrock_embedding_model() throws Exception {
         // Skip test if key is null
         if (AWS_ACCESS_KEY_ID == null || AWS_SECRET_ACCESS_KEY == null || AWS_SESSION_TOKEN == null) {
             return;
         }
-        String templates = Files.readString(Path.of(RestMLPredictionAction.class.getClassLoader().getResource("org/opensearch/ml/rest/templates/BedRockConnectorBodies.json").toURI()));
+        String templates = Files
+            .readString(
+                Path
+                    .of(
+                        RestMLPredictionAction.class
+                            .getClassLoader()
+                            .getResource("org/opensearch/ml/rest/templates/BedRockConnectorBodies.json")
+                            .toURI()
+                    )
+            );
         Map<String, Object> templateMap = StringUtils.gson.fromJson(templates, Map.class);
         for (Map.Entry<String, Object> templateEntry : templateMap.entrySet()) {
             String bedrockEmbeddingModelName = "bedrock embedding model " + randomAlphaOfLength(5);
             String testCaseName = templateEntry.getKey();
             String errorMsg = String.format(Locale.ROOT, "Failing test case name: %s", testCaseName);
-            String modelId = registerRemoteModel(String.format(StringUtils.gson.toJson(templateEntry.getValue()), GITHUB_CI_AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN), bedrockEmbeddingModelName, true);
+            String modelId = registerRemoteModel(
+                String
+                    .format(
+                        StringUtils.gson.toJson(templateEntry.getValue()),
+                        GITHUB_CI_AWS_REGION,
+                        AWS_ACCESS_KEY_ID,
+                        AWS_SECRET_ACCESS_KEY,
+                        AWS_SESSION_TOKEN
+                    ),
+                bedrockEmbeddingModelName,
+                true
+            );
 
             TextDocsInputDataSet inputDataSet = TextDocsInputDataSet.builder().docs(List.of("hello", "world")).build();
             MLInput mlInput = MLInput.builder().inputDataset(inputDataSet).algorithm(FunctionName.TEXT_EMBEDDING).build();
