40c5edc4f29d74068a864d703e94a21580048297
Jing Zhang <jngz@amazon.com>
2024-06-10 20:36:29 -0700

    guardrails model support (#2491) (#2526)
    * guardrails model support
    
    
    
    * add IT for remote guardrails model
    
    
    
    * address comments
    
    
    
    * address more comments 1
    
    Signed-off-by: Jing Zhang <jngz@amazon.com>

---
+++ common/src/main/java/org/opensearch/ml/common/model/LocalRegexGuardrail.java
@@ -0,0 +1,277 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.model;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
+import org.opensearch.action.LatchedActionListener;
+import org.opensearch.action.search.SearchRequest;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.client.Client;
+import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.common.xcontent.LoggingDeprecationHandler;
+import org.opensearch.common.xcontent.XContentType;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.core.xcontent.XContentParser;
+import org.opensearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.opensearch.core.xcontent.XContentParserUtils.ensureExpectedToken;
+import static org.opensearch.ml.common.CommonValue.stopWordsIndices;
+import static org.opensearch.ml.common.utils.StringUtils.gson;
+
+@Log4j2
+@EqualsAndHashCode
+@Getter
+public class LocalRegexGuardrail extends Guardrail {
+    public static final String STOP_WORDS_FIELD = "stop_words";
+    public static final String REGEX_FIELD = "regex";
+
+    private List<StopWords> stopWords;
+    private String[] regex;
+    private List<Pattern> regexPattern;
+    private Map<String, List<String>> stopWordsIndicesInput;
+    private NamedXContentRegistry xContentRegistry;
+    private Client client;
+
+    @Builder(toBuilder = true)
+    public LocalRegexGuardrail(List<StopWords> stopWords, String[] regex) {
+        this.stopWords = stopWords;
+        this.regex = regex;
+    }
+    public LocalRegexGuardrail(@NonNull Map<String, Object> params) {
+        List<Map> words = (List<Map>) params.get(STOP_WORDS_FIELD);
+        stopWords = new ArrayList<>();
+        if (words != null && !words.isEmpty()) {
+            for (Map e : words) {
+                stopWords.add(new StopWords(e));
+            }
+        }
+        List<String> regexes = (List<String>) params.get(REGEX_FIELD);
+        if (regexes != null && !regexes.isEmpty()) {
+            this.regex = regexes.toArray(new String[0]);
+        }
+    }
+
+    public LocalRegexGuardrail(StreamInput input) throws IOException {
+        if (input.readBoolean()) {
+            stopWords = new ArrayList<>();
+            int size = input.readInt();
+            for (int i=0; i<size; i++) {
+                stopWords.add(new StopWords(input));
+            }
+        }
+        regex = input.readOptionalStringArray();
+    }
+
+    public void writeTo(StreamOutput out) throws IOException {
+        if (stopWords != null && stopWords.size() > 0) {
+            out.writeBoolean(true);
+            out.writeInt(stopWords.size());
+            for (StopWords e : stopWords) {
+                e.writeTo(out);
+            }
+        } else {
+            out.writeBoolean(false);
+        }
+        out.writeOptionalStringArray(regex);
+    }
+
+    @Override
+    public Boolean validate(String input, Map<String, String> parameters) {
+        return validateRegexList(input, regexPattern) && validateStopWords(input, stopWordsIndicesInput);
+    }
+
+    @Override
+    public void init(NamedXContentRegistry xContentRegistry, Client client) {
+        this.xContentRegistry = xContentRegistry;
+        this.client = client;
+        init();
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+        if (stopWords != null && stopWords.size() > 0) {
+            builder.field(STOP_WORDS_FIELD, stopWords);
+        }
+        if (regex != null) {
+            builder.field(REGEX_FIELD, regex);
+        }
+        builder.endObject();
+        return builder;
+    }
+
+    public static LocalRegexGuardrail parse(XContentParser parser) throws IOException {
+        List<StopWords> stopWords = null;
+        String[] regex = null;
+
+        ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
+        while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
+            String fieldName = parser.currentName();
+            parser.nextToken();
+
+            switch (fieldName) {
+                case STOP_WORDS_FIELD:
+                    stopWords = new ArrayList<>();
+                    ensureExpectedToken(XContentParser.Token.START_ARRAY, parser.currentToken(), parser);
+                    while (parser.nextToken() != XContentParser.Token.END_ARRAY) {
+                        stopWords.add(StopWords.parse(parser));
+                    }
+                    break;
+                case REGEX_FIELD:
+                    regex = parser.list().toArray(new String[0]);
+                    break;
+                default:
+                    parser.skipChildren();
+                    break;
+            }
+        }
+        return LocalRegexGuardrail.builder()
+                .stopWords(stopWords)
+                .regex(regex)
+                .build();
+    }
+
+    private void init() {
+        stopWordsIndicesInput = stopWordsToMap();
+        List<String> regexList = regex == null ? new ArrayList<>() : Arrays.asList(regex);
+        regexPattern = regexList.stream().map(reg -> Pattern.compile(reg)).collect(Collectors.toList());
+    }
+
+    private Map<String, List<String>> stopWordsToMap() {
+        Map<String, List<String>> map = new HashMap<>();
+        if (stopWords != null && !stopWords.isEmpty()) {
+            for (StopWords e : stopWords) {
+                if (e.getIndex() != null && e.getSourceFields() != null) {
+                    map.put(e.getIndex(), Arrays.asList(e.getSourceFields()));
+                }
+            }
+        }
+        return map;
+    }
+
+    public Boolean validateRegexList(String input, List<Pattern> regexPatterns) {
+        if (regexPatterns == null || regexPatterns.isEmpty()) {
+            return true;
+        }
+        for (Pattern pattern : regexPatterns) {
+            if (!validateRegex(input, pattern)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Boolean validateRegex(String input, Pattern pattern) {
+        Matcher matcher = pattern.matcher(input);
+        return !matcher.matches();
+    }
+
+    public Boolean validateStopWords(String input, Map<String, List<String>> stopWordsIndices) {
+        if (stopWordsIndices == null || stopWordsIndices.isEmpty()) {
+            return true;
+        }
+        for (Map.Entry entry : stopWordsIndices.entrySet()) {
+            if (!validateStopWordsSingleIndex(input, (String) entry.getKey(), (List<String>) entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Validate the input string against stop words
+     * @param input the string to validate against stop words
+     * @param indexName the index containing stop words
+     * @param fieldNames a list of field names containing stop words
+     * @return true if no stop words matching, otherwise false.
+     */
+    public Boolean validateStopWordsSingleIndex(String input, String indexName, List<String> fieldNames) {
+        SearchRequest searchRequest;
+        AtomicBoolean hitStopWords = new AtomicBoolean(false);
+        String queryBody;
+        Map<String, String> documentMap = new HashMap<>();
+        for (String field : fieldNames) {
+            documentMap.put(field, input);
+        }
+        Map<String, Object> queryBodyMap = Map
+                .of("query", Map.of("percolate", Map.of("field", "query", "document", documentMap)));
+        CountDownLatch latch = new CountDownLatch(1);
+        ThreadContext.StoredContext context = null;
+
+        try {
+            queryBody = AccessController.doPrivileged((PrivilegedExceptionAction<String>) () -> gson.toJson(queryBodyMap));
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+            XContentParser queryParser = XContentType.JSON.xContent().createParser(xContentRegistry, LoggingDeprecationHandler.INSTANCE, queryBody);
+            searchSourceBuilder.parseXContent(queryParser);
+            searchSourceBuilder.size(1); //Only need 1 doc returned, if hit.
+            searchRequest = new SearchRequest().source(searchSourceBuilder).indices(indexName);
+            if (isStopWordsSystemIndex(indexName)) {
+                context = client.threadPool().getThreadContext().stashContext();
+                ThreadContext.StoredContext finalContext = context;
+                client.search(searchRequest, ActionListener.runBefore(new LatchedActionListener(ActionListener.<SearchResponse>wrap(r -> {
+                    if (r == null || r.getHits() == null || r.getHits().getTotalHits() == null || r.getHits().getTotalHits().value == 0) {
+                        hitStopWords.set(true);
+                    }
+                }, e -> {
+                    log.error("Failed to search stop words index {}", indexName, e);
+                    hitStopWords.set(true);
+                }), latch), () -> finalContext.restore()));
+            } else {
+                client.search(searchRequest, new LatchedActionListener(ActionListener.<SearchResponse>wrap(r -> {
+                    if (r == null || r.getHits() == null || r.getHits().getTotalHits() == null || r.getHits().getTotalHits().value == 0) {
+                        hitStopWords.set(true);
+                    }
+                }, e -> {
+                    log.error("Failed to search stop words index {}", indexName, e);
+                    hitStopWords.set(true);
+                }), latch));
+            }
+        } catch (Exception e) {
+            log.error("[validateStopWords] Searching stop words index failed.", e);
+            latch.countDown();
+            hitStopWords.set(true);
+        } finally {
+            if (context != null) {
+                context.close();
+            }
+        }
+
+        try {
+            latch.await(5, SECONDS);
+        } catch (InterruptedException e) {
+            log.error("[validateStopWords] Searching stop words index was timeout.", e);
+            throw new IllegalStateException(e);
+        }
+        return hitStopWords.get();
+    }
+
+    private boolean isStopWordsSystemIndex(String index) {
+        return stopWordsIndices.contains(index);
+    }
+}
---
+++ common/src/test/java/org/opensearch/ml/common/model/LocalRegexGuardrailTests.java
@@ -0,0 +1,292 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.model;
+
+import org.apache.lucene.search.TotalHits;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.action.search.ShardSearchFailure;
+import org.opensearch.client.Client;
+import org.opensearch.common.action.ActionFuture;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.unit.TimeValue;
+import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.common.xcontent.XContentType;
+import org.opensearch.core.common.bytes.BytesReference;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.core.xcontent.ToXContent;
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.core.xcontent.XContentParser;
+import org.opensearch.ml.common.TestHelper;
+import org.opensearch.search.SearchHit;
+import org.opensearch.search.SearchHits;
+import org.opensearch.search.SearchModule;
+import org.opensearch.search.aggregations.InternalAggregations;
+import org.opensearch.search.internal.InternalSearchResponse;
+import org.opensearch.search.profile.SearchProfileShardResults;
+import org.opensearch.search.suggest.Suggest;
+import org.opensearch.threadpool.ThreadPool;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class LocalRegexGuardrailTests {
+    NamedXContentRegistry xContentRegistry;
+    @Mock
+    Client client;
+    @Mock
+    ThreadPool threadPool;
+    ThreadContext threadContext;
+
+    StopWords stopWords;
+    String[] regex;
+    List<Pattern> regexPatterns;
+    LocalRegexGuardrail localRegexGuardrail;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        xContentRegistry = new NamedXContentRegistry(new SearchModule(Settings.EMPTY, Collections.emptyList()).getNamedXContents());
+        Settings settings = Settings.builder().build();
+        this.threadContext = new ThreadContext(settings);
+        when(this.client.threadPool()).thenReturn(this.threadPool);
+        when(this.threadPool.getThreadContext()).thenReturn(this.threadContext);
+
+        stopWords = new StopWords("test_index", List.of("test_field").toArray(new String[0]));
+        regex = List.of("(.|\n)*stop words(.|\n)*").toArray(new String[0]);
+        regexPatterns = List.of(Pattern.compile("(.|\n)*stop words(.|\n)*"));
+        localRegexGuardrail = new LocalRegexGuardrail(List.of(stopWords), regex);
+    }
+
+    @Test
+    public void readWriteEmptyContent() throws IOException {
+        LocalRegexGuardrail localRegexGuardrail = new LocalRegexGuardrail(Collections.emptyList(), new String[0]);
+        BytesStreamOutput output = new BytesStreamOutput();
+        localRegexGuardrail.writeTo(output);
+        LocalRegexGuardrail localRegexGuardrail1 = new LocalRegexGuardrail(output.bytes().streamInput());
+
+        Assert.assertNull(localRegexGuardrail1.getStopWords());
+        Assert.assertArrayEquals(Collections.emptyList().toArray(), localRegexGuardrail1.getRegex());
+    }
+
+    @Test
+    public void readWriteNullContent() throws IOException {
+        LocalRegexGuardrail localRegexGuardrail = new LocalRegexGuardrail(null, null);
+        BytesStreamOutput output = new BytesStreamOutput();
+        localRegexGuardrail.writeTo(output);
+        LocalRegexGuardrail localRegexGuardrail1 = new LocalRegexGuardrail(output.bytes().streamInput());
+
+        Assert.assertNull(localRegexGuardrail1.getStopWords());
+        Assert.assertNull(localRegexGuardrail1.getRegex());
+    }
+
+    @Test
+    public void writeTo() throws IOException {
+        LocalRegexGuardrail localRegexGuardrail = new LocalRegexGuardrail(List.of(stopWords), regex);
+        BytesStreamOutput output = new BytesStreamOutput();
+        localRegexGuardrail.writeTo(output);
+        LocalRegexGuardrail localRegexGuardrail1 = new LocalRegexGuardrail(output.bytes().streamInput());
+
+        Assert.assertArrayEquals(localRegexGuardrail.getStopWords().toArray(), localRegexGuardrail1.getStopWords().toArray());
+        Assert.assertArrayEquals(localRegexGuardrail.getRegex(), localRegexGuardrail1.getRegex());
+    }
+
+    @Test
+    public void toXContent() throws IOException {
+        LocalRegexGuardrail localRegexGuardrail = new LocalRegexGuardrail(List.of(stopWords), regex);
+        XContentBuilder builder = XContentBuilder.builder(XContentType.JSON.xContent());
+        localRegexGuardrail.toXContent(builder, ToXContent.EMPTY_PARAMS);
+        String content = TestHelper.xContentBuilderToString(builder);
+
+        Assert.assertEquals("{\"stop_words\":[{\"index_name\":\"test_index\",\"source_fields\":[\"test_field\"]}],\"regex\":[\"(.|\\n)*stop words(.|\\n)*\"]}", content);
+    }
+
+    @Test
+    public void parse() throws IOException {
+        String jsonStr = "{\"stop_words\":[{\"index_name\":\"test_index\",\"source_fields\":[\"test_field\"]}],\"regex\":[\"(.|\\n)*stop words(.|\\n)*\"]}";
+        XContentParser parser = XContentType.JSON.xContent().createParser(new NamedXContentRegistry(new SearchModule(Settings.EMPTY,
+                Collections.emptyList()).getNamedXContents()), null, jsonStr);
+        parser.nextToken();
+        LocalRegexGuardrail localRegexGuardrail = LocalRegexGuardrail.parse(parser);
+
+        Assert.assertArrayEquals(localRegexGuardrail.getStopWords().toArray(), List.of(stopWords).toArray());
+        Assert.assertArrayEquals(localRegexGuardrail.getRegex(), regex);
+    }
+
+    @Test
+    public void validateRegexListSuccess() {
+        String input = "\n\nHuman:hello good words.\n\nAssistant:";
+        Boolean res = localRegexGuardrail.validateRegexList(input, regexPatterns);
+
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateRegexListFailed() {
+        String input = "\n\nHuman:hello stop words.\n\nAssistant:";
+        Boolean res = localRegexGuardrail.validateRegexList(input, regexPatterns);
+
+        Assert.assertFalse(res);
+    }
+
+    @Test
+    public void validateRegexListNull() {
+        String input = "\n\nHuman:hello good words.\n\nAssistant:";
+        Boolean res = localRegexGuardrail.validateRegexList(input, null);
+
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateRegexListEmpty() {
+        String input = "\n\nHuman:hello good words.\n\nAssistant:";
+        Boolean res = localRegexGuardrail.validateRegexList(input, List.of());
+
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateRegexSuccess() {
+        String input = "\n\nHuman:hello good words.\n\nAssistant:";
+        Boolean res = localRegexGuardrail.validateRegex(input, regexPatterns.get(0));
+
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateRegexFailed() {
+        String input = "\n\nHuman:hello stop words.\n\nAssistant:";
+        Boolean res = localRegexGuardrail.validateRegex(input, regexPatterns.get(0));
+
+        Assert.assertFalse(res);
+    }
+
+    @Test
+    public void validateStopWords() throws IOException {
+        Map<String, List<String>> stopWordsIndices = Map.of("test_index", List.of("test_field"));
+        SearchResponse searchResponse = createSearchResponse(1);
+        ActionFuture<SearchResponse> future = createSearchResponseFuture(searchResponse);
+        when(this.client.search(any())).thenReturn(future);
+
+        Boolean res = localRegexGuardrail.validateStopWords("hello world", stopWordsIndices);
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateStopWordsNull() {
+        Boolean res = localRegexGuardrail.validateStopWords("hello world", null);
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateStopWordsEmpty() {
+        Boolean res = localRegexGuardrail.validateStopWords("hello world", Map.of());
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void validateStopWordsSingleIndex() throws IOException {
+        SearchResponse searchResponse = createSearchResponse(1);
+        ActionFuture<SearchResponse> future = createSearchResponseFuture(searchResponse);
+        when(this.client.search(any())).thenReturn(future);
+
+        Boolean res = localRegexGuardrail.validateStopWordsSingleIndex("hello world", "test_index", List.of("test_field"));
+        Assert.assertTrue(res);
+    }
+
+    private SearchResponse createSearchResponse(int size) throws IOException {
+        XContentBuilder content = localRegexGuardrail.toXContent(XContentFactory.jsonBuilder(), ToXContent.EMPTY_PARAMS);
+        SearchHit[] hits = new SearchHit[size];
+        if (size > 0) {
+            hits[0] = new SearchHit(0).sourceRef(BytesReference.bytes(content));
+        }
+        return new SearchResponse(
+                new InternalSearchResponse(
+                        new SearchHits(hits, new TotalHits(size, TotalHits.Relation.EQUAL_TO), 1.0f),
+                        InternalAggregations.EMPTY,
+                        new Suggest(Collections.emptyList()),
+                        new SearchProfileShardResults(Collections.emptyMap()),
+                        false,
+                        false,
+                        1
+                ),
+                "",
+                5,
+                5,
+                0,
+                100,
+                ShardSearchFailure.EMPTY_ARRAY,
+                SearchResponse.Clusters.EMPTY
+        );
+    }
+
+    private ActionFuture<SearchResponse> createSearchResponseFuture(SearchResponse searchResponse) {
+        return new ActionFuture<>() {
+            @Override
+            public SearchResponse actionGet() {
+                return searchResponse;
+            }
+
+            @Override
+            public SearchResponse actionGet(String timeout) {
+                return searchResponse;
+            }
+
+            @Override
+            public SearchResponse actionGet(long timeoutMillis) {
+                return searchResponse;
+            }
+
+            @Override
+            public SearchResponse actionGet(long timeout, TimeUnit unit) {
+                return searchResponse;
+            }
+
+            @Override
+            public SearchResponse actionGet(TimeValue timeout) {
+                return searchResponse;
+            }
+
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return false;
+            }
+
+            @Override
+            public SearchResponse get() {
+                return searchResponse;
+            }
+
+            @Override
+            public SearchResponse get(long timeout, TimeUnit unit) {
+                return searchResponse;
+            }
+        };
+    }
+}
