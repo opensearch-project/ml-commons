ea07b85ca0ef8d75334b564db4e269934a916871
Daniel Widdis <widdis@gmail.com>
2024-06-14 09:49:55 -0700

    [Feature/multi_tenancy] Add source map to GetDataObjectResponse (#2489)
    * Add source map to GetDataObjectResponse
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add test for map getter in clients
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- common/src/test/java/org/opensearch/sdk/GetDataObjectResponseTests.java
+++ common/src/test/java/org/opensearch/sdk/GetDataObjectResponseTests.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opensearch.core.xcontent.XContentParser;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -21,18 +22,21 @@ public class GetDataObjectResponseTests {
 
     private String testId;
     private XContentParser testParser;
+    private Map<String, Object> testSource;
 
     @Before
     public void setUp() {
         testId = "test-id";
         testParser = mock(XContentParser.class);
+        testSource = Map.of("foo", "bar");
     }
 
     @Test
     public void testGetDataObjectResponse() {
-        GetDataObjectResponse response = new GetDataObjectResponse.Builder().id(testId).parser(Optional.of(testParser)).build();
+        GetDataObjectResponse response = new GetDataObjectResponse.Builder().id(testId).parser(Optional.of(testParser)).source(testSource).build();
 
         assertEquals(testId, response.id());
         assertEquals(testParser, response.parser().get());
+        assertEquals(testSource, response.source());
     }
 }
