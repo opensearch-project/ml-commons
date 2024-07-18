6a2d1b9612284a5269dd9ec94a1591f829a6094c
Dhrubo Saha <dhrubo@amazon.com>
2024-07-18 13:02:59 -0700

    fixing ml-model tool
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/MLModelTool.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/MLModelTool.java
@@ -71,7 +71,7 @@ public class MLModelTool implements Tool {
         outputParser = o -> {
             try {
                 List<ModelTensors> mlModelOutputs = (List<ModelTensors>) o;
-                Map<String, ?> dataAsMap = mlModelOutputs.getFirst().getMlModelTensors().getFirst().getDataAsMap();
+                Map<String, ?> dataAsMap = mlModelOutputs.get(0).getMlModelTensors().get(0).getDataAsMap();
                 // Return the response field if it exists, otherwise return the whole response as json string.
                 if (dataAsMap.containsKey(responseField)) {
                     return dataAsMap.get(responseField);
