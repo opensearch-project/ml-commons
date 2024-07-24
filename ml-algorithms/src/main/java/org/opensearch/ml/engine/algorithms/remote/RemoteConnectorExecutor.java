5e4ce7a0a20bba47609ae0a0e4c31235774b7a8b
Sicheng Song <sicheng.song@outlook.com>
2024-07-24 02:21:28 -0700

    Add logging for throttling and guardrail in connector (#2725)
    Signed-off-by: b4sjoo <sicheng.song@outlook.com>
    (cherry picked from commit 8f160805080b4ad2d576f4d4ca48007b73f6297e)

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/RemoteConnectorExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/RemoteConnectorExecutor.java
@@ -196,17 +196,20 @@ public interface RemoteConnectorExecutor {
             .getTransient(ConfigConstants.OPENSEARCH_SECURITY_USER_INFO_THREAD_CONTEXT);
         User user = User.parse(userStr);
         if (getRateLimiter() != null && !getRateLimiter().request()) {
+            getLogger().error("Request is throttled at model level.");
             throw new OpenSearchStatusException("Request is throttled at model level.", RestStatus.TOO_MANY_REQUESTS);
         } else if (user != null
             && getUserRateLimiterMap() != null
             && getUserRateLimiterMap().get(user.getName()) != null
             && !getUserRateLimiterMap().get(user.getName()).request()) {
+            getLogger().error("Request is throttled at user level.");
             throw new OpenSearchStatusException(
                 "Request is throttled at user level. If you think there's an issue, please contact your cluster admin.",
                 RestStatus.TOO_MANY_REQUESTS
             );
         } else {
             if (getMlGuard() != null && !getMlGuard().validate(payload, MLGuard.Type.INPUT, parameters)) {
+                getLogger().error("guardrails triggered for user input");
                 throw new IllegalArgumentException("guardrails triggered for user input");
             }
             if (getConnectorClientConfig().getMaxRetryTimes() != 0) {
