72df1bb1348d968a5f16eb834263b93479c07892
Daniel Widdis <widdis@gmail.com>
2024-08-12 12:54:04 -0700

    Add tests for Agent
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

---
+++ plugin/src/test/java/org/opensearch/ml/rest/RestMLAgentTenantAwareIT.java
@@ -0,0 +1,271 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.rest;
+
+import static org.opensearch.ml.common.CommonValue.ML_AGENT_INDEX;
+import static org.opensearch.ml.common.CommonValue.ML_CONNECTOR_INDEX;
+import static org.opensearch.ml.common.CommonValue.ML_MODEL_INDEX;
+import static org.opensearch.ml.common.CommonValue.TENANT_ID;
+import static org.opensearch.ml.common.MLTask.MODEL_ID_FIELD;
+import static org.opensearch.ml.common.input.execute.agent.AgentMLInput.AGENT_ID_FIELD;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.client.Response;
+import org.opensearch.client.ResponseException;
+import org.opensearch.rest.RestRequest;
+
+public class RestMLAgentTenantAwareIT extends MLCommonsTenantAwareRestTestCase {
+
+    private static final String AGENTS_PATH = "/_plugins/_ml/agents/";
+
+    public void testAgentCRUD() throws IOException, InterruptedException {
+        testAgentCRUDMultitenancyEnabled(true);
+        testAgentCRUDMultitenancyEnabled(false);
+    }
+
+    private void testAgentCRUDMultitenancyEnabled(boolean multiTenancyEnabled) throws IOException, InterruptedException {
+        enableMultiTenancy(multiTenancyEnabled);
+
+        /*
+         * Setup
+         */
+        // Create a connector to use
+        RestRequest createConnectorRequest = getRestRequestWithHeadersAndContent(tenantId, createConnectorContent());
+        Response response = makeRequest(createConnectorRequest, POST, CONNECTORS_PATH + "_create");
+        assertOK(response);
+        Map<String, Object> map = responseToMap(response);
+        assertTrue(map.containsKey(CONNECTOR_ID));
+        String connectorId = map.get(CONNECTOR_ID).toString();
+        // Register a remote model to use
+        RestRequest registerModelRequest = getRestRequestWithHeadersAndContent(
+            tenantId,
+            registerRemoteModelContent("test model", connectorId)
+        );
+        response = makeRequest(registerModelRequest, POST, MODELS_PATH + "_register");
+        assertOK(response);
+        map = responseToMap(response);
+        assertTrue(map.containsKey(MODEL_ID_FIELD));
+        String modelId = map.get(MODEL_ID_FIELD).toString();
+
+        /*
+         * Create
+         */
+        // Register a flow agent with a tenant id
+        RestRequest registerAgentRequest = getRestRequestWithHeadersAndContent(tenantId, registerFlowAgentContent("test agent", modelId));
+        response = makeRequest(registerAgentRequest, POST, AGENTS_PATH + "_register");
+        assertOK(response);
+        map = responseToMap(response);
+        assertTrue(map.containsKey(AGENT_ID_FIELD));
+        String agentId = map.get(AGENT_ID_FIELD).toString();
+
+        /*
+         * Get
+         */
+        // Now try to get that agent
+        response = makeRequest(tenantRequest, GET, AGENTS_PATH + agentId);
+        assertOK(response);
+        map = responseToMap(response);
+        assertEquals("test agent", map.get("name"));
+        if (multiTenancyEnabled) {
+            assertEquals(tenantId, map.get(TENANT_ID));
+        } else {
+            assertNull(map.get(TENANT_ID));
+        }
+
+        // Now try again with an other ID
+        if (multiTenancyEnabled) {
+            ResponseException ex = assertThrows(ResponseException.class, () -> makeRequest(otherTenantRequest, GET, AGENTS_PATH + agentId));
+            response = ex.getResponse();
+            assertForbidden(response);
+            map = responseToMap(response);
+            assertEquals(NO_PERMISSION_REASON, getErrorReasonFromResponseMap(map));
+        } else {
+            response = makeRequest(otherTenantRequest, GET, AGENTS_PATH + agentId);
+            assertOK(response);
+            map = responseToMap(response);
+            assertEquals("test agent", map.get("name"));
+        }
+
+        // Now try again with a null ID
+        if (multiTenancyEnabled) {
+            ResponseException ex = assertThrows(ResponseException.class, () -> makeRequest(nullTenantRequest, GET, AGENTS_PATH + agentId));
+            response = ex.getResponse();
+            assertForbidden(response);
+            map = responseToMap(response);
+            assertEquals(MISSING_TENANT_REASON, getErrorReasonFromResponseMap(map));
+        } else {
+            response = makeRequest(nullTenantRequest, GET, AGENTS_PATH + agentId);
+            assertOK(response);
+            map = responseToMap(response);
+            assertEquals("test agent", map.get("name"));
+        }
+
+        /*
+         * Update
+         */
+        // Update Agent not implemented
+
+        /*
+         * Execute
+         */
+        // TODO Add tests here
+
+        /*
+         * Search
+         */
+        // Register a second flow agent using otherTenantId (Using wrong tenant model ID)
+        RestRequest otherAgentRequest = getRestRequestWithHeadersAndContent(
+            otherTenantId,
+            registerFlowAgentContent("other test agent", modelId)
+        );
+        response = makeRequest(otherAgentRequest, POST, AGENTS_PATH + "_register");
+        assertOK(response);
+        map = responseToMap(response);
+        assertTrue(map.containsKey(AGENT_ID_FIELD));
+        String otherAgentId = map.get(AGENT_ID_FIELD).toString();
+
+        // Verify it
+        response = makeRequest(otherTenantRequest, GET, AGENTS_PATH + otherAgentId);
+        assertOK(response);
+        map = responseToMap(response);
+        assertEquals("other test agent", map.get("name"));
+
+        // Search should show only the agent for tenant
+        response = makeRequest(tenantMatchAllRequest, GET, AGENTS_PATH + "_search");
+        assertOK(response);
+        SearchResponse searchResponse = searchResponseFromResponse(response);
+        if (multiTenancyEnabled) {
+            // TODO Change to 1 when https://github.com/opensearch-project/ml-commons/pull/2803 is merged
+            assertEquals(2, searchResponse.getHits().getTotalHits().value);
+            assertEquals(tenantId, searchResponse.getHits().getHits()[0].getSourceAsMap().get(TENANT_ID));
+        } else {
+            assertEquals(2, searchResponse.getHits().getTotalHits().value);
+            assertNull(searchResponse.getHits().getHits()[0].getSourceAsMap().get(TENANT_ID));
+            assertNull(searchResponse.getHits().getHits()[1].getSourceAsMap().get(TENANT_ID));
+        }
+
+        // Search should show only the agent for other tenant
+        response = makeRequest(otherTenantMatchAllRequest, GET, AGENTS_PATH + "_search");
+        assertOK(response);
+        searchResponse = searchResponseFromResponse(response);
+        if (multiTenancyEnabled) {
+            // TODO Change to 1 when https://github.com/opensearch-project/ml-commons/pull/2803 is merged
+            assertEquals(2, searchResponse.getHits().getTotalHits().value);
+            // TODO change [1] to [0]
+            assertEquals(otherTenantId, searchResponse.getHits().getHits()[1].getSourceAsMap().get(TENANT_ID));
+        } else {
+            assertEquals(2, searchResponse.getHits().getTotalHits().value);
+            assertNull(searchResponse.getHits().getHits()[0].getSourceAsMap().get(TENANT_ID));
+            assertNull(searchResponse.getHits().getHits()[1].getSourceAsMap().get(TENANT_ID));
+        }
+
+        // Search should fail without a tenant id
+        if (multiTenancyEnabled) {
+            ResponseException ex = assertThrows(
+                ResponseException.class,
+                () -> makeRequest(nullTenantMatchAllRequest, GET, AGENTS_PATH + "_search")
+            );
+            response = ex.getResponse();
+            assertForbidden(response);
+            map = responseToMap(response);
+            assertEquals(MISSING_TENANT_REASON, getErrorReasonFromResponseMap(map));
+        } else {
+            response = makeRequest(nullTenantMatchAllRequest, GET, AGENTS_PATH + "_search");
+            assertOK(response);
+            searchResponse = searchResponseFromResponse(response);
+            assertEquals(2, searchResponse.getHits().getTotalHits().value);
+            assertNull(searchResponse.getHits().getHits()[0].getSourceAsMap().get(TENANT_ID));
+            assertNull(searchResponse.getHits().getHits()[1].getSourceAsMap().get(TENANT_ID));
+        }
+
+        /*
+         * Delete
+         */
+        // Delete the agents
+        // First test that we can't delete other tenant agents
+        if (multiTenancyEnabled) {
+            ResponseException ex = assertThrows(
+                ResponseException.class,
+                () -> makeRequest(tenantRequest, DELETE, AGENTS_PATH + otherAgentId)
+            );
+            response = ex.getResponse();
+            assertForbidden(response);
+            map = responseToMap(response);
+            assertEquals(NO_PERMISSION_REASON, getErrorReasonFromResponseMap(map));
+
+            ex = assertThrows(ResponseException.class, () -> makeRequest(otherTenantRequest, DELETE, AGENTS_PATH + agentId));
+            response = ex.getResponse();
+            assertForbidden(response);
+            map = responseToMap(response);
+            assertEquals(NO_PERMISSION_REASON, getErrorReasonFromResponseMap(map));
+
+            // and can't delete without a tenant ID either
+            ex = assertThrows(ResponseException.class, () -> makeRequest(nullTenantRequest, DELETE, AGENTS_PATH + agentId));
+            response = ex.getResponse();
+            assertForbidden(response);
+            map = responseToMap(response);
+            assertEquals(MISSING_TENANT_REASON, getErrorReasonFromResponseMap(map));
+        }
+
+        // Now actually do the deletions. Same result whether multi-tenancy is enabled.
+
+        // Delete from tenant
+        response = makeRequest(tenantRequest, DELETE, AGENTS_PATH + agentId);
+        assertOK(response);
+        map = responseToMap(response);
+        assertEquals(agentId, map.get(DOC_ID).toString());
+
+        // Verify the deletion
+        ResponseException ex = assertThrows(ResponseException.class, () -> makeRequest(tenantRequest, GET, AGENTS_PATH + agentId));
+        response = ex.getResponse();
+        assertNotFound(response);
+        map = responseToMap(response);
+        assertEquals("Failed to find agent with the provided agent id: " + agentId, getErrorReasonFromResponseMap(map));
+
+        // Delete from other tenant
+        response = makeRequest(otherTenantRequest, DELETE, AGENTS_PATH + otherAgentId);
+        assertOK(response);
+        map = responseToMap(response);
+        assertEquals(otherAgentId, map.get(DOC_ID).toString());
+
+        // Verify the deletion
+        ex = assertThrows(ResponseException.class, () -> makeRequest(otherTenantRequest, GET, AGENTS_PATH + otherAgentId));
+        response = ex.getResponse();
+        assertNotFound(response);
+        map = responseToMap(response);
+        assertEquals("Failed to find agent with the provided agent id: " + otherAgentId, getErrorReasonFromResponseMap(map));
+
+        // Cleanup (since deletions may linger in search results)
+        deleteIndexWithAdminClient(ML_AGENT_INDEX);
+        // We test model and connector deletion elsewhere, just wipe the index
+        deleteIndexWithAdminClient(ML_MODEL_INDEX);
+        deleteIndexWithAdminClient(ML_CONNECTOR_INDEX);
+    }
+
+    private static String registerFlowAgentContent(String name, String modelId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{\n");
+        sb.append("  \"name\": \"").append(name).append("\",\n");
+        sb.append("  \"type\": \"flow\",\n");
+        sb.append("  \"tools\": [");
+        sb.append("    {");
+        sb.append("      \"type\": \"MLModelTool\",");
+        sb.append("      \"parameters\": {");
+        sb.append("        \"model_id\": \"").append(modelId).append("\",");
+        sb
+            .append("        \"prompt\": \"\\n\\n")
+            .append("Human: Always answer questions based on the given context first.\\n\\n")
+            .append("Context:\\n${parameters.vector_tool.output}\\n\\nHuman:${parameters.question}\\n\\nAssistant:\"");
+        sb.append("      }");
+        sb.append("    }");
+        sb.append("  ]");
+        sb.append("}");
+        return sb.toString();
+    }
+}
