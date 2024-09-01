a0c94e4943f5b37126550dd5dd0b2929d9da8368
Daniel Widdis <widdis@gmail.com>
2024-08-31 23:41:25 -0700

    Field rename is better fix for version typing bug
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- common/src/main/java/org/opensearch/sdk/SdkClientUtils.java
+++ common/src/main/java/org/opensearch/sdk/SdkClientUtils.java
@@ -56,14 +56,14 @@ public class SdkClientUtils {
     }
 
     /**
-     * If an internal variable name matches a field name not assigned to that field, Jackson's ObjectMapper can parse the wrong type. This method removes quotes around numeric fields to work around these cases.
+     * If an internal variable name matches a field name not assigned to that field, Jackson's ObjectMapper can assign it to the wrong name. This will align with XContent parsing.
      * @param field The JSON field to remove the quotes from its value
+     * @param newField The JSON field that it's supposed to be
      * @param json The full JSON to process
-     * @return The JSON with the quotes removed from a numeric value in the specified field
+     * @return The JSON with the field renamed
      */
-    public static String unwrapQuotedInteger(String field, String json) {
-        String regex = "(\"" + Pattern.quote(field) + "\"):\"(\\d+)\"";
-        return json.replaceAll(regex, "$1:$2");
+    public static String renameField(String field, String newField, String json) {
+        return json.replaceAll("\"" + Pattern.quote(field) + "\"", "\"" + Pattern.quote(newField) + "\"");
     }
 
     /**
