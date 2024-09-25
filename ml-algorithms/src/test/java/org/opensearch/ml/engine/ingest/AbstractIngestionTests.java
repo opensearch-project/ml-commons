d07ee6a40962f4bdc1695758b0d837b17748fea0
Xun Zhang <xunzh@amazon.com>
2024-09-25 15:25:12 -0700

    use connector credential in offline batch ingestion
    Signed-off-by: Xun Zhang <xunzh@amazon.com>

--- ml-algorithms/src/test/java/org/opensearch/ml/engine/ingest/AbstractIngestionTests.java
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/ingest/AbstractIngestionTests.java
@@ -166,7 +166,8 @@ public class AbstractIngestionTests {
             fieldMap,
             ingestFields,
             new HashMap<>(),
-            new HashMap<>()
+            new HashMap<>(),
+            null
         );
         Map<String, Object> result = s3DataIngestion.filterFieldMapping(mlBatchIngestionInput, 0);
 
@@ -190,7 +191,8 @@ public class AbstractIngestionTests {
             fieldMap,
             ingestFields,
             new HashMap<>(),
-            new HashMap<>()
+            new HashMap<>(),
+            null
         );
 
         // Act
@@ -219,7 +221,8 @@ public class AbstractIngestionTests {
             fieldMap,
             ingestFields,
             new HashMap<>(),
-            new HashMap<>()
+            new HashMap<>(),
+            null
         );
 
         // Act
@@ -292,7 +295,8 @@ public class AbstractIngestionTests {
             fieldMap,
             ingestFields,
             new HashMap<>(),
-            new HashMap<>()
+            new HashMap<>(),
+            null
         );
         ActionListener<BulkResponse> bulkResponseListener = mock(ActionListener.class);
         s3DataIngestion.batchIngest(sourceLines, mlBatchIngestionInput, bulkResponseListener, 0, true);
@@ -318,7 +322,8 @@ public class AbstractIngestionTests {
             fieldMap,
             ingestFields,
             new HashMap<>(),
-            new HashMap<>()
+            new HashMap<>(),
+            null
         );
         ActionListener<BulkResponse> bulkResponseListener = mock(ActionListener.class);
         s3DataIngestion.batchIngest(sourceLines, mlBatchIngestionInput, bulkResponseListener, 0, false);
