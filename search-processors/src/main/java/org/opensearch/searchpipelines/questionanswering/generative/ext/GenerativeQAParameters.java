f1f19720c3c54869d3db79dc44a98a08305c226a
Pavan Yekbote <mail2pavanyekbote@gmail.com>
2024-10-11 11:16:38 -0700

    fix: field names
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>

--- search-processors/src/main/java/org/opensearch/searchpipelines/questionanswering/generative/ext/GenerativeQAParameters.java
+++ search-processors/src/main/java/org/opensearch/searchpipelines/questionanswering/generative/ext/GenerativeQAParameters.java
@@ -47,7 +47,7 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
 
     // Optional parameter; if provided, conversational memory will be used for RAG
     // and the current interaction will be saved in the conversation referenced by this id.
-    private static final String CONVERSATION_ID = "memory_id";
+    private static final String CONVERSATION_ID = "conversation_id";
 
     // Optional parameter; if an LLM model is not set at the search pipeline level, one must be
     // provided at the search request level.
@@ -63,7 +63,7 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
 
     // Optional parameter; this parameter controls the number of the interactions to include
     // in the user prompt.
-    private static final String INTERACTION_SIZE = "message_size";
+    private static final String INTERACTION_SIZE = "interaction_size";
 
     // Optional parameter; this parameter controls how long the search pipeline waits for a response
     // from a remote inference endpoint before timing out the request.
