bc6ec93ac5090a1135da0a8e74f294c433152c6e
Pavan Yekbote <mail2pavanyekbote@gmail.com>
2024-10-11 10:24:47 -0700

    [Backport 2.12] Bug Fix: Fix for rag processor throwing NPE (#3089)
    * [Backport 2.17] Bug Fix: Fix for rag processor throwing NPE when optional parameters are not provided (#3066) (#3076)
    
    (cherry picked from commit 8b5b38e4b28e182a0cdfbf1c55c6ef00e6663a57)
    
    Co-authored-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    
    * fix: spotless changes
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    
    ---------
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    Co-authored-by: opensearch-trigger-bot[bot] <98922864+opensearch-trigger-bot[bot]@users.noreply.github.com>

--- search-processors/src/main/java/org/opensearch/searchpipelines/questionanswering/generative/ext/GenerativeQAParameters.java
+++ search-processors/src/main/java/org/opensearch/searchpipelines/questionanswering/generative/ext/GenerativeQAParameters.java
@@ -17,21 +17,22 @@
  */
 package org.opensearch.searchpipelines.questionanswering.generative.ext;
 
+import static org.opensearch.core.xcontent.XContentParserUtils.ensureExpectedToken;
+
 import java.io.IOException;
 import java.util.Objects;
 
-import org.opensearch.core.ParseField;
 import org.opensearch.core.common.Strings;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
 import org.opensearch.core.common.io.stream.Writeable;
-import org.opensearch.core.xcontent.ObjectParser;
 import org.opensearch.core.xcontent.ToXContentObject;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.xcontent.XContentParser;
 
 import com.google.common.base.Preconditions;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -44,43 +45,31 @@ import lombok.Setter;
 @NoArgsConstructor
 public class GenerativeQAParameters implements Writeable, ToXContentObject {
 
-    private static final ObjectParser<GenerativeQAParameters, Void> PARSER;
-
     // Optional parameter; if provided, conversational memory will be used for RAG
     // and the current interaction will be saved in the conversation referenced by this id.
-    private static final ParseField CONVERSATION_ID = new ParseField("conversation_id");
+    private static final String CONVERSATION_ID = "memory_id";
 
     // Optional parameter; if an LLM model is not set at the search pipeline level, one must be
     // provided at the search request level.
-    private static final ParseField LLM_MODEL = new ParseField("llm_model");
+    private static final String LLM_MODEL = "llm_model";
 
     // Required parameter; this is sent to LLMs as part of the user prompt.
     // TODO support question rewriting when chat history is not used (conversation_id is not provided).
-    private static final ParseField LLM_QUESTION = new ParseField("llm_question");
+    private static final String LLM_QUESTION = "llm_question";
 
     // Optional parameter; this parameter controls the number of search results ("contexts") to
     // include in the user prompt.
-    private static final ParseField CONTEXT_SIZE = new ParseField("context_size");
+    private static final String CONTEXT_SIZE = "context_size";
 
     // Optional parameter; this parameter controls the number of the interactions to include
     // in the user prompt.
-    private static final ParseField INTERACTION_SIZE = new ParseField("interaction_size");
+    private static final String INTERACTION_SIZE = "message_size";
 
     // Optional parameter; this parameter controls how long the search pipeline waits for a response
     // from a remote inference endpoint before timing out the request.
-    private static final ParseField TIMEOUT = new ParseField("timeout");
+    private static final String TIMEOUT = "timeout";
 
     public static final int SIZE_NULL_VALUE = -1;
-
-    static {
-        PARSER = new ObjectParser<>("generative_qa_parameters", GenerativeQAParameters::new);
-        PARSER.declareString(GenerativeQAParameters::setConversationId, CONVERSATION_ID);
-        PARSER.declareString(GenerativeQAParameters::setLlmModel, LLM_MODEL);
-        PARSER.declareString(GenerativeQAParameters::setLlmQuestion, LLM_QUESTION);
-        PARSER.declareIntOrNull(GenerativeQAParameters::setContextSize, SIZE_NULL_VALUE, CONTEXT_SIZE);
-        PARSER.declareIntOrNull(GenerativeQAParameters::setInteractionSize, SIZE_NULL_VALUE, INTERACTION_SIZE);
-        PARSER.declareIntOrNull(GenerativeQAParameters::setTimeout, SIZE_NULL_VALUE, TIMEOUT);
-    }
 
     @Setter
     @Getter
@@ -106,6 +95,7 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
     @Getter
     private Integer timeout;
 
+    @Builder
     public GenerativeQAParameters(
         String conversationId,
         String llmModel,
@@ -119,7 +109,7 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
 
         // TODO: keep this requirement until we can extract the question from the query or from the request processor parameters
         // for question rewriting.
-        Preconditions.checkArgument(!Strings.isNullOrEmpty(llmQuestion), LLM_QUESTION.getPreferredName() + " must be provided.");
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(llmQuestion), LLM_QUESTION + " must be provided.");
         this.llmQuestion = llmQuestion;
         this.contextSize = (contextSize == null) ? SIZE_NULL_VALUE : contextSize;
         this.interactionSize = (interactionSize == null) ? SIZE_NULL_VALUE : interactionSize;
@@ -137,13 +127,33 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
 
     @Override
     public XContentBuilder toXContent(XContentBuilder xContentBuilder, Params params) throws IOException {
-        return xContentBuilder
-            .field(CONVERSATION_ID.getPreferredName(), this.conversationId)
-            .field(LLM_MODEL.getPreferredName(), this.llmModel)
-            .field(LLM_QUESTION.getPreferredName(), this.llmQuestion)
-            .field(CONTEXT_SIZE.getPreferredName(), this.contextSize)
-            .field(INTERACTION_SIZE.getPreferredName(), this.interactionSize)
-            .field(TIMEOUT.getPreferredName(), this.timeout);
+        xContentBuilder.startObject();
+        if (this.conversationId != null) {
+            xContentBuilder.field(CONVERSATION_ID, this.conversationId);
+        }
+
+        if (this.llmModel != null) {
+            xContentBuilder.field(LLM_MODEL, this.llmModel);
+        }
+
+        if (this.llmQuestion != null) {
+            xContentBuilder.field(LLM_QUESTION, this.llmQuestion);
+        }
+
+        if (this.contextSize != null) {
+            xContentBuilder.field(CONTEXT_SIZE, this.contextSize);
+        }
+
+        if (this.interactionSize != null) {
+            xContentBuilder.field(INTERACTION_SIZE, this.interactionSize);
+        }
+
+        if (this.timeout != null) {
+            xContentBuilder.field(TIMEOUT, this.timeout);
+        }
+
+        xContentBuilder.endObject();
+        return xContentBuilder;
     }
 
     @Override
@@ -159,7 +169,52 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
     }
 
     public static GenerativeQAParameters parse(XContentParser parser) throws IOException {
-        return PARSER.parse(parser, null);
+        String conversationId = null;
+        String llmModel = null;
+        String llmQuestion = null;
+        Integer contextSize = null;
+        Integer interactionSize = null;
+        Integer timeout = null;
+
+        ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
+        while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
+            String field = parser.currentName();
+            parser.nextToken();
+
+            switch (field) {
+                case CONVERSATION_ID:
+                    conversationId = parser.text();
+                    break;
+                case LLM_MODEL:
+                    llmModel = parser.text();
+                    break;
+                case LLM_QUESTION:
+                    llmQuestion = parser.text();
+                    break;
+                case CONTEXT_SIZE:
+                    contextSize = parser.intValue();
+                    break;
+                case INTERACTION_SIZE:
+                    interactionSize = parser.intValue();
+                    break;
+                case TIMEOUT:
+                    timeout = parser.intValue();
+                    break;
+                default:
+                    parser.skipChildren();
+                    break;
+            }
+        }
+
+        return GenerativeQAParameters
+            .builder()
+            .conversationId(conversationId)
+            .llmModel(llmModel)
+            .llmQuestion(llmQuestion)
+            .contextSize(contextSize)
+            .interactionSize(interactionSize)
+            .timeout(timeout)
+            .build();
     }
 
     @Override
