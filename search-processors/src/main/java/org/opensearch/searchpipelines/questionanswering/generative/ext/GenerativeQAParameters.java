bf77d1be7b654e0dbee89236e3242f60bb5b0df2
Pavan Yekbote <mail2pavanyekbote@gmail.com>
2024-10-29 09:02:09 -0700

    [bug-fix] Handle BWC for bedrock converse API (#3173)
    * fix: bwc check for llm messages
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    
    * fix: add bwc for llmquestion
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    
    * test: adding ut for bwc
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    
    * fix: test case which counts optional occurence
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    
    ---------
    
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>
    (cherry picked from commit 24d264007a0ae99506b133d7aa01d59d4c53c95b)

--- search-processors/src/main/java/org/opensearch/searchpipelines/questionanswering/generative/ext/GenerativeQAParameters.java
+++ search-processors/src/main/java/org/opensearch/searchpipelines/questionanswering/generative/ext/GenerativeQAParameters.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import org.opensearch.Version;
 import org.opensearch.core.common.Strings;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
@@ -31,6 +32,7 @@ import org.opensearch.core.common.io.stream.Writeable;
 import org.opensearch.core.xcontent.ToXContentObject;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.xcontent.XContentParser;
+import org.opensearch.ml.common.CommonValue;
 import org.opensearch.searchpipelines.questionanswering.generative.llm.MessageBlock;
 
 import com.google.common.base.Preconditions;
@@ -85,6 +87,8 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
     private static final String LLM_MESSAGES_FIELD = "llm_messages";
 
     public static final int SIZE_NULL_VALUE = -1;
+
+    static final Version MINIMAL_SUPPORTED_VERSION_FOR_BEDROCK_CONVERSE_LLM_MESSAGES = CommonValue.VERSION_2_18_0;
 
     @Setter
     @Getter
@@ -185,16 +189,27 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
     }
 
     public GenerativeQAParameters(StreamInput input) throws IOException {
+        Version version = input.getVersion();
         this.conversationId = input.readOptionalString();
         this.llmModel = input.readOptionalString();
-        this.llmQuestion = input.readOptionalString();
+
+        // this string was made optional in 2.18
+        if (version.onOrAfter(MINIMAL_SUPPORTED_VERSION_FOR_BEDROCK_CONVERSE_LLM_MESSAGES)) {
+            this.llmQuestion = input.readOptionalString();
+        } else {
+            this.llmQuestion = input.readString();
+        }
+
         this.systemPrompt = input.readOptionalString();
         this.userInstructions = input.readOptionalString();
         this.contextSize = input.readInt();
         this.interactionSize = input.readInt();
         this.timeout = input.readInt();
         this.llmResponseField = input.readOptionalString();
-        this.llmMessages.addAll(input.readList(MessageBlock::new));
+
+        if (version.onOrAfter(MINIMAL_SUPPORTED_VERSION_FOR_BEDROCK_CONVERSE_LLM_MESSAGES)) {
+            this.llmMessages.addAll(input.readList(MessageBlock::new));
+        }
     }
 
     @Override
@@ -246,16 +261,27 @@ public class GenerativeQAParameters implements Writeable, ToXContentObject {
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
+        Version version = out.getVersion();
         out.writeOptionalString(conversationId);
         out.writeOptionalString(llmModel);
-        out.writeOptionalString(llmQuestion);
+
+        // this string was made optional in 2.18
+        if (version.onOrAfter(MINIMAL_SUPPORTED_VERSION_FOR_BEDROCK_CONVERSE_LLM_MESSAGES)) {
+            out.writeOptionalString(llmQuestion);
+        } else {
+            out.writeString(llmQuestion);
+        }
+
         out.writeOptionalString(systemPrompt);
         out.writeOptionalString(userInstructions);
         out.writeInt(contextSize);
         out.writeInt(interactionSize);
         out.writeInt(timeout);
         out.writeOptionalString(llmResponseField);
-        out.writeList(llmMessages);
+
+        if (version.onOrAfter(MINIMAL_SUPPORTED_VERSION_FOR_BEDROCK_CONVERSE_LLM_MESSAGES)) {
+            out.writeList(llmMessages);
+        }
     }
 
     public static GenerativeQAParameters parse(XContentParser parser) throws IOException {
