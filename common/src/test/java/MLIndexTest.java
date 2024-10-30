b383b48d532c75fbd11e60efe4ce7a6afa5f95d1
Pavan Yekbote <mail2pavanyekbote@gmail.com>
2024-10-29 17:26:35 -0700

    test: adding testcase for MLIndex to catch failures before runtime
    Signed-off-by: Pavan Yekbote <mail2pavanyekbote@gmail.com>

---
+++ common/src/test/java/MLIndexTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.opensearch.ml.common.MLIndex;
+
+public class MLIndexTest {
+
+    /**
+     * Mappings are initialised during runtime when the MLIndex enum is referenced.
+     * We want to catch any failure in mapping assignment before runtime.
+     * This test simply references the enums to fetch the mapping. It will fail in case the enum is not initialized.
+    **/
+     @Test
+     public void testValidateMappingsForSystemIndices() {
+         for (MLIndex index : MLIndex.values()) {
+             String mapping = index.getMapping();
+         }
+     }
+}
