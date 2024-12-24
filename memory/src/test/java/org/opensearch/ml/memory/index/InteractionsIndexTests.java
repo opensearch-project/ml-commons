06d39b9fc4a5673636745beaf5b8f05a4acb0fb6
Rithin Pullela <rithinp@amazon.com>
2024-12-24 12:26:04 -0800

    Enhance Message and Memory API Validation and storage (#3283)
    * Enchance Message and Memory API Validation and storage
    
    Throw an error when an unknown field is provided in CreateConversation or CreateInteraction.
    Skip saving empty fields in interactions and conversations to optimize storage usage.
    Modify GET requests for interactions and conversations to return only non-null fields.
    Throw an exception if all fields in a create interaction call are empty or null.
    Add unit tests to cover the above cases.
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    * Update unit test to check for null instead of empty map
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    * Refactored userstr to Camel Case
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    * Addressing comments
    
    Used assertThrows and added promptTemplate with empty string in test_ToXContent to ensure well rounded testing of expected functionality
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    * Undo: throw an error when an unknown field is provided in CreateConversation or CreateInteraction.
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    ---------
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>

--- memory/src/test/java/org/opensearch/ml/memory/index/InteractionsIndexTests.java
+++ memory/src/test/java/org/opensearch/ml/memory/index/InteractionsIndexTests.java
@@ -156,7 +156,7 @@ public class InteractionsIndexTests extends OpenSearchTestCase {
     }
 
     private void setupDenyAccess(String user) {
-        String userstr = user == null ? "" : user + "||";
+        String userStr = user == null ? "" : user + "||";
         doAnswer(invocation -> {
             ActionListener<Boolean> al = invocation.getArgument(1);
             al.onResponse(false);
@@ -164,7 +164,7 @@ public class InteractionsIndexTests extends OpenSearchTestCase {
         }).when(conversationMetaIndex).checkAccess(anyString(), any());
         doAnswer(invocation -> {
             ThreadContext tc = new ThreadContext(Settings.EMPTY);
-            tc.putTransient(ConfigConstants.OPENSEARCH_SECURITY_USER_INFO_THREAD_CONTEXT, userstr);
+            tc.putTransient(ConfigConstants.OPENSEARCH_SECURITY_USER_INFO_THREAD_CONTEXT, userStr);
             return tc;
         }).when(threadPool).getThreadContext();
     }
