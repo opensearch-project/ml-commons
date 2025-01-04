3f7a00f3d675bc19264c5342664c4488b7a30392
tkykenmt <tkykenmto+github.com@gmail.com>
2025-01-04 23:20:43 +0900

    Add pre and post process functions for Bedrock Rerank API #3254
    Signed-off-by: tkykenmt <tkykenmto+github.com@gmail.com>

--- common/src/main/java/org/opensearch/ml/common/connector/MLPostProcessFunction.java
+++ common/src/main/java/org/opensearch/ml/common/connector/MLPostProcessFunction.java
@@ -12,6 +12,7 @@ import java.util.function.Function;
 
 import org.opensearch.ml.common.connector.functions.postprocess.BedrockBatchJobArnPostProcessFunction;
 import org.opensearch.ml.common.connector.functions.postprocess.BedrockEmbeddingPostProcessFunction;
+import org.opensearch.ml.common.connector.functions.postprocess.BedrockRerankPostProcessFunction;
 import org.opensearch.ml.common.connector.functions.postprocess.CohereRerankPostProcessFunction;
 import org.opensearch.ml.common.connector.functions.postprocess.EmbeddingPostProcessFunction;
 import org.opensearch.ml.common.output.model.ModelTensor;
@@ -23,6 +24,7 @@ public class MLPostProcessFunction {
     public static final String BEDROCK_EMBEDDING = "connector.post_process.bedrock.embedding";
     public static final String BEDROCK_BATCH_JOB_ARN = "connector.post_process.bedrock.batch_job_arn";
     public static final String COHERE_RERANK = "connector.post_process.cohere.rerank";
+    public static final String BEDROCK_RERANK = "connector.post_process.bedrock.rerank";
     public static final String DEFAULT_EMBEDDING = "connector.post_process.default.embedding";
     public static final String DEFAULT_RERANK = "connector.post_process.default.rerank";
 
@@ -35,12 +37,14 @@ public class MLPostProcessFunction {
         BedrockEmbeddingPostProcessFunction bedrockEmbeddingPostProcessFunction = new BedrockEmbeddingPostProcessFunction();
         BedrockBatchJobArnPostProcessFunction batchJobArnPostProcessFunction = new BedrockBatchJobArnPostProcessFunction();
         CohereRerankPostProcessFunction cohereRerankPostProcessFunction = new CohereRerankPostProcessFunction();
+        BedrockRerankPostProcessFunction bedrockRerankPostProcessFunction = new BedrockRerankPostProcessFunction();
         JSON_PATH_EXPRESSION.put(OPENAI_EMBEDDING, "$.data[*].embedding");
         JSON_PATH_EXPRESSION.put(COHERE_EMBEDDING, "$.embeddings");
         JSON_PATH_EXPRESSION.put(DEFAULT_EMBEDDING, "$[*]");
         JSON_PATH_EXPRESSION.put(BEDROCK_EMBEDDING, "$.embedding");
         JSON_PATH_EXPRESSION.put(BEDROCK_BATCH_JOB_ARN, "$");
         JSON_PATH_EXPRESSION.put(COHERE_RERANK, "$.results");
+        JSON_PATH_EXPRESSION.put(BEDROCK_RERANK, "$.results");
         JSON_PATH_EXPRESSION.put(DEFAULT_RERANK, "$[*]");
         POST_PROCESS_FUNCTIONS.put(OPENAI_EMBEDDING, embeddingPostProcessFunction);
         POST_PROCESS_FUNCTIONS.put(COHERE_EMBEDDING, embeddingPostProcessFunction);
@@ -48,6 +52,7 @@ public class MLPostProcessFunction {
         POST_PROCESS_FUNCTIONS.put(BEDROCK_EMBEDDING, bedrockEmbeddingPostProcessFunction);
         POST_PROCESS_FUNCTIONS.put(BEDROCK_BATCH_JOB_ARN, batchJobArnPostProcessFunction);
         POST_PROCESS_FUNCTIONS.put(COHERE_RERANK, cohereRerankPostProcessFunction);
+        POST_PROCESS_FUNCTIONS.put(BEDROCK_RERANK, bedrockRerankPostProcessFunction);
         POST_PROCESS_FUNCTIONS.put(DEFAULT_RERANK, cohereRerankPostProcessFunction);
     }
 
---
+++ common/src/test/java/org/opensearch/ml/common/connector/functions/postprocess/BedrockRerankPostProcessFunctionTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.connector.functions.postprocess;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.opensearch.ml.common.output.model.ModelTensor;
+
+public class BedrockRerankPostProcessFunctionTest {
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    BedrockRerankPostProcessFunction function;
+
+    @Before
+    public void setUp() {
+        function = new BedrockRerankPostProcessFunction();
+    }
+
+    @Test
+    public void process_WrongInput_NotList() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Post process function input is not a List.");
+        function.apply("abc");
+    }
+
+    @Test
+    public void process_WrongInput_NotCorrectList() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Post process function input is not a List of Map.");
+        function.apply(Arrays.asList("abc"));
+    }
+
+    @Test
+    public void process_WrongInput_NotCorrectMap() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("The rerank result should contain index and relevance_score.");
+        function.apply(Arrays.asList(Map.of("test1", "value1")));
+    }
+
+    @Test
+    public void process_CorrectInput() {
+        List<Map<String, Object>> rerankResults = List
+            .of(
+                Map.of("index", 2, "relevanceScore", 0.7711548805236816),
+                Map.of("index", 0, "relevanceScore", 0.0025114635936915874),
+                Map.of("index", 1, "relevanceScore", 2.4876489987946115e-05),
+                Map.of("index", 3, "relevanceScore", 6.339210358419223e-06)
+            );
+        List<ModelTensor> result = function.apply(rerankResults);
+        assertEquals(4, result.size());
+        assertEquals(1, result.get(0).getData().length);
+        assertEquals(0.0025114635936915874, result.get(0).getData()[0]);
+        assertEquals(2.4876489987946115e-05, result.get(1).getData()[0]);
+        assertEquals(0.7711548805236816, result.get(2).getData()[0]);
+        assertEquals(6.339210358419223e-06, result.get(3).getData()[0]);
+    }
+}
