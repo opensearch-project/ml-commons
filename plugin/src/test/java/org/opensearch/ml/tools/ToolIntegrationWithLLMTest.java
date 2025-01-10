382b4ec298e1dba8fa3771933acded877df281f6
Dhrubo Saha <dhrubo@amazon.com>
2025-01-09 19:26:26 -0500

    changing back
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/tools/ToolIntegrationWithLLMTest.java
+++ plugin/src/test/java/org/opensearch/ml/tools/ToolIntegrationWithLLMTest.java
@@ -93,14 +93,12 @@ public abstract class ToolIntegrationWithLLMTest extends RestBaseAgentToolsIT {
     protected Response waitResponseMeetingCondition(String method, String endpoint, String jsonEntity, Predicate<Response> condition) {
         for (int attempt = 1; attempt <= MAX_RETRIES; attempt++) {
             Response response = TestHelper.makeRequest(client(), method, endpoint, null, jsonEntity, null);
-            RestStatus status = RestStatus.fromCode(response.getStatusLine().getStatusCode());
-            assertEquals(RestStatus.OK, status);
-            logger.info("Attempt {}: Status: {}, Response: {}", attempt, status, response);
-            if (status == RestStatus.OK && condition.test(response)) {
+            assertEquals(RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
+            if (condition.test(response)) {
                 return response;
             }
             logger.info("The {}-th attempt on {}:{} . response: {}", attempt, method, endpoint, response.toString());
-            Thread.sleep(DEFAULT_TASK_RESULT_QUERY_INTERVAL_IN_MILLISECOND * attempt);
+            Thread.sleep(DEFAULT_TASK_RESULT_QUERY_INTERVAL_IN_MILLISECOND);
         }
         fail(
             String
