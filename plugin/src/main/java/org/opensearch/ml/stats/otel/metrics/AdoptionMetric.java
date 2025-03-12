7847bee16fd84aa541c0dc18b9534f4af9db51a9
Pavan Yekbote <pybot@amazon.com>
2025-03-11 21:31:29 -0700

    feat(telemetry aware plugin): adding counters and implementing telemetry aware plugin

---
+++ plugin/src/main/java/org/opensearch/ml/stats/otel/metrics/AdoptionMetric.java
@@ -0,0 +1,15 @@
+package org.opensearch.ml.stats.otel.metrics;
+
+import lombok.Getter;
+
+@Getter
+public enum AdoptionMetric {
+    MODEL_COUNT("Number of models created"),
+    CONNECTOR_COUNT("Number of connectors created");
+
+    private final String description;
+
+    AdoptionMetric(String description) {
+        this.description = description;
+    }
+}
