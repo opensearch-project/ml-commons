aa18cae0931b60ff7d12657f5b00b4bae3356c2f
zhichao-aws <zhichaog@amazon.com>
2025-04-21 13:50:00 +0800

    Add BERT Tokenizer as OpenSearch built-in analyzer (#3719)
    * bert analyzer
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * add license header
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * add rest test case
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * load from zip
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * address comments
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    * retry for init
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    
    ---------
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    Signed-off-by: Abdul Muneer Kolarkunnu <muneer.kolarkunnu@netapp.com>

---
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/analysis/HFModelAnalyzerProviderTests.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.ml.engine.analysis;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.junit.Test;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.core.index.Index;
+import org.opensearch.index.IndexSettings;
+import org.opensearch.index.analysis.AnalyzerProvider;
+import org.opensearch.test.IndexSettingsModule;
+
+public class HFModelAnalyzerProviderTests extends HFModelAnalyzerTestCase {
+    @Test
+    public void testProvide() {
+        final Index index = new Index("test", "_na_");
+        Settings settings = Settings
+            .builder()
+            .put("tokenizer_id", "opensearch-project/opensearch-neural-sparse-encoding-doc-v2-mini")
+            .build();
+        final IndexSettings indexProperties = IndexSettingsModule.newIndexSettings(index, settings);
+
+        AnalyzerProvider<HFModelAnalyzer> analyzerProvider = new HFModelAnalyzerProvider(indexProperties, null, "test", settings);
+        assertNotNull(analyzerProvider);
+        Analyzer analyzer = analyzerProvider.get();
+        assertNotNull(analyzer);
+        assertSame(analyzer, analyzerProvider.get());
+    }
+}
