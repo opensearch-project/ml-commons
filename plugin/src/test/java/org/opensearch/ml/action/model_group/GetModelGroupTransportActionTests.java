4b2621773a43723b6e79fc7363a6897128c6aef2
Daniel Widdis <widdis@gmail.com>
2025-04-23 09:18:32 -0700

    Replace null GetResponse with valid response and not exists (#3759)
    * Replace null GetResponse with valid response and not exists
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Remove unnecessary null check impacting coverage check
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add tests to increase coverage
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Test IndexNotFound branch
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    Signed-off-by: Abdul Muneer Kolarkunnu <muneer.kolarkunnu@netapp.com>

--- plugin/src/test/java/org/opensearch/ml/action/model_group/GetModelGroupTransportActionTests.java
+++ plugin/src/test/java/org/opensearch/ml/action/model_group/GetModelGroupTransportActionTests.java
@@ -10,6 +10,9 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.opensearch.index.seqno.SequenceNumbers.UNASSIGNED_PRIMARY_TERM;
+import static org.opensearch.index.seqno.SequenceNumbers.UNASSIGNED_SEQ_NO;
+import static org.opensearch.ml.common.CommonValue.ML_MODEL_GROUP_INDEX;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -167,10 +170,21 @@ public class GetModelGroupTransportActionTests extends OpenSearchTestCase {
         assertEquals("Failed to validate access", argumentCaptor.getValue().getMessage());
     }
 
-    public void testGetModel_NullResponse() {
+    public void testGetModel_NotExistsResponse() {
+        GetResult getResult = new GetResult(
+            ML_MODEL_GROUP_INDEX,
+            "fake_id",
+            UNASSIGNED_SEQ_NO,
+            UNASSIGNED_PRIMARY_TERM,
+            -1L,
+            false,
+            null,
+            null,
+            null
+        );
         doAnswer(invocation -> {
             ActionListener<GetResponse> listener = invocation.getArgument(1);
-            listener.onResponse(null);
+            listener.onResponse(new GetResponse(getResult));
             return null;
         }).when(client).get(any(), any());
         getModelGroupTransportAction.doExecute(null, mlModelGroupGetRequest, actionListener);
