907cff5e868d247c6379f2368c1585bbcf1701e2
Pavan Yekbote <pybot@amazon.com>
2025-04-25 08:55:00 -0700

    [BUG] Agent framework: Fix SearchIndexTool to parse special floating point values and NaN (#3754)
    * fix: support null/NaN values returned from document search
    
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>
    
    * spotless
    
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>
    
    * chore: add document for NaN test case
    
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>
    
    * fix: use static variable for gson instance
    
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>
    
    ---------
    
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>
    (cherry picked from commit 9795399515019dd95536aaf344523d22ff596c39)

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/SearchIndexTool.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/SearchIndexTool.java
@@ -27,11 +27,12 @@ import org.opensearch.ml.common.spi.tools.ToolAnnotation;
 import org.opensearch.ml.common.transport.connector.MLConnectorSearchAction;
 import org.opensearch.ml.common.transport.model.MLModelSearchAction;
 import org.opensearch.ml.common.transport.model_group.MLModelGroupSearchAction;
-import org.opensearch.ml.common.utils.StringUtils;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.builder.SearchSourceBuilder;
 import org.opensearch.transport.client.Client;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
@@ -61,6 +62,8 @@ public class SearchIndexTool implements Tool {
         + " Valid value:\\n{\\\"query\\\":{\\\"match\\\":{\\\"population_description\\\":\\\"seattle 2023 population\\\"}},\\\"size\\\":2,\\\"_source\\\":\\\"population_description\\\"}\\n"
         + "Invalid value: \\n{\\\"match\\\":{\\\"population_description\\\":\\\"seattle 2023 population\\\"}}\\nThe value is invalid because the match not wrapped by \\\"query\\\".\","
         + "\"additionalProperties\":false}},\"required\":[\"index\",\"query\"],\"additionalProperties\":false}";
+
+    private static final Gson GSON = new GsonBuilder().serializeSpecialFloatingPointValues().create();
 
     private String name = TYPE;
     private Map<String, Object> attributes;
@@ -114,7 +117,7 @@ public class SearchIndexTool implements Tool {
     public <T> void run(Map<String, String> parameters, ActionListener<T> listener) {
         try {
             String input = parameters.get(INPUT_FIELD);
-            JsonObject jsonObject = StringUtils.gson.fromJson(input, JsonObject.class);
+            JsonObject jsonObject = GSON.fromJson(input, JsonObject.class);
             String index = Optional.ofNullable(jsonObject).map(x -> x.get(INDEX_FIELD)).map(JsonElement::getAsString).orElse(null);
             String query = Optional.ofNullable(jsonObject).map(x -> x.get(QUERY_FIELD)).map(JsonElement::toString).orElse(null);
             if (index == null || query == null) {
@@ -131,7 +134,7 @@ public class SearchIndexTool implements Tool {
                     for (SearchHit hit : hits) {
                         String doc = AccessController.doPrivileged((PrivilegedExceptionAction<String>) () -> {
                             Map<String, Object> docContent = processResponse(hit);
-                            return StringUtils.gson.toJson(docContent);
+                            return GSON.toJson(docContent);
                         });
                         contextBuilder.append(doc).append("\n");
                     }
