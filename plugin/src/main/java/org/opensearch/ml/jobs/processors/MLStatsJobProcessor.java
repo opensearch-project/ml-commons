8d746f7439847c38d8ca4709e9721ea6b3fd7706
Pavan Yekbote <pybot@amazon.com>
2025-05-19 13:19:12 -0700

    fix: fetch connector from connector_id
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/jobs/processors/MLStatsJobProcessor.java
+++ plugin/src/main/java/org/opensearch/ml/jobs/processors/MLStatsJobProcessor.java
@@ -1,5 +1,6 @@
 package org.opensearch.ml.jobs.processors;
 
+import static org.opensearch.ml.common.CommonValue.ML_CONNECTOR_INDEX;
 import static org.opensearch.ml.common.CommonValue.ML_MODEL_INDEX;
 
 import org.apache.logging.log4j.LogManager;
@@ -7,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
 import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.common.xcontent.LoggingDeprecationHandler;
 import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.core.action.ActionListener;
@@ -15,8 +17,11 @@ import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.index.query.BoolQueryBuilder;
 import org.opensearch.index.query.QueryBuilders;
 import org.opensearch.ml.common.MLModel;
+import org.opensearch.ml.helper.ConnectorAccessControlHelper;
 import org.opensearch.ml.stats.otel.counters.MLAdoptionMetricsCounter;
 import org.opensearch.ml.stats.otel.metrics.AdoptionMetric;
+import org.opensearch.remote.metadata.client.GetDataObjectRequest;
+import org.opensearch.remote.metadata.client.SdkClient;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.builder.SearchSourceBuilder;
 import org.opensearch.threadpool.ThreadPool;
@@ -27,8 +32,16 @@ public class MLStatsJobProcessor extends MLJobProcessor {
     private static final Logger log = LogManager.getLogger(MLStatsJobProcessor.class);
 
     private static MLStatsJobProcessor instance;
+    private final ConnectorAccessControlHelper connectorAccessControlHelper;
+    private final SdkClient sdkClient;
 
-    public static MLStatsJobProcessor getInstance(ClusterService clusterService, Client client, ThreadPool threadPool) {
+    public static MLStatsJobProcessor getInstance(
+        ClusterService clusterService,
+        Client client,
+        ThreadPool threadPool,
+        ConnectorAccessControlHelper connectorAccessControlHelper,
+        SdkClient sdkClient
+    ) {
         if (instance != null) {
             return instance;
         }
@@ -38,13 +51,21 @@ public class MLStatsJobProcessor extends MLJobProcessor {
                 return instance;
             }
 
-            instance = new MLStatsJobProcessor(clusterService, client, threadPool);
+            instance = new MLStatsJobProcessor(clusterService, client, threadPool, connectorAccessControlHelper, sdkClient);
             return instance;
         }
     }
 
-    public MLStatsJobProcessor(ClusterService clusterService, Client client, ThreadPool threadPool) {
+    public MLStatsJobProcessor(
+        ClusterService clusterService,
+        Client client,
+        ThreadPool threadPool,
+        ConnectorAccessControlHelper connectorAccessControlHelper,
+        SdkClient sdkClient
+    ) {
         super(clusterService, client, threadPool);
+        this.connectorAccessControlHelper = connectorAccessControlHelper;
+        this.sdkClient = sdkClient;
     }
 
     @Override
@@ -73,8 +94,38 @@ public class MLStatsJobProcessor extends MLJobProcessor {
                             .xContent()
                             .createParser(NamedXContentRegistry.EMPTY, LoggingDeprecationHandler.INSTANCE, hit.getSourceAsString());
                         parser.nextToken();
+
                         String algorithmName = hit.getSourceAsMap().get(MLModel.ALGORITHM_FIELD).toString();
                         MLModel model = MLModel.parse(parser, algorithmName);
+
+                        if (model.getConnector() == null && model.getConnectorId() != null) {
+                            try (ThreadContext.StoredContext context = client.threadPool().getThreadContext().stashContext()) {
+                                GetDataObjectRequest getDataObjectRequest = GetDataObjectRequest
+                                    .builder()
+                                    .index(ML_CONNECTOR_INDEX)
+                                    .id(model.getConnectorId())
+                                    .build();
+
+                                connectorAccessControlHelper
+                                    .getConnector(
+                                        sdkClient,
+                                        client,
+                                        context,
+                                        getDataObjectRequest,
+                                        model.getConnectorId(),
+                                        ActionListener
+                                            .wrap(
+                                                connector -> MLAdoptionMetricsCounter
+                                                    .getInstance()
+                                                    .incrementCounter(AdoptionMetric.MODEL_COUNT, model.getTags(connector)),
+                                                e -> log.error("Failed to get connector for model: {}", model.getModelId(), e)
+                                            )
+                                    );
+                            }
+
+                            return;
+                        }
+
                         MLAdoptionMetricsCounter.getInstance().incrementCounter(AdoptionMetric.MODEL_COUNT, model.getTags());
                     } catch (Exception e) {
                         log.error("Failed to parse model from hit: {}", hit.getId(), e);
