322140ee901b8e3a0bca8cde79995849f4f09e5f
seungwon cho <rycho@amazon.com>
2025-05-29 10:44:36 -0700

    modify & add new files to support SEARCH Prompt API
    Signed-off-by: seungwon cho <rycho@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
+++ plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
@@ -103,6 +103,7 @@ import org.opensearch.ml.action.prediction.TransportPredictionTaskAction;
 import org.opensearch.ml.action.profile.MLProfileAction;
 import org.opensearch.ml.action.profile.MLProfileTransportAction;
 import org.opensearch.ml.action.prompt.GetPromptTransportAction;
+import org.opensearch.ml.action.prompt.SearchPromptTransportAction;
 import org.opensearch.ml.action.prompt.TransportCreatePromptAction;
 import org.opensearch.ml.action.register.TransportRegisterModelAction;
 import org.opensearch.ml.action.stats.MLStatsNodesAction;
@@ -183,6 +184,7 @@ import org.opensearch.ml.common.transport.model_group.MLUpdateModelGroupAction;
 import org.opensearch.ml.common.transport.prediction.MLPredictionTaskAction;
 import org.opensearch.ml.common.transport.prompt.MLCreatePromptAction;
 import org.opensearch.ml.common.transport.prompt.MLPromptGetAction;
+import org.opensearch.ml.common.transport.prompt.MLPromptSearchAction;
 import org.opensearch.ml.common.transport.register.MLRegisterModelAction;
 import org.opensearch.ml.common.transport.sync.MLSyncUpAction;
 import org.opensearch.ml.common.transport.task.MLCancelBatchJobAction;
@@ -294,6 +296,7 @@ import org.opensearch.ml.rest.RestMLSearchAgentAction;
 import org.opensearch.ml.rest.RestMLSearchConnectorAction;
 import org.opensearch.ml.rest.RestMLSearchModelAction;
 import org.opensearch.ml.rest.RestMLSearchModelGroupAction;
+import org.opensearch.ml.rest.RestMLSearchPromptAction;
 import org.opensearch.ml.rest.RestMLSearchTaskAction;
 import org.opensearch.ml.rest.RestMLStatsAction;
 import org.opensearch.ml.rest.RestMLTrainAndPredictAction;
@@ -478,6 +481,7 @@ public class MachineLearningPlugin extends Plugin
                 new ActionHandler<>(MLConnectorSearchAction.INSTANCE, SearchConnectorTransportAction.class),
                 new ActionHandler<>(MLCreatePromptAction.INSTANCE, TransportCreatePromptAction.class),
                 new ActionHandler<>(MLPromptGetAction.INSTANCE, GetPromptTransportAction.class),
+                new ActionHandler<>(MLPromptSearchAction.INSTANCE, SearchPromptTransportAction.class),
                 new ActionHandler<>(CreateConversationAction.INSTANCE, CreateConversationTransportAction.class),
                 new ActionHandler<>(GetConversationsAction.INSTANCE, GetConversationsTransportAction.class),
                 new ActionHandler<>(CreateInteractionAction.INSTANCE, CreateInteractionTransportAction.class),
@@ -856,6 +860,7 @@ public class MachineLearningPlugin extends Plugin
         RestMLSearchConnectorAction restMLSearchConnectorAction = new RestMLSearchConnectorAction(mlFeatureEnabledSetting);
         RestMLCreatePromptAction restMLCreatePromptAction = new RestMLCreatePromptAction(mlFeatureEnabledSetting);
         RestMLGetPromptAction restMLGetPromptAction = new RestMLGetPromptAction(mlFeatureEnabledSetting);
+        RestMLSearchPromptAction restMLSearchPromptAction = new RestMLSearchPromptAction(mlFeatureEnabledSetting);
         RestMemoryCreateConversationAction restCreateConversationAction = new RestMemoryCreateConversationAction();
         RestMemoryGetConversationsAction restListConversationsAction = new RestMemoryGetConversationsAction();
         RestMemoryCreateInteractionAction restCreateInteractionAction = new RestMemoryCreateInteractionAction();
@@ -919,6 +924,7 @@ public class MachineLearningPlugin extends Plugin
                 restMLSearchConnectorAction,
                 restMLCreatePromptAction,
                 restMLGetPromptAction,
+                restMLSearchPromptAction,
                 restCreateConversationAction,
                 restListConversationsAction,
                 restCreateInteractionAction,
---
+++ plugin/src/main/java/org/opensearch/ml/rest/RestMLSearchPromptAction.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.rest;
+
+import static org.opensearch.ml.common.CommonValue.ML_PROMPT_INDEX;
+import static org.opensearch.ml.plugin.MachineLearningPlugin.ML_BASE_URI;
+
+import org.opensearch.ml.common.prompt.MLPrompt;
+import org.opensearch.ml.common.settings.MLFeatureEnabledSetting;
+import org.opensearch.ml.common.transport.prompt.MLPromptSearchAction;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Rest Action class that handles SEARCH REST API request
+ */
+public class RestMLSearchPromptAction extends AbstractMLSearchAction<MLPrompt> {
+    private static final String ML_SEARCH_PROMPT_ACTION = "ml_search_prompt_action";
+    private static final String SEARCH_PROMPT_PATH = ML_BASE_URI + "/prompts/_search";
+
+    public RestMLSearchPromptAction(MLFeatureEnabledSetting mlFeatureEnabledSetting) {
+        super(
+            ImmutableList.of(SEARCH_PROMPT_PATH),
+            ML_PROMPT_INDEX,
+            MLPrompt.class,
+            MLPromptSearchAction.INSTANCE,
+            mlFeatureEnabledSetting
+        );
+    }
+
+    @Override
+    public String getName() {
+        return ML_SEARCH_PROMPT_ACTION;
+    }
+}
