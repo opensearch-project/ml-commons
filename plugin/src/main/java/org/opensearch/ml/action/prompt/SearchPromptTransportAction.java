6eefbb0562502e8c2df46201cd6cceb6f58e9664
seungwon cho <rycho@amazon.com>
2025-06-19 14:50:12 -0700

    remove credentials from response body on search
    Signed-off-by: seungwon cho <rycho@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/action/prompt/SearchPromptTransportAction.java
+++ plugin/src/main/java/org/opensearch/ml/action/prompt/SearchPromptTransportAction.java
@@ -7,12 +7,20 @@ package org.opensearch.ml.action.prompt;
 
 import static org.opensearch.ml.utils.RestActionUtils.wrapListenerToHandleSearchIndexNotFound;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.opensearch.action.search.SearchResponse;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.action.support.HandledTransportAction;
 import org.opensearch.common.inject.Inject;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.action.ActionListener;
+import org.opensearch.ml.common.prompt.MLPrompt;
+import org.opensearch.ml.common.prompt.PromptExtraConfig;
 import org.opensearch.ml.common.settings.MLFeatureEnabledSetting;
 import org.opensearch.ml.common.transport.prompt.MLPromptSearchAction;
 import org.opensearch.ml.common.transport.search.MLSearchActionRequest;
@@ -20,6 +28,8 @@ import org.opensearch.ml.utils.TenantAwareHelper;
 import org.opensearch.remote.metadata.client.SdkClient;
 import org.opensearch.remote.metadata.client.SearchDataObjectRequest;
 import org.opensearch.remote.metadata.common.SdkClientUtils;
+import org.opensearch.search.builder.SearchSourceBuilder;
+import org.opensearch.search.fetch.subphase.FetchSourceContext;
 import org.opensearch.tasks.Task;
 import org.opensearch.transport.TransportService;
 import org.opensearch.transport.client.Client;
@@ -68,9 +78,34 @@ public class SearchPromptTransportAction extends HandledTransportAction<MLSearch
         }
 
         try (ThreadContext.StoredContext context = client.threadPool().getThreadContext().stashContext()) {
-            final ActionListener<SearchResponse> wrappedListener = ActionListener
+            ActionListener<SearchResponse> wrappedListener = ActionListener.runBefore(actionListener, context::restore);
+
+            List<String> excludes = Optional
+                .ofNullable(mlSearchActionRequest.source())
+                .map(SearchSourceBuilder::fetchSource)
+                .map(FetchSourceContext::excludes)
+                .map(x -> Arrays.stream(x).collect(Collectors.toList()))
+                .orElse(new ArrayList<>());
+            excludes.add(MLPrompt.PROMPT_EXTRA_CONFIG_FIELD + "." + PromptExtraConfig.LANGFUSE_PROMPT_ACCESS_KEY_FIELD);
+            excludes.add(MLPrompt.PROMPT_EXTRA_CONFIG_FIELD + "." + PromptExtraConfig.LANGFUSE_PROMPT_PUBLIC_KEY_FIELD);
+            FetchSourceContext rebuiltFetchSourceContext = new FetchSourceContext(
+                Optional
+                    .ofNullable(mlSearchActionRequest.source())
+                    .map(SearchSourceBuilder::fetchSource)
+                    .map(FetchSourceContext::fetchSource)
+                    .orElse(true),
+                Optional
+                    .ofNullable(mlSearchActionRequest.source())
+                    .map(SearchSourceBuilder::fetchSource)
+                    .map(FetchSourceContext::includes)
+                    .orElse(null),
+                excludes.toArray(new String[0])
+            );
+            mlSearchActionRequest.source().fetchSource(rebuiltFetchSourceContext);
+
+            final ActionListener<SearchResponse> doubleWrappedListener = ActionListener
                 .runBefore(
-                    ActionListener.wrap(actionListener::onResponse, e -> wrapListenerToHandleSearchIndexNotFound(e, actionListener)),
+                    ActionListener.wrap(wrappedListener::onResponse, e -> wrapListenerToHandleSearchIndexNotFound(e, wrappedListener)),
                     context::restore
                 );
 
@@ -81,7 +116,9 @@ public class SearchPromptTransportAction extends HandledTransportAction<MLSearch
                 .tenantId(tenantId)
                 .build();
 
-            sdkClient.searchDataObjectAsync(searchDataObjectRequest).whenComplete(SdkClientUtils.wrapSearchCompletion(wrappedListener));
+            sdkClient
+                .searchDataObjectAsync(searchDataObjectRequest)
+                .whenComplete(SdkClientUtils.wrapSearchCompletion(doubleWrappedListener));
         } catch (Exception e) {
             log.error("Failed to search ML Prompt", e);
             actionListener.onFailure(e);
