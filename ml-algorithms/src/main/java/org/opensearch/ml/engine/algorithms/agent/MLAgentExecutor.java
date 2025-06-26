b08bb3a65f0f65720ef37d7a4cb8512ef6de238f
chrislai <chrlaii@amazon.com>
2025-06-26 10:50:30 -0700

    adding agent tracing to mlplugin
    Signed-off-by: chrislai <chrlaii@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/MLAgentExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/MLAgentExecutor.java
@@ -64,6 +64,7 @@ import org.opensearch.ml.common.settings.SettingsChangeListener;
 import org.opensearch.ml.common.spi.memory.Memory;
 import org.opensearch.ml.common.spi.tools.Tool;
 import org.opensearch.ml.engine.Executable;
+import org.opensearch.ml.engine.algorithms.agent.tracing.MLAgentTracer;
 import org.opensearch.ml.engine.annotation.Function;
 import org.opensearch.ml.engine.encryptor.Encryptor;
 import org.opensearch.ml.engine.memory.ConversationIndexMemory;
@@ -110,6 +111,7 @@ public class MLAgentExecutor implements Executable, SettingsChangeListener {
     private volatile Boolean isMultiTenancyEnabled;
     private Encryptor encryptor;
     private static volatile boolean mcpConnectorIsEnabled;
+    private MLAgentTracer agentTracer;
 
     public MLAgentExecutor(
         Client client,
@@ -605,5 +607,9 @@ public class MLAgentExecutor implements Executable, SettingsChangeListener {
             log.error("Failed to create ML task for {}, {}", mlTask.getFunctionName(), mlTask.getTaskType(), e);
             listener.onFailure(e);
         }
+    }
+
+    public void setAgentTracer(MLAgentTracer agentTracer) {
+        this.agentTracer = agentTracer;
     }
 }
