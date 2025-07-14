2515b922e460caf2aa1000480032bc46a1580242
chrislai <chrlaii@amazon.com>
2025-07-14 13:47:37 -0700

    fixes
    Signed-off-by: chrislai <chrlaii@amazon.com>

--- common/src/test/java/org/opensearch/ml/common/settings/MLFeatureEnabledSettingTests.java
+++ common/src/test/java/org/opensearch/ml/common/settings/MLFeatureEnabledSettingTests.java
@@ -3,45 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.opensearch.ml.settings;
+package org.opensearch.ml.common.settings;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_AGENT_FRAMEWORK_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_CONNECTOR_PRIVATE_IP_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_CONTROLLER_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_LOCAL_MODEL_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_MCP_SERVER_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_METRIC_COLLECTION_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_MULTI_TENANCY_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_OFFLINE_BATCH_INFERENCE_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_OFFLINE_BATCH_INGESTION_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_RAG_PIPELINE_FEATURE_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_REMOTE_INFERENCE_ENABLED;
-import static org.opensearch.ml.common.settings.MLCommonsSettings.ML_COMMONS_STATIC_METRIC_COLLECTION_ENABLED;
 
 import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.ClusterSettings;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.ml.common.settings.MLFeatureEnabledSetting;
-import org.opensearch.ml.common.settings.SettingsChangeListener;
 
 public class MLFeatureEnabledSettingTests {
-    @Mock
-    private ClusterService clusterService;
-    private Settings settings;
-    private MLFeatureEnabledSetting mlFeatureEnabledSetting;
-    private SettingsChangeListener listener;
+
+    private ClusterService mockClusterService;
+    private ClusterSettings mockClusterSettings;
 
     @Before
     public void setUp() {
@@ -125,13 +106,8 @@ public class MLFeatureEnabledSettingTests {
     }
 
     @Test
-    public void testMetricCollectionSettings() {
-        // Test initial values
-        assertTrue(mlFeatureEnabledSetting.isMetricCollectionEnabled());
-        assertFalse(mlFeatureEnabledSetting.isStaticMetricCollectionEnabled());
-
-        // Simulate settings change
-        Settings newSettings = Settings
+    public void testDefaults_someFeaturesDisabled() {
+        Settings settings = Settings
             .builder()
             .put("plugins.ml_commons.remote_inference.enabled", false)
             .put("plugins.ml_commons.agent_framework_enabled", false)
