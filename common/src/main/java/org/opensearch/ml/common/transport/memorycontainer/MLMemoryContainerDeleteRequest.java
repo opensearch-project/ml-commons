513834f60286cb6b9d76633afb9fdf58b1f78d47
Nathalie Jonathan <nathhjo@amazon.com>
2025-07-31 15:37:02 -0700

    Add delete memory container API
    Signed-off-by: Nathalie Jonathan <nathhjo@amazon.com>

---
+++ common/src/main/java/org/opensearch/ml/common/transport/memorycontainer/MLMemoryContainerDeleteRequest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.ml.common.transport.memorycontainer;
+
+import static org.opensearch.action.ValidateActions.addValidationError;
+import static org.opensearch.ml.common.CommonValue.VERSION_2_19_0;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import org.opensearch.Version;
+import org.opensearch.action.ActionRequest;
+import org.opensearch.action.ActionRequestValidationException;
+import org.opensearch.core.common.io.stream.InputStreamStreamInput;
+import org.opensearch.core.common.io.stream.OutputStreamStreamOutput;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class MLMemoryContainerDeleteRequest extends ActionRequest {
+    @Getter
+    String containerId;
+
+    @Getter
+    String tenantId;
+
+    @Builder
+    public MLMemoryContainerDeleteRequest(String containerId, String tenantId) {
+        this.containerId = containerId;
+        this.tenantId = tenantId;
+    }
+
+    public MLMemoryContainerDeleteRequest(StreamInput input) throws IOException {
+        super(input);
+        Version streamInputVersion = input.getVersion();
+        this.containerId = input.readString();
+        this.tenantId = streamInputVersion.onOrAfter(VERSION_2_19_0) ? input.readOptionalString() : null;
+    }
+
+    @Override
+    public void writeTo(StreamOutput output) throws IOException {
+        super.writeTo(output);
+        Version streamOutputVersion = output.getVersion();
+        output.writeString(containerId);
+        if (streamOutputVersion.onOrAfter(VERSION_2_19_0)) {
+            output.writeOptionalString(tenantId);
+        }
+    }
+
+    @Override
+    public ActionRequestValidationException validate() {
+        ActionRequestValidationException exception = null;
+
+        if (this.containerId == null) {
+            exception = addValidationError("ML memory container id can't be null", exception);
+        }
+
+        return exception;
+    }
+
+    public static MLMemoryContainerDeleteRequest fromActionRequest(ActionRequest actionRequest) {
+        if (actionRequest instanceof MLMemoryContainerDeleteRequest) {
+            return (MLMemoryContainerDeleteRequest) actionRequest;
+        }
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(); OutputStreamStreamOutput osso = new OutputStreamStreamOutput(baos)) {
+            actionRequest.writeTo(osso);
+            try (StreamInput input = new InputStreamStreamInput(new ByteArrayInputStream(baos.toByteArray()))) {
+                return new MLMemoryContainerDeleteRequest(input);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException("failed to parse ActionRequest into MLMemoryContainerDeleteRequest", e);
+        }
+    }
+}
