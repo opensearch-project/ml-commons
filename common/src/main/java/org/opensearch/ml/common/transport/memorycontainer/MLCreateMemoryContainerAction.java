39bae673333656442fa47e873aeaa117ff439aa5
Sicheng Song <sicheng.song@outlook.com>
2025-08-04 06:09:33 +0000

    Readability enhancement on memory container api
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

--- common/src/main/java/org/opensearch/ml/common/transport/memorycontainer/MLCreateMemoryContainerAction.java
+++ common/src/main/java/org/opensearch/ml/common/transport/memorycontainer/MLCreateMemoryContainerAction.java
@@ -9,7 +9,7 @@ import org.opensearch.action.ActionType;
 
 public class MLCreateMemoryContainerAction extends ActionType<MLCreateMemoryContainerResponse> {
     public static final MLCreateMemoryContainerAction INSTANCE = new MLCreateMemoryContainerAction();
-    public static final String NAME = "cluster:admin/opensearch/ml/memory_container/create";
+    public static final String NAME = "cluster:admin/opensearch/ml/memory_containers/create";
 
     private MLCreateMemoryContainerAction() {
         super(NAME, MLCreateMemoryContainerResponse::new);
