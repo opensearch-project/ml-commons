65881b83607554a07f2172cee48e65e20d7759ee
Sicheng Song <sicheng.song@outlook.com>
2025-08-04 06:09:33 +0000

    Readability enhancement on memory container api
    Signed-off-by: Sicheng Song <sicheng.song@outlook.com>

--- common/src/main/java/org/opensearch/ml/common/transport/memorycontainer/MLMemoryContainerGetAction.java
+++ common/src/main/java/org/opensearch/ml/common/transport/memorycontainer/MLMemoryContainerGetAction.java
@@ -9,7 +9,7 @@ import org.opensearch.action.ActionType;
 
 public class MLMemoryContainerGetAction extends ActionType<MLMemoryContainerGetResponse> {
     public static final MLMemoryContainerGetAction INSTANCE = new MLMemoryContainerGetAction();
-    public static final String NAME = "cluster:admin/opensearch/ml/memory_container/get";
+    public static final String NAME = "cluster:admin/opensearch/ml/memory_containers/get";
 
     private MLMemoryContainerGetAction() {
         super(NAME, MLMemoryContainerGetResponse::new);
