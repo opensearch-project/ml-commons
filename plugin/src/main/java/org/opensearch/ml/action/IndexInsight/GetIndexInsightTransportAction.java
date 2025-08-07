b56e7adfdf258854e563034cf9bb9cd72c3a3485
shuangli-z <shualee@amazon.com>
2025-08-07 18:05:07 +0800

    Remove redundant task status checks in GetIndexInsightTransportAction
    Signed-off-by: shuangli-z <shualee@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/action/IndexInsight/GetIndexInsightTransportAction.java
+++ plugin/src/main/java/org/opensearch/ml/action/IndexInsight/GetIndexInsightTransportAction.java
@@ -1,29 +1,15 @@
 package org.opensearch.ml.action.IndexInsight;
 
-import static org.opensearch.core.xcontent.XContentParserUtils.ensureExpectedToken;
-import static org.opensearch.ml.common.CommonValue.ML_INDEX_INSIGHT_INDEX;
-import static org.opensearch.ml.common.indexInsight.IndexInsight.INDEX_NAME_FIELD;
-import static org.opensearch.ml.common.indexInsight.IndexInsight.TASK_TYPE_FIELD;
-import static org.opensearch.ml.utils.MLNodeUtils.createXContentParserFromRegistry;
-
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.opensearch.OpenSearchStatusException;
 import org.opensearch.action.ActionRequest;
-import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.action.support.HandledTransportAction;
 import org.opensearch.common.inject.Inject;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.action.ActionListener;
-import org.opensearch.core.rest.RestStatus;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
-import org.opensearch.core.xcontent.XContentParser;
-import org.opensearch.index.query.BoolQueryBuilder;
-import org.opensearch.index.query.QueryBuilders;
-import org.opensearch.index.query.TermQueryBuilder;
 import org.opensearch.ml.common.indexInsight.IndexInsight;
 import org.opensearch.ml.common.indexInsight.IndexInsightAccessControllerHelper;
 import org.opensearch.ml.common.indexInsight.IndexInsightTask;
@@ -38,8 +24,7 @@ import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.ml.common.transport.indexInsight.MLIndexInsightGetAction;
 import org.opensearch.ml.common.transport.indexInsight.MLIndexInsightGetRequest;
 import org.opensearch.ml.common.transport.indexInsight.MLIndexInsightGetResponse;
-import org.opensearch.search.SearchHit;
-import org.opensearch.search.builder.SearchSourceBuilder;
+
 import org.opensearch.tasks.Task;
 import org.opensearch.transport.TransportService;
 import org.opensearch.transport.client.Client;
@@ -83,39 +68,14 @@ public class GetIndexInsightTransportAction extends HandledTransportAction<Actio
             }
             
             ActionListener<Boolean> actionAfterDryRun = ActionListener.wrap(r -> {
-            SearchRequest searchRequest = new SearchRequest(ML_INDEX_INSIGHT_INDEX);
-            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
-            boolQueryBuilder.must(new TermQueryBuilder(INDEX_NAME_FIELD, indexName));
-            boolQueryBuilder.must(new TermQueryBuilder(TASK_TYPE_FIELD, mlIndexInsightGetRequest.getTargetIndexInsight().toString()));
-            searchSourceBuilder.query(boolQueryBuilder);
-            searchRequest.source(searchSourceBuilder);
-
-            try (ThreadContext.StoredContext context = client.threadPool().getThreadContext().stashContext()) {
-                ActionListener<MLIndexInsightGetResponse> wrappedListener = ActionListener
-                    .runBefore(actionListener, () -> context.restore());
-                client.search(searchRequest, ActionListener.wrap(searchResponse -> {
-                    SearchHit[] hits = searchResponse.getHits().getHits();
-                    
-                    if (hits.length == 0) {
-                        // No record found - execute task
-                        executeTaskAndReturn(mlIndexInsightGetRequest, wrappedListener);
-                        return;
-                    }
-                    
-                    try (XContentParser parser = createXContentParserFromRegistry(xContentRegistry, hits[0].getSourceRef())) {
-                        ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
-                        IndexInsight existingInsight = IndexInsight.parse(parser);
-                        handleExistingRecord(existingInsight, mlIndexInsightGetRequest, wrappedListener);
-                    } catch (Exception e) {
-                        wrappedListener.onFailure(e);
-                    }
-                }, wrappedListener::onFailure));
-
-            } catch (Exception e) {
-                log.error("fail to get index insight", e);
-                actionListener.onFailure(e);
-            }
+                try (ThreadContext.StoredContext context = client.threadPool().getThreadContext().stashContext()) {
+                    ActionListener<MLIndexInsightGetResponse> wrappedListener = ActionListener
+                        .runBefore(actionListener, () -> context.restore());
+                    executeTaskAndReturn(mlIndexInsightGetRequest, wrappedListener);
+                } catch (Exception e) {
+                    log.error("fail to get index insight", e);
+                    actionListener.onFailure(e);
+                }
             }, actionListener::onFailure);
             IndexInsightAccessControllerHelper.verifyAccessController(client, actionAfterDryRun, indexName);
         }, actionListener::onFailure));
@@ -132,44 +92,7 @@ public class GetIndexInsightTransportAction extends HandledTransportAction<Actio
         ));
     }
     
-    private void handleExistingRecord(IndexInsight existingInsight, MLIndexInsightGetRequest request, 
-                                     ActionListener<MLIndexInsightGetResponse> listener) {
-        IndexInsightTaskStatus status = existingInsight.getStatus();
-        long lastUpdateTime = existingInsight.getLastUpdatedTime().toEpochMilli();
-        long currentTime = Instant.now().toEpochMilli();
-        String indexName = request.getIndexName();
-        
-        switch (status) {
-            case COMPLETED:
-                if ((currentTime - lastUpdateTime) <= IndexInsightTask.UPDATE_INTERVAL) {
-                    // Not expired - return immediately
-                    listener.onResponse(MLIndexInsightGetResponse.builder()
-                        .indexInsight(existingInsight).build());
-                    return;
-                }
-                // Expired - re-execute task
-                executeTaskAndReturn(request, listener);
-                break;
-                
-            case GENERATING:
-                if ((currentTime - lastUpdateTime) <= IndexInsightTask.GENERATING_TIMEOUT) {
-                    // Not timeout - return generating message
-                    log.info("Index insight for index {} with task type {} is being generated, please wait...", 
-                            indexName, request.getTargetIndexInsight());
-                    listener.onFailure(new OpenSearchStatusException(
-                        "Index insight is being generated, please wait...", RestStatus.ACCEPTED));
-                    return;
-                }
-                // Timeout - re-execute task
-                executeTaskAndReturn(request, listener);
-                break;
-                
-            case FAILED:
-                // Failed - retry task
-                executeTaskAndReturn(request, listener);
-                break;
-        }
-    }
+
     
     private IndexInsightTask createTask(MLIndexInsightGetRequest request) {
         switch (request.getTargetIndexInsight()) {
