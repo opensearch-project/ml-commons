42cbebdf14d04357a112c9b3a9ee212426465ce3
Christopher Lai <43394684+chriswlai@users.noreply.github.com>
2025-08-08 10:54:06 -0700

    Conversational Flow and Flow Agent Tracing #3969 (#3976)
    * adding agent tracing to mlplugin
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * add tests
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * cr fixes
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * spotlessApply
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * add tests
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * P-E-R tracing
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * add input/output to all spans
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * conversational agent tracing
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * allow flexability in tool return format
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * directly access singleton + good practice coding
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * convflow and flow agent tracing
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * new settings and noop update
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * overloading method
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * propagate tokens
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * rebase cleaning
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * clean
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * clean
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * javadoc
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * rebase to main
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * refactoring code
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * refactoring
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * address comments
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * fixed numbering
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    * address comments
    
    Signed-off-by: chrislai <chrlaii@amazon.com>
    
    ---------
    
    Signed-off-by: chrislai <chrlaii@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/MLChatAgentRunner.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/MLChatAgentRunner.java
@@ -352,8 +352,8 @@ public class MLChatAgentRunner implements MLAgentRunner {
             .startConversationalLLMCallSpan(
                 question,
                 context.getLlmCallIndex().get(),
-                tmpParameters.get("system_prompt"),
-                tmpParameters.get("_llm_interface"),
+                tmpParameters.get(MLAgentTracer.PARAM_SYSTEM_PROMPT),
+                tmpParameters.get(LLM_INTERFACE),
                 agentTaskSpan
             );
         StepListener firstListener = new StepListener<MLTaskResponse>();
@@ -584,8 +584,8 @@ public class MLChatAgentRunner implements MLAgentRunner {
                         .startConversationalLLMCallSpan(
                             question,
                             currentLlmCallIndex,
-                            tmpParameters.get("system_prompt"),
-                            tmpParameters.get("_llm_interface"),
+                            tmpParameters.get(MLAgentTracer.PARAM_SYSTEM_PROMPT),
+                            tmpParameters.get(LLM_INTERFACE),
                             agentTaskSpan
                         );
 
