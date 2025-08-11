3df7f36846cf147fee8691ebf17486309195842f
shuangli-z <shualee@amazon.com>
2025-08-11 15:26:52 +0800

    add license
    Signed-off-by: shuangli-z <shualee@amazon.com>

--- common/src/main/java/org/opensearch/ml/common/indexInsight/IndexInsightContainer.java
+++ common/src/main/java/org/opensearch/ml/common/indexInsight/IndexInsightContainer.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.ml.common.indexInsight;
 
 import static org.opensearch.core.xcontent.XContentParserUtils.ensureExpectedToken;
--- plugin/src/main/java/org/opensearch/ml/action/IndexInsight/GetIndexInsightTransportAction.java
+++ plugin/src/main/java/org/opensearch/ml/action/IndexInsight/GetIndexInsightTransportAction.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.ml.action.IndexInsight;
 
 import static org.opensearch.common.xcontent.json.JsonXContent.jsonXContent;
