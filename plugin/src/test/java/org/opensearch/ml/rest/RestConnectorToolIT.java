42c89edaf1db78bdbf66755180c79111c14191e8
Xinyuan Lu <xinyual@amazon.com>
2025-09-12 04:59:30 +0800

    Fix claude model it (#4167)
    * fix model it by replace claude v1/v2
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * remove useless change
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    ---------
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    Signed-off-by: Brian Flores <iflorbri@amazon.com>

--- plugin/src/test/java/org/opensearch/ml/rest/RestConnectorToolIT.java
+++ plugin/src/test/java/org/opensearch/ml/rest/RestConnectorToolIT.java
@@ -19,7 +19,9 @@ public class RestConnectorToolIT extends RestBaseAgentToolsIT {
     private static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
     private static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
     private static final String AWS_SESSION_TOKEN = System.getenv("AWS_SESSION_TOKEN");
+
     private static final String GITHUB_CI_AWS_REGION = "us-west-2";
+    private static final String BEDROCK_ANTHROPIC_CLAUDE_3_5_SONNET = "anthropic.claude-3-5-sonnet-20240620-v1:0";
 
     private String bedrockClaudeConnectorId;
     private String bedrockClaudeConnectorIdForPredict;
@@ -35,8 +37,8 @@ public class RestConnectorToolIT extends RestBaseAgentToolsIT {
 
     private String createBedrockClaudeConnector(String action) throws IOException, InterruptedException {
         String bedrockClaudeConnectorEntity = "{\n"
-            + "  \"name\": \"BedRock Claude instant-v1 Connector \",\n"
-            + "  \"description\": \"The connector to BedRock service for claude model\",\n"
+            + "  \"name\": \"Bedrock Connector: claude 3.5\",\n"
+            + "  \"description\": \"The connector to bedrock claude 3.5 model\",\n"
             + "  \"version\": 1,\n"
             + "  \"protocol\": \"aws_sigv4\",\n"
             + "  \"parameters\": {\n"
@@ -44,10 +46,11 @@ public class RestConnectorToolIT extends RestBaseAgentToolsIT {
             + GITHUB_CI_AWS_REGION
             + "\",\n"
             + "    \"service_name\": \"bedrock\",\n"
-            + "    \"anthropic_version\": \"bedrock-2023-05-31\",\n"
-            + "    \"max_tokens_to_sample\": 8000,\n"
-            + "    \"temperature\": 0.0001,\n"
-            + "    \"response_filter\": \"$.completion\"\n"
+            + "    \"model\": \""
+            + BEDROCK_ANTHROPIC_CLAUDE_3_5_SONNET
+            + "\",\n"
+            + "    \"system_prompt\": \"You are a helpful assistant.\",\n"
+            + "\"response_filter\": \"$.output.message.content[0].text\""
             + "  },\n"
             + "  \"credential\": {\n"
             + "    \"access_key\": \""
@@ -61,19 +64,22 @@ public class RestConnectorToolIT extends RestBaseAgentToolsIT {
             + "\"\n"
             + "  },\n"
             + "  \"actions\": [\n"
-            + "    {\n"
-            + "      \"action_type\": \""
+            + "        {\n"
+            + "            \"action_type\": \""
             + action
             + "\",\n"
-            + "      \"method\": \"POST\",\n"
-            + "      \"url\": \"https://bedrock-runtime.${parameters.region}.amazonaws.com/model/anthropic.claude-instant-v1/invoke\",\n"
-            + "      \"headers\": {\n"
-            + "        \"content-type\": \"application/json\",\n"
-            + "        \"x-amz-content-sha256\": \"required\"\n"
-            + "      },\n"
-            + "      \"request_body\": \"{\\\"prompt\\\":\\\"\\\\n\\\\nHuman:${parameters.question}\\\\n\\\\nAssistant:\\\", \\\"max_tokens_to_sample\\\":${parameters.max_tokens_to_sample}, \\\"temperature\\\":${parameters.temperature},  \\\"anthropic_version\\\":\\\"${parameters.anthropic_version}\\\" }\"\n"
-            + "    }\n"
-            + "  ]\n"
+            + "            \"method\": \"POST\",\n"
+            + "            \"headers\": {\n"
+            + "                \"content-type\": \"application/json\"\n"
+            + "            },\n"
+            + "            \"url\": \"https://bedrock-runtime."
+            + GITHUB_CI_AWS_REGION
+            + ".amazonaws.com/model/"
+            + BEDROCK_ANTHROPIC_CLAUDE_3_5_SONNET
+            + "/converse\",\n"
+            + "            \"request_body\": \"{ \\\"system\\\": [{\\\"text\\\": \\\"you are a helpful assistant.\\\"}], \\\"messages\\\":[{\\\"role\\\": \\\"user\\\", \\\"content\\\":[ {\\\"type\\\": \\\"text\\\", \\\"text\\\":\\\"${parameters.messages}\\\"}]}] , \\\"inferenceConfig\\\": {\\\"temperature\\\": 0.0, \\\"topP\\\": 0.9, \\\"maxTokens\\\": 1000} }\"\n"
+            + "        }\n"
+            + "    ]\n"
             + "}";
         return registerConnector(bedrockClaudeConnectorEntity);
     }
@@ -135,7 +141,7 @@ public class RestConnectorToolIT extends RestBaseAgentToolsIT {
             + "  ]\n"
             + "}";
         String agentId = createAgent(registerAgentRequestBody);
-        String agentInput = "{\n" + "  \"parameters\": {\n" + "    \"question\": \"hello\"\n" + "  }\n" + "}";
+        String agentInput = "{\n" + "  \"parameters\": {\n" + "    \"messages\": \"hello\"\n" + "  }\n" + "}";
         String result = executeAgent(agentId, agentInput);
         assertNotNull(result);
     }
