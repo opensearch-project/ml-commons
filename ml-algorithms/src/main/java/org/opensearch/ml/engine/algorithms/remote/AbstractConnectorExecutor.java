dadaafdce264615828800bd28bcb9496647a86ac
Nathalie Jonathan <nathhjo@amazon.com>
2025-09-23 01:04:41 -0700

    Initial commit for predict stream
    Signed-off-by: Nathalie Jonathan <nathhjo@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AbstractConnectorExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AbstractConnectorExecutor.java
@@ -5,8 +5,17 @@
 
 package org.opensearch.ml.engine.algorithms.remote;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.opensearch.ml.common.connector.Connector;
 import org.opensearch.ml.common.connector.ConnectorClientConfig;
+import org.opensearch.ml.common.output.model.ModelTensor;
+import org.opensearch.ml.common.output.model.ModelTensorOutput;
+import org.opensearch.ml.common.output.model.ModelTensors;
+import org.opensearch.ml.common.transport.MLTaskResponse;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +30,25 @@ public abstract class AbstractConnectorExecutor implements RemoteConnectorExecut
             connectorClientConfig = connector.getConnectorClientConfig();
         } else {
             connectorClientConfig = new ConnectorClientConfig();
+        }
+    }
+
+    public void sendContentResponse(String content, boolean isLast, StreamPredictActionListener<MLTaskResponse, ?> actionListener) {
+        List<ModelTensor> modelTensors = new ArrayList<>();
+        Map<String, Object> dataMap = Map.of("content", content, "is_last", isLast);
+
+        modelTensors.add(ModelTensor.builder().name("response").dataAsMap(dataMap).build());
+        ModelTensorOutput output = ModelTensorOutput
+            .builder()
+            .mlModelOutputs(List.of(ModelTensors.builder().mlModelTensors(modelTensors).build()))
+            .build();
+        MLTaskResponse response = MLTaskResponse.builder().output(output).build();
+        actionListener.onStreamResponse(response, isLast);
+    }
+
+    public void sendCompletionResponse(AtomicBoolean isStreamClosed, StreamPredictActionListener<MLTaskResponse, ?> actionListener) {
+        if (isStreamClosed.compareAndSet(false, true)) {
+            sendContentResponse("", true, actionListener);
         }
     }
 }
--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/HttpJsonConnectorExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/HttpJsonConnectorExecutor.java
@@ -6,18 +6,24 @@
 package org.opensearch.ml.engine.algorithms.remote;
 
 import static org.opensearch.ml.common.connector.ConnectorProtocols.HTTP;
+import static org.opensearch.ml.engine.algorithms.agent.AgentUtils.LLM_INTERFACE_OPENAI_V1_CHAT_COMPLETIONS;
+import static org.opensearch.ml.engine.algorithms.agent.MLChatAgentRunner.LLM_INTERFACE;
+import static org.opensearch.ml.engine.function_calling.OpenaiV1ChatCompletionsFunctionCalling.FINISH_REASON_PATH;
 import static software.amazon.awssdk.http.SdkHttpMethod.GET;
 import static software.amazon.awssdk.http.SdkHttpMethod.POST;
 
 import java.net.URL;
 import java.security.AccessController;
+import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.time.Duration;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.common.collect.Tuple;
 import org.opensearch.common.util.TokenBucket;
@@ -28,14 +34,26 @@ import org.opensearch.ml.common.exception.MLException;
 import org.opensearch.ml.common.input.MLInput;
 import org.opensearch.ml.common.model.MLGuard;
 import org.opensearch.ml.common.output.model.ModelTensors;
+import org.opensearch.ml.common.transport.MLTaskResponse;
+import org.opensearch.ml.common.utils.StringUtils;
 import org.opensearch.ml.engine.annotation.ConnectorExecutor;
 import org.opensearch.ml.engine.httpclient.MLHttpClientFactory;
 import org.opensearch.script.ScriptService;
+import org.opensearch.transport.StreamTransportService;
 import org.opensearch.transport.client.Client;
+
+import com.jayway.jsonpath.JsonPath;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.internal.http2.StreamResetException;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import okhttp3.sse.EventSources;
 import software.amazon.awssdk.core.internal.http.async.SimpleHttpContentPublisher;
 import software.amazon.awssdk.http.SdkHttpFullRequest;
 import software.amazon.awssdk.http.async.AsyncExecuteRequest;
@@ -68,6 +86,11 @@ public class HttpJsonConnectorExecutor extends AbstractConnectorExecutor {
 
     private SdkAsyncHttpClient httpClient;
 
+    private OkHttpClient okHttpClient;
+    @Setter
+    @Getter
+    private StreamTransportService streamTransportService;
+
     public HttpJsonConnectorExecutor(Connector connector) {
         super.initialize(connector);
         this.connector = (HttpConnector) connector;
@@ -75,6 +98,18 @@ public class HttpJsonConnectorExecutor extends AbstractConnectorExecutor {
         Duration readTimeout = Duration.ofSeconds(super.getConnectorClientConfig().getReadTimeout());
         Integer maxConnection = super.getConnectorClientConfig().getMaxConnections();
         this.httpClient = MLHttpClientFactory.getAsyncHttpClient(connectionTimeout, readTimeout, maxConnection);
+        try {
+            AccessController.doPrivileged((PrivilegedExceptionAction<Void>) () -> {
+                this.okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(1, TimeUnit.MINUTES)
+                    .retryOnConnectionFailure(true)
+                    .build();
+                return null;
+            });
+        } catch (PrivilegedActionException e) {
+            throw new RuntimeException("Failed to build OkHttpClient.", e);
+        }
     }
 
     @Override
@@ -133,6 +168,36 @@ public class HttpJsonConnectorExecutor extends AbstractConnectorExecutor {
         }
     }
 
+    @Override
+    public void invokeRemoteServiceStream(
+        String action,
+        MLInput mlInput,
+        Map<String, String> parameters,
+        String payload,
+        ExecutionContext executionContext,
+        StreamPredictActionListener<MLTaskResponse, ?> actionListener
+    ) {
+        try {
+            String llmInterface = parameters.get(LLM_INTERFACE);
+            llmInterface = llmInterface.trim().toLowerCase(Locale.ROOT);
+            llmInterface = StringEscapeUtils.unescapeJava(llmInterface);
+            validateLLMInterface(llmInterface);
+
+            log.info("Creating SSE connection for streaming request");
+            EventSourceListener listener = new HTTPEventSourceListener(actionListener, llmInterface);
+            Request request = ConnectorUtils.buildOKHttpRequestPOST(action, connector, parameters, payload);
+
+            AccessController.doPrivileged((PrivilegedExceptionAction<Void>) () -> {
+                EventSources.createFactory(okHttpClient).newEventSource(request, listener);
+                return null;
+            });
+
+        } catch (Exception e) {
+            log.error("Failed to execute streaming", e);
+            actionListener.onFailure(new MLException("Fail to execute streaming", e));
+        }
+    }
+
     private void validateHttpClientParameters(String action, Map<String, String> parameters) throws Exception {
         String endpoint = connector.getActionEndpoint(action, parameters);
         URL url = new URL(endpoint);
@@ -140,5 +205,102 @@ public class HttpJsonConnectorExecutor extends AbstractConnectorExecutor {
         String host = url.getHost();
         int port = url.getPort();
         MLHttpClientFactory.validate(protocol, host, port, connectorPrivateIpEnabled);
+    }
+
+    private void validateLLMInterface(String llmInterface) {
+        switch (llmInterface) {
+            case LLM_INTERFACE_OPENAI_V1_CHAT_COMPLETIONS:
+                break;
+            default:
+                throw new MLException(String.format("Unsupported llm interface: %s", llmInterface));
+        }
+    }
+
+    public final class HTTPEventSourceListener extends EventSourceListener {
+        private StreamPredictActionListener<MLTaskResponse, ?> streamActionListener;
+        private final String llmInterface;
+        private volatile AtomicBoolean isStreamClosed;
+
+        public HTTPEventSourceListener(StreamPredictActionListener<MLTaskResponse, ?> streamActionListener, String llmInterface) {
+            this.streamActionListener = streamActionListener;
+            this.llmInterface = llmInterface;
+            this.isStreamClosed = new AtomicBoolean(false);
+        }
+
+        /***
+         * Callback when the SSE endpoint connection is made.
+         * @param eventSource the event source
+         * @param response the response
+         */
+        @Override
+        public void onOpen(EventSource eventSource, Response response) {
+            log.debug("Connected to SSE Endpoint.");
+        }
+
+        /***
+         * For each event received from the SSE endpoint
+         * @param eventSource The event source
+         * @param id The id of the event
+         * @param type The type of the event which is used to filter
+         * @param data The event data
+         */
+        @Override
+        public void onEvent(EventSource eventSource, String id, String type, String data) {
+            log.debug("The data is: {}", data);
+            switch (llmInterface) {
+                case LLM_INTERFACE_OPENAI_V1_CHAT_COMPLETIONS:
+                    onOpenAIEvent(data);
+                    break;
+                default:
+                    throw new MLException(String.format("Unsupported llm interface: %s", llmInterface));
+            }
+        }
+
+        /***
+         * When the connection is closed we receive this even which is currently only logged.
+         * @param eventSource The event source
+         */
+        @Override
+        public void onClosed(EventSource eventSource) {
+            log.debug("SSE CLOSED.");
+        }
+
+        /***
+         * If there is any failure we log the error and the stack trace
+         * During stream resets with no errors we set the connected flag to false to allow the main thread to attempt a re-connect
+         * @param eventSource The event source
+         * @param t The error object
+         * @param response The response
+         */
+        @Override
+        public void onFailure(EventSource eventSource, Throwable t, Response response) {
+            log.error("SSE failure.");
+            if (t != null) {
+                log.error("Error: " + t.getMessage(), t);
+                if (t instanceof StreamResetException && t.getMessage().contains("NO_ERROR")) {
+                    // TODO: reconnect
+                } else {
+                    streamActionListener.onFailure(new MLException("SSE failure.", t));
+                }
+            }
+        }
+
+        private void onOpenAIEvent(String data) {
+            if (data.contentEquals("[DONE]")) {
+                sendCompletionResponse(isStreamClosed, streamActionListener);
+                return;
+            }
+            Map<String, Object> dataMap = StringUtils.fromJson(data, "data");
+            String llmFinishReason = JsonPath.read(dataMap, FINISH_REASON_PATH);
+            if (llmFinishReason != null && llmFinishReason.contentEquals("stop")) {
+                sendCompletionResponse(isStreamClosed, streamActionListener);
+                return;
+            }
+            String deltaContent = JsonPath.read(dataMap, "$.choices[0].delta.content");
+            if (deltaContent != null && !deltaContent.isEmpty()) {
+                log.debug("Streaming content: {}", deltaContent);
+                sendContentResponse(deltaContent, false, streamActionListener);
+            }
+        }
     }
 }
