7d64200a317ec6b174c55a175504e1781f9128d8
Yaliang Wu <ylwu@amazon.com>
2025-10-07 17:59:23 -0700

    fix json parsing error by extracing json first; add for each processor; support input processor (#4278)
    (cherry picked from commit b8d70c9c154a2f8d6120cad8deb6afc163c0fc18)

--- plugin/src/main/java/org/opensearch/ml/action/memorycontainer/memory/MemoryProcessingService.java
+++ plugin/src/main/java/org/opensearch/ml/action/memorycontainer/memory/MemoryProcessingService.java
@@ -46,6 +46,8 @@ import org.opensearch.ml.common.transport.memorycontainer.memory.MessageInput;
 import org.opensearch.ml.common.transport.prediction.MLPredictionTaskAction;
 import org.opensearch.ml.common.transport.prediction.MLPredictionTaskRequest;
 import org.opensearch.ml.common.utils.StringUtils;
+import org.opensearch.ml.engine.processor.MLProcessorType;
+import org.opensearch.ml.engine.processor.ProcessorChain;
 import org.opensearch.transport.client.Client;
 
 import com.jayway.jsonpath.JsonPath;
@@ -58,10 +60,14 @@ public class MemoryProcessingService {
     public static final String DEFAULT_LLM_RESULT_PATH = "$.content[0].text";
     private final Client client;
     private final NamedXContentRegistry xContentRegistry;
+    private final ProcessorChain extractJsonProcessorChain;
 
     public MemoryProcessingService(Client client, NamedXContentRegistry xContentRegistry) {
         this.client = client;
         this.xContentRegistry = xContentRegistry;
+        List<Map<String, Object>> processorConfigs = new ArrayList<>();
+        processorConfigs.add(Map.of("type", MLProcessorType.EXTRACT_JSON.getValue(), "extract_type", "object"));
+        this.extractJsonProcessorChain = new ProcessorChain(processorConfigs);
     }
 
     public void runMemoryStrategy(
@@ -321,6 +327,7 @@ public class MemoryProcessingService {
                 llmResult = cleanMarkdownFromJson(llmResult);
             }
             if (llmResult != null) {
+                llmResult = StringUtils.toJson(extractJsonProcessorChain.process(llmResult));
                 try (XContentParser parser = jsonXContent.createParser(xContentRegistry, LoggingDeprecationHandler.INSTANCE, llmResult)) {
                     ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
                     while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
