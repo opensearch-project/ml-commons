6883eb438ef4654acb1893c286e458e565493a22
Jiaru Jiang <jiaruj@amazon.com>
2025-10-29 23:27:55 +0800

    fix: summary prompt
    Signed-off-by: Jiaru Jiang <jiaruj@amazon.com>

--- ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/agent/MLChatAgentRunnerTest.java
+++ ml-algorithms/src/test/java/org/opensearch/ml/engine/algorithms/agent/MLChatAgentRunnerTest.java
@@ -1311,12 +1311,7 @@ public class MLChatAgentRunnerTest {
                             .asList(
                                 ModelTensors
                                     .builder()
-                                    .mlModelTensors(
-                                        Arrays
-                                            .asList(
-                                                ModelTensor.builder().dataAsMap(ImmutableMap.of("response", "Valid summary text")).build()
-                                            )
-                                    )
+                                    .mlModelTensors(Arrays.asList(ModelTensor.builder().result("Valid summary text").build()))
                                     .build()
                             )
                     )
