b675bfc57e83304bfad57deb9bb03a3d1ac37025
Jiaru Jiang <jiaruj@amazon.com>
2025-10-30 10:27:25 +0800

    fix: configuration file
    Signed-off-by: Jiaru Jiang <jiaruj@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/helper/ModelAccessControlHelper.java
+++ plugin/src/main/java/org/opensearch/ml/helper/ModelAccessControlHelper.java
@@ -295,7 +295,7 @@ public class ModelAccessControlHelper {
      */
     public static boolean shouldUseResourceAuthz(String resourceType) {
         var client = ResourceSharingClientAccessor.getInstance().getResourceSharingClient();
-        return client != null;
+        return client != null && client.isFeatureEnabledForType(resourceType);
     }
 
     public boolean skipModelAccessControl(User user) {
