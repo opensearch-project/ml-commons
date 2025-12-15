2cc5c8a86aaf41ec7c26e7e2205031246e16bb63
Mingshi Liu <mingshl@amazon.com>
2025-12-15 11:40:04 -0800

    add update api
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>

--- common/src/main/java/org/opensearch/ml/common/CommonValue.java
+++ common/src/main/java/org/opensearch/ml/common/CommonValue.java
@@ -100,6 +100,7 @@ public class CommonValue {
     public static final Version VERSION_3_2_0 = Version.fromString("3.2.0");
     public static final Version VERSION_3_3_0 = Version.fromString("3.3.0");
     public static final Version VERSION_3_4_0 = Version.fromString("3.4.0");
+    public static final Version VERSION_3_5_0 = Version.fromString("3.5.0");
 
     // Connector Constants
     public static final String NAME_FIELD = "name";
