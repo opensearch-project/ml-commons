8fc46835b2e56121174201a7c61e6833a17a31a5
Mingshi Liu <mingshl@amazon.com>
2026-01-08 13:16:40 -0800

    pass hook registry to agui agent
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/MLAGUIAgentRunner.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/MLAGUIAgentRunner.java
@@ -37,6 +37,7 @@ import org.opensearch.common.settings.Settings;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.ml.common.agent.MLAgent;
+import org.opensearch.ml.common.hooks.HookRegistry;
 import org.opensearch.ml.common.memory.Memory;
 import org.opensearch.ml.common.spi.tools.Tool;
 import org.opensearch.ml.engine.encryptor.Encryptor;
@@ -63,6 +64,7 @@ public class MLAGUIAgentRunner implements MLAgentRunner {
     private final Map<String, Memory.Factory> memoryFactoryMap;
     private final SdkClient sdkClient;
     private final Encryptor encryptor;
+    private final HookRegistry hookRegistry;
 
     public MLAGUIAgentRunner(
         Client client,
@@ -74,6 +76,20 @@ public class MLAGUIAgentRunner implements MLAgentRunner {
         SdkClient sdkClient,
         Encryptor encryptor
     ) {
+        this(client, settings, clusterService, xContentRegistry, toolFactories, memoryFactoryMap, sdkClient, encryptor, null);
+    }
+
+    public MLAGUIAgentRunner(
+        Client client,
+        Settings settings,
+        ClusterService clusterService,
+        NamedXContentRegistry xContentRegistry,
+        Map<String, Tool.Factory> toolFactories,
+        Map<String, Memory.Factory> memoryFactoryMap,
+        SdkClient sdkClient,
+        Encryptor encryptor,
+        HookRegistry hookRegistry
+    ) {
         this.client = client;
         this.settings = settings;
         this.clusterService = clusterService;
@@ -82,6 +98,7 @@ public class MLAGUIAgentRunner implements MLAgentRunner {
         this.memoryFactoryMap = memoryFactoryMap;
         this.sdkClient = sdkClient;
         this.encryptor = encryptor;
+        this.hookRegistry = hookRegistry;
     }
 
     @Override
@@ -110,7 +127,8 @@ public class MLAGUIAgentRunner implements MLAgentRunner {
                 toolFactories,
                 memoryFactoryMap,
                 sdkClient,
-                encryptor
+                encryptor,
+                hookRegistry
             );
 
             // Execute with streaming - events are generated in RestMLExecuteStreamAction
