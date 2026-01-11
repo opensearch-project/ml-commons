93e7f36a120165f830811dbc333c2adace5f67dd
Isha Gupta <igupta24@apple.com>
2026-01-11 18:46:50 +0530

    Complete fix for SearchIndexTool JSON normalization with security enhancements
    Signed-off-by: ishag4 <isha26701@gmail.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/SearchIndexTool.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/SearchIndexTool.java
@@ -276,7 +276,9 @@ public class SearchIndexTool implements Tool {
     private SearchRequest getSearchRequest(String index, String query) throws IOException {
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         try {
-            XContentParser queryParser = XContentType.JSON.xContent().createParser(xContentRegistry, LoggingDeprecationHandler.INSTANCE, query);
+            XContentParser queryParser = XContentType.JSON
+                .xContent()
+                .createParser(xContentRegistry, LoggingDeprecationHandler.INSTANCE, query);
             searchSourceBuilder.parseXContent(queryParser);
         } catch (Exception e) {
             throw new IllegalArgumentException("Invalid query format: malformed JSON structure detected", e);
