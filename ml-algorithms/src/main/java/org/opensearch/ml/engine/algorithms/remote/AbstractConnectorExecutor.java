fb867ab96962d92d3fb7752dcfc5ae0e593df061
Jiaping Zeng <jpz@amazon.com>
2026-01-16 12:34:26 -0800

    Accept passthrough headers in agent execute (#4364)
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AbstractConnectorExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AbstractConnectorExecutor.java
@@ -5,12 +5,18 @@
 
 package org.opensearch.ml.engine.algorithms.remote;
 
+import java.net.http.HttpRequest;
+
+import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.ml.common.CommonValue;
 import org.opensearch.ml.common.connector.Connector;
 import org.opensearch.ml.common.connector.ConnectorClientConfig;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Setter
 @Getter
 public abstract class AbstractConnectorExecutor implements RemoteConnectorExecutor {
@@ -21,6 +27,55 @@ public abstract class AbstractConnectorExecutor implements RemoteConnectorExecut
             connectorClientConfig = connector.getConnectorClientConfig();
         } else {
             connectorClientConfig = new ConnectorClientConfig();
+        }
+    }
+
+    /**
+     * Gets MCP request headers from ThreadContext and adds them to the HTTP request builder.
+     * 
+     * @param builder HttpRequest.Builder to add headers to
+     */
+    protected void getMcpRequestHeaders(HttpRequest.Builder builder) {
+        if (getClient() == null) {
+            return;
+        }
+
+        ThreadContext threadContext = getClient().threadPool().getThreadContext();
+
+        String accessKeyId = threadContext.getHeader(CommonValue.MCP_HEADER_AWS_ACCESS_KEY_ID);
+        if (accessKeyId != null && !accessKeyId.isEmpty()) {
+            builder.header(CommonValue.MCP_HEADER_AWS_ACCESS_KEY_ID, accessKeyId);
+            log.debug("Get MCP header: {}", CommonValue.MCP_HEADER_AWS_ACCESS_KEY_ID);
+        }
+
+        String secretAccessKey = threadContext.getHeader(CommonValue.MCP_HEADER_AWS_SECRET_ACCESS_KEY);
+        if (secretAccessKey != null && !secretAccessKey.isEmpty()) {
+            builder.header(CommonValue.MCP_HEADER_AWS_SECRET_ACCESS_KEY, secretAccessKey);
+            log.debug("Get MCP header: {}", CommonValue.MCP_HEADER_AWS_SECRET_ACCESS_KEY);
+        }
+
+        String sessionToken = threadContext.getHeader(CommonValue.MCP_HEADER_AWS_SESSION_TOKEN);
+        if (sessionToken != null && !sessionToken.isEmpty()) {
+            builder.header(CommonValue.MCP_HEADER_AWS_SESSION_TOKEN, sessionToken);
+            log.debug("Get MCP header: {}", CommonValue.MCP_HEADER_AWS_SESSION_TOKEN);
+        }
+
+        String region = threadContext.getHeader(CommonValue.MCP_HEADER_AWS_REGION);
+        if (region != null && !region.isEmpty()) {
+            builder.header(CommonValue.MCP_HEADER_AWS_REGION, region);
+            log.debug("Get MCP header: {}", CommonValue.MCP_HEADER_AWS_REGION);
+        }
+
+        String serviceName = threadContext.getHeader(CommonValue.MCP_HEADER_AWS_SERVICE_NAME);
+        if (serviceName != null && !serviceName.isEmpty()) {
+            builder.header(CommonValue.MCP_HEADER_AWS_SERVICE_NAME, serviceName);
+            log.debug("Get MCP header: {}", CommonValue.MCP_HEADER_AWS_SERVICE_NAME);
+        }
+
+        String opensearchUrl = threadContext.getHeader(CommonValue.MCP_HEADER_OPENSEARCH_URL);
+        if (opensearchUrl != null && !opensearchUrl.isEmpty()) {
+            builder.header(CommonValue.MCP_HEADER_OPENSEARCH_URL, opensearchUrl);
+            log.debug("Get MCP header: {}", CommonValue.MCP_HEADER_OPENSEARCH_URL);
         }
     }
 }
--- plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
+++ plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
@@ -1368,7 +1368,8 @@ public class MachineLearningPlugin extends Plugin
                 MLCommonsSettings.REMOTE_METADATA_GLOBAL_TENANT_ID,
                 MLCommonsSettings.REMOTE_METADATA_GLOBAL_RESOURCE_CACHE_TTL,
                 MLCommonsSettings.ML_COMMONS_STREAM_ENABLED,
-                MLCommonsSettings.ML_COMMONS_MAX_JSON_SIZE
+                MLCommonsSettings.ML_COMMONS_MAX_JSON_SIZE,
+                MLCommonsSettings.ML_COMMONS_MCP_HEADER_PASSTHROUGH_ENABLED
             );
         return settings;
     }
