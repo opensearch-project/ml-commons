8763de5768c1d11ecba52092f6a27005c6afc65f
Pavan Yekbote <pybot@amazon.com>
2026-01-21 14:19:47 -0800

    fix: remove unused code
    Signed-off-by: Pavan Yekbote <pybot@amazon.com>

--- common/src/main/java/org/opensearch/ml/common/agent/ContentBlock.java
+++ common/src/main/java/org/opensearch/ml/common/agent/ContentBlock.java
@@ -19,54 +19,10 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-public class ContentBlock implements Writeable {
+public class ContentBlock {
     private ContentType type;
     private String text; // for text content
     private ImageContent image; // for image content
     private VideoContent video; // for video content
     private DocumentContent document; // for document content
-
-    /**
-     * Constructor for text content block.
-     */
-    public ContentBlock(String text) {
-        this.type = ContentType.TEXT;
-        this.text = text;
-    }
-
-    /**
-     * Constructor for image content block.
-     */
-    public ContentBlock(ImageContent image) {
-        this.type = ContentType.IMAGE;
-        this.image = image;
-    }
-
-    /**
-     * Constructor for video content block.
-     */
-    public ContentBlock(VideoContent video) {
-        this.type = ContentType.VIDEO;
-        this.video = video;
-    }
-
-    /**
-     * Constructor for document content block.
-     */
-    public ContentBlock(DocumentContent document) {
-        this.type = ContentType.DOCUMENT;
-        this.document = document;
-    }
-
-    // TODO: Add stream and XContent constructors when content classes support them
-    // For now, we'll implement a basic version for the POC
-
-    @Override
-    public void writeTo(StreamOutput out) throws IOException {
-        // Basic implementation for POC - only support text content for now
-        out.writeString(type != null ? type.name() : ContentType.TEXT.name());
-        out.writeOptionalString(text);
-        // TODO: Add support for other content types when their classes support serialization
-    }
 }
