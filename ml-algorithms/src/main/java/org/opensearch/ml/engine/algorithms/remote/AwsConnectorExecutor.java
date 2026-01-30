9c3fd39c0fcb2b1897c16e523185d588265482c0
zane-neo <zaniu@amazon.com>
2026-01-30 17:43:55 +0800

    Optimize inline connector async httpclient instance creation
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AwsConnectorExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AwsConnectorExecutor.java
@@ -201,7 +201,7 @@ public class AwsConnectorExecutor extends AbstractConnectorExecutor {
     }
 
     @VisibleForTesting
-    protected SdkAsyncHttpClient getHttpClient() {
+    public SdkAsyncHttpClient getHttpClient() {
         if (httpClientRef.get() == null) {
             Duration connectionTimeout = Duration.ofSeconds(super.getConnectorClientConfig().getConnectionTimeout());
             Duration readTimeout = Duration.ofSeconds(super.getConnectorClientConfig().getReadTimeout());
@@ -233,5 +233,11 @@ public class AwsConnectorExecutor extends AbstractConnectorExecutor {
                 );
         }
         return httpClientRef.get();
+    }
+
+    public void setAsyncHttpClient(SdkAsyncHttpClient client) {
+        if (this.httpClientRef.get() == null) {
+            this.httpClientRef.set(client);
+        }
     }
 }
