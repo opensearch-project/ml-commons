3ca45fcd2e73f985cca158f1e6c4e97a53401015
zane-neo <zaniu@amazon.com>
2026-01-31 08:44:48 +0800

    optimize code
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AwsConnectorExecutor.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/remote/AwsConnectorExecutor.java
@@ -127,17 +127,9 @@ public class AwsConnectorExecutor extends AbstractConnectorExecutor {
             }
             ThreadContext.StoredContext storedContext = client.threadPool().getThreadContext().newStoredContext(true);
 
-            // Wrap listener to offload response processing from Netty I/O thread to ML thread pool
-            // This prevents blocking I/O threads during long-running cases like PER agent.
             // TODO: We should have an idea to identify the source of the request(predict/agent execution),
             // but currently it's not easy, so reusing the predict thread pool won't harm anything.
-            ThreadedActionListener<Tuple<Integer, ModelTensors>> threadedListener = new ThreadedActionListener<>(
-                log,
-                client.threadPool(),
-                "opensearch_ml_predict_remote",
-                actionListener,
-                false
-            );
+            ThreadedActionListener<Tuple<Integer, ModelTensors>> threadedListener = createThreadedListener(log, actionListener);
 
             AsyncExecuteRequest executeRequest = AsyncExecuteRequest
                 .builder()
