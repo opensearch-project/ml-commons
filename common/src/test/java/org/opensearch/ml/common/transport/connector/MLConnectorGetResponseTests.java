870a57c39de5d42e88f0d1b5ab1cefaecbf8a1fa
Rithin Pullela <rithinp@amazon.com>
2026-04-01 10:16:04 -0700

    Fix connection_timeout and read_timeout defaults to match seconds unit (#4759)
    * Fix connection_timeout and read_timeout default values to match seconds unit
    
    The default values for connection_timeout and read_timeout in
    ConnectorClientConfig were set to 30000, but the code interprets these
    values as seconds via Duration.ofSeconds(). This resulted in effective
    timeouts of ~8.3 hours instead of the intended 30 seconds, causing
    connection pool exhaustion errors in integration tests.
    
    Changed defaults from 30000 to 30 so that Duration.ofSeconds(30)
    correctly produces 30-second timeouts. Also fixed log messages to
    display the unit as seconds instead of milliseconds.
    
    Signed-off-by: Rithin Pullela <rithinp@amazon.com>
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    * Update tests to use seconds for connection_timeout and read_timeout
    
    Update integration tests (50000 -> 50) and unit test expected values
    (30000 -> 30) to match the corrected default values in
    ConnectorClientConfig.
    
    Signed-off-by: Rithin Pullela <rithinp@amazon.com>
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    * Fix ConnectorClientConfigTest default value assertion
    
    Signed-off-by: Rithin Pullela <rithinp@amazon.com>
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>
    
    ---------
    
    Signed-off-by: Rithin Pullela <rithinp@amazon.com>
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>

--- common/src/test/java/org/opensearch/ml/common/transport/connector/MLConnectorGetResponseTests.java
+++ common/src/test/java/org/opensearch/ml/common/transport/connector/MLConnectorGetResponseTests.java
@@ -71,7 +71,7 @@ public class MLConnectorGetResponseTests {
             + "\"post_process_function\":\"connector.post_process.openai.embedding\"}],"
             + "\"backend_roles\":[\"role1\",\"role2\"],\"access\":\"public\","
             + "\"client_config\":{\"max_connection\":30,"
-            + "\"connection_timeout\":30000,\"read_timeout\":30000,"
+            + "\"connection_timeout\":30,\"read_timeout\":30,"
             + "\"retry_backoff_millis\":10,\"retry_timeout_seconds\":10,\"max_retry_times\":-1,\"retry_backoff_policy\":\"constant\"}}";
 
         assertEquals(expectedControllerResponse, jsonStr);
--- common/src/test/java/org/opensearch/ml/common/transport/register/MLRegisterModelInputTest.java
+++ common/src/test/java/org/opensearch/ml/common/transport/register/MLRegisterModelInputTest.java
@@ -65,7 +65,7 @@ public class MLRegisterModelInputTest {
         + "\"pre_process_function\":\"connector.pre_process.openai.embedding\","
         + "\"post_process_function\":\"connector.post_process.openai.embedding\"}],"
         + "\"backend_roles\":[\"role1\",\"role2\"],\"access\":\"public\","
-        + "\"client_config\":{\"max_connection\":30,\"connection_timeout\":30000,\"read_timeout\":30000,"
+        + "\"client_config\":{\"max_connection\":30,\"connection_timeout\":30,\"read_timeout\":30,"
         + "\"retry_backoff_millis\":10,\"retry_timeout_seconds\":10,\"max_retry_times\":-1,\"retry_backoff_policy\":\"constant\"}},\"is_hidden\":false}";
     private final FunctionName functionName = FunctionName.TEXT_EMBEDDING;
     private final String modelName = "modelName";
@@ -189,7 +189,7 @@ public class MLRegisterModelInputTest {
             + "\"pre_process_function\":\"connector.pre_process.openai.embedding\","
             + "\"post_process_function\":\"connector.post_process.openai.embedding\"}],"
             + "\"backend_roles\":[\"role1\",\"role2\"],\"access\":\"public\","
-            + "\"client_config\":{\"max_connection\":30,\"connection_timeout\":30000,\"read_timeout\":30000,"
+            + "\"client_config\":{\"max_connection\":30,\"connection_timeout\":30,\"read_timeout\":30,"
             + "\"retry_backoff_millis\":10,\"retry_timeout_seconds\":10,\"max_retry_times\":-1,\"retry_backoff_policy\":\"constant\"}},\"is_hidden\":false}";
 
         assertEquals(expectedFunctionInputStr, jsonStr);
@@ -219,7 +219,7 @@ public class MLRegisterModelInputTest {
             + "\"pre_process_function\":\"connector.pre_process.openai.embedding\","
             + "\"post_process_function\":\"connector.post_process.openai.embedding\"}],"
             + "\"backend_roles\":[\"role1\",\"role2\"],\"access\":\"public\","
-            + "\"client_config\":{\"max_connection\":30,\"connection_timeout\":30000,\"read_timeout\":30000,"
+            + "\"client_config\":{\"max_connection\":30,\"connection_timeout\":30,\"read_timeout\":30,"
             + "\"retry_backoff_millis\":10,\"retry_timeout_seconds\":10,\"max_retry_times\":-1,\"retry_backoff_policy\":\"constant\"}},\"is_hidden\":false}";
 
         assertEquals(expectedIncompleteInputStr, jsonStr);
