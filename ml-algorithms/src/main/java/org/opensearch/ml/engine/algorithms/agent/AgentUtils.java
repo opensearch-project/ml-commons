d28d3b1c74c5a5d28a8deff0fb3c8a4f79da9b31
Abdul Muneer Kolarkunnu <muneer.kolarkunnu@netapp.com>
2026-04-01 22:43:36 +0530

    Revert "Support tool description override in MCP connector"
    This reverts commit f042848309a856b065c62855a1345a1cefb78efd.
    
    Signed-off-by: Abdul Muneer Kolarkunnu <muneer.kolarkunnu@netapp.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/AgentUtils.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/algorithms/agent/AgentUtils.java
@@ -887,6 +887,15 @@ public class AgentUtils {
         if (Strings.isNullOrEmpty(overrideDescription)) {
             return toolSpec;
         }
+        System.out
+            .println(
+                "*************************** toolname:"
+                    + toolSpec.getName()
+                    + ": old description: "
+                    + toolSpec.getDescription()
+                    + " :overrideDescription: "
+                    + overrideDescription
+            );
         return toolSpec.toBuilder().description(overrideDescription).runtimeResources(toolSpec.getRuntimeResources()).build();
     }
 
